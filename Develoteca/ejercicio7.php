63de977aa168b4457340b53406f7494b6b3939a2
Diego Pasten <diegopasten78@gmail.com>
2023-05-22 11:01:25 -0400

    Se agrega el ejercicio 7.

---
+++ Develoteca/ejercicio7.php
@@ -0,0 +1,14 @@
+<?php
+    // Variable no constante 
+    $edad = 32;
+    echo $edad;
+    echo "<br>";
+    $edad = 40;
+    echo $edad;
+    echo "<br>";
+
+    // Constante
+    define("nombre", "diego");
+    echo nombre;
+
+?>
