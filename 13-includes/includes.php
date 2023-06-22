c8af10fac96cc6d001c83ff7b6a0426eca00f8fb
Diego Pasten <diegopasten78@gmail.com>
2023-06-22 17:11:51 -0400

    Se elimina el archivo includes.php, ya que se cambió el nombre del archivo por index y no es necesario guardar includes.php dentro de la carpeta.

--- 13-includes/includes.php
+++
@@ -1,8 +0,0 @@
-<?php
-    echo "<h2>Includes</h2>";
-    echo "<p>En PHP, el uso de 'includes' es una forma común de reutilizar código y organizar tu proyecto. La declaración 'include' se utiliza para importar y ejecutar el contenido de otro archivo PHP dentro de un archivo actual. </p>";  
-    echo "<p>Esto te permite dividir tu código en archivos más pequeños y mantener una estructura modular.</p>";  
-    echo "<p>Hay dos formas principales de incluir archivos en PHP: 'include' y 'require'. La diferencia entre ellos radica en cómo manejan los errores. La declaración 'include' mostrará una advertencia y continuará la ejecución del script si no se encuentra el archivo incluido, mientras que la declaración 'require' mostrará un error fatal y detendrá la ejecución.</p>";
-
-
-?>
