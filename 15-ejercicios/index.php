f6ce1a967a0deb0b9d46139b63c98ef0b81d3f19
Diego Pasten <diegopasten78@gmail.com>
2023-06-28 18:53:45 -0400

    Hola.

---
+++ 15-ejercicios/index.php
@@ -0,0 +1,7 @@
+<?php
+
+ //Ejercicio 1: Hacer un programa que tenga una Array con 8 numeros enteros y que haga lo siguiente: a) Recorer y mostrar el array, b) ordenarlo y mostrar el array, c) mostrar su longitud, d) mostrar algún elemento dentro del array.
+
+ 
+
+?>
