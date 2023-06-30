371799d89581d0fd1658f36cc599b168a7c236e2
Diego Pasten <diegopasten78@gmail.com>
2023-06-29 21:08:49 -0400

    Se elimina el archivo index.php, ya que no es necesario almacenarlo dentro del repositorio.

--- 15-ejercicios/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
- //Ejercicio 1: Hacer un programa que tenga una Array con 8 numeros enteros y que haga lo siguiente: a) Recorer y mostrar el array, b) ordenarlo y mostrar el array, c) mostrar su longitud, d) mostrar algún elemento dentro del array.
-
- 
-
-?>
