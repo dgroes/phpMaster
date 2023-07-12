d5b606ba5c9cd63d61840716c844655a9bfdc748
Diego Pasten <diegopasten78@gmail.com>
2023-07-12 15:26:47 -0400

    Se finaliza ejercicio con ejemplo terminado sobre el Envio y Recibir de un formulario en PHP.

--- 18-formularios/guardar.php
+++ 18-formularios/guardar.php
@@ -1,5 +1,8 @@
 <?php
+    if(isset($_POST['titulo']) && isset($_POST['descripcion'])){
+        echo "<h2>".$_POST['titulo']."</h2>";
+        echo "<p>".$_POST['descripcion']."</p>";
+    }
 
-    
 
 ?>
