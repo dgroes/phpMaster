fc82671e8684fa4b585818c9a5c068695feed7ae
Diego Pasten <diegopasten78@gmail.com>
2023-07-12 16:33:41 -0400

    Se actaulizan los ficheros de la carpeta.

--- 19-validacion/index.php
+++ 19-validacion/index.php
@@ -7,9 +7,32 @@
 </head>
 <body>
     <h1>Validar Formularios en PHP</h1>
+    <?php
+        if(isset($_GET['error'])){
+            $error = $_GET['error'];
+            if($error == 'faltan_valores'){
+                echo '<strong style="color:red">Introduce todos los campos de formulario por favor.</strong>';
+            }
+        }
+    ?>
     <form action="procesar_formulario.php" method="post">
+    <label for="nombre">Nombre</label><br>
+    <input type="text" name="nombre" required="required" pattern="[A-Za-z]+"><br>
 
-    </form>
+    <label for="apellido">Apellido</label><br>
+    <input type="text" name="apellido" required="required" pattern="[A-Za-z]+"><br>
+        
+    <label for="edad">Edad</label><br>
+    <input type="number" name="edad" required="required" pattern="[0-9]+"><br>
+
+    <label for="email">Email</label><br>
+    <input type="email" name="email" required="required"><br>
+
+    <label for="contrasena">Contraseña</label><br>
+    <input type="password" name="contrasena"><br>
+
+    <input type="submit" value="Enviar">
+</form>
     
 </body>
 </html>
