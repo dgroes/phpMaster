267ccbf56418d795e64a47eaa166077c1e5a39ca
Diego Pasten <diegopasten78@gmail.com>
2023-07-12 15:38:18 -0400

    Se agrega fichero el cual contiene el formulario.

---
+++ 19-validacion/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Validación de Formulario</title>
+</head>
+<body>
+    <h1>Validar Formularios en PHP</h1>
+    <form action="procesar_formulario.php" method="post">
+
+    </form>
+    
+</body>
+</html>
