6bc6f7cebd6023e1fd22d48aadbb6bb32d1943e9
Diego Pasten <diegopasten78@gmail.com>
2023-07-15 16:46:32 -0400

    se modifica el comentario de filer_var.

--- EjerciciosGPT/validacionDeFormulario/procesar_formulario.php
+++ EjerciciosGPT/validacionDeFormulario/procesar_formulario.php
@@ -16,6 +16,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $errors[] = 'El campo Email es requerido';
     } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $errors[] = 'El email proporcionado no es válido';
+				//Este elseif se utiliza para validar el campo de email en el formulario. Veamos cómo funciona:
+				/* En esta parte del código, se utiliza la función filter_var() de PHP para validar si el valor del campo email cumple con el formato de una dirección de correo electrónico válida.*/
+				/*La función filter_var() toma dos argumentos: el valor que se desea validar y el filtro que se aplicará. En este caso, el valor es $email, que es el valor ingresado por el usuario en el campo de email del formulario.
+				El filtro utilizado es FILTER_VALIDATE_EMAIL, que es una constante predefinida en PHP para validar direcciones de correo electrónico.
+				Si filter_var() devuelve false, significa que el valor proporcionado en el campo de email no es válido según el formato de una dirección de correo electrónico. En ese caso, se agrega un mensaje de error al array $errors[], indicando que el email proporcionado no es válido.
+				De esta manera, se verifica que el campo de email tenga un formato válido antes de continuar con el procesamiento de los datos.
+				Es importante realizar esta validación para asegurarse de que los datos ingresados cumplan con los requisitos esperados y evitar posibles problemas al utilizarlos posteriormente.
+				*/
     }
 
     // Si no hay errores, procesar los datos
