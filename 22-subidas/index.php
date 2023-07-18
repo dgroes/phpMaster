ba55d1c3883cf6d92f86c825cccd28c91a583bde
Diego Pasten <diegopasten78@gmail.com>
2023-07-18 12:23:25 -0400

    Se finaliza el fichero y se actualiza con una lectura de el directorio de imagenes.

--- 22-subidas/index.php
+++ 22-subidas/index.php
@@ -10,9 +10,22 @@
     <form action="upload.php" method="post" enctype="multipart/form-data">
         <input type="file" name="archivo">
         <input type="submit" value="enviar">
-        
-
     </form>
+    
+
+    <h2>Listado de imagenes</h2>
+    <?php
+
+        $gestor = opendir('./images');
+        if($gestor):
+            while (($image = readdir($gestor)) !== false):
+                if($image != '.' && $image != '..'):
+                    echo "<img src='images/$image' width='200px'><br>";
+                endif;
+            endwhile;
+        endif;
+
+    ?>
     
 </body>
 </html>
