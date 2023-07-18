be7482ad4fa44b565e7ca4a2464e711e6600ca3f
Diego Pasten <diegopasten78@gmail.com>
2023-07-18 12:07:30 -0400

    Se actualiza el fichero index.php

--- 22-subidas/index.php
+++ 22-subidas/index.php
@@ -6,8 +6,12 @@
     <title>Subida de archivos</title>
 </head>
 <body>
-    <form action="">
+    <h1>Subir archivos en PHP</h1>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="archivo">
+        <input type="submit" value="enviar">
         
+
     </form>
     
 </body>
