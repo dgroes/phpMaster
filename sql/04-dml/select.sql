d281f979f160494727b7da9f6af858f2a8b3b617
Diego Pasten <diegopasten78@gmail.com>
2023-07-26 17:05:22 -0400

    Ejemplo de operadores aritmeticos.

--- sql/04-dml/select.sql
+++ sql/04-dml/select.sql
@@ -1,2 +1,12 @@
 #Mostrar todos los registros de una tabla#
 SELECT email, nombre, apellidos FROM usuarios;
+
+#Mostrar todos los campos#
+SELECT * FROM usuarios;
+
+#Operadores aritmeticos#
+SELECT email, (4+7) as 'Operacion' FROM usuarios; 
+
+SELECT id, email  FROM usuarios ORDER BY id; 
+
+#Funciones Matematicas#
