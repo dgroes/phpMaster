63621142dab9a59797b3497734cef7a99f60a3ab
Diego Pasten <diegopasten78@gmail.com>
2023-07-26 16:46:01 -0400

    Ejmplo de introducir valores dentro de una tabla.

---
+++ sql/04-dml/inser.sql
@@ -0,0 +1,8 @@
+#insertar nuevos registros
+INSERT INTO usuarios VALUES(null, 'Diego', 'Pasten Uribe', 'diego@gmail.com', '192837465n', '2023-07-26');
+INSERT INTO usuarios VALUES(null, 'Miguel', 'Paz Urritia', 'miguel@gmail.com', '385720jewm', '2023-07-26');
+INSERT INTO usuarios VALUES(null, 'Carmen', 'Espinoza Flores', 'carmen@gmail.com', 'jfhII38103p', '2023-07-26');
+
+
+#Insertar filas solo con ciertas columnas#
+INSERT INTO usuarios(email, password) VALUES('dark@gmail.com', '1234admin');
