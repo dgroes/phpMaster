7ee6b43e1eda88abc6748fc75334119018a30ee5
Diego Pasten <diegopasten78@gmail.com>
2023-07-26 20:36:50 -0400

    Se agregan funciones de fechas.

--- sql/04-dml/select.sql
+++ sql/04-dml/select.sql
@@ -11,3 +11,29 @@ SELECT id, email  FROM usuarios ORDER BY id;
 
 #Funciones Matematicas#
 SELECT abs(7) as 'Operacion' FROM usuarios; 
+
+
+#Funciones de caracteres
+SELECT UPPER(NOMBRE) FROM USUARIOS;
+SELECT CONCAT(nombre, ' ' apellidos) from usuarios;
+SELECT UPPER(CONCAT(nombre, ' ', apellidos)) as nombreCompleto from usuarios;
+SELECT LENGTH(CONCAT(nombre, ' ', apellidos)) as nombreCompleto from usuarios;
+SELECT TRIM(CONCAT('       ', nombre, ' ', apellidos, '       ')) as nombreCompleto from usuarios;
+
+#Funciones con fechas#
+SELECT fecha from USUARIOS;
+SELECT fecha, curdate() as fechaActual from USUARIOS;
+SELECT nombre, datediff(fecha, curdate()) as fechaActual from USUARIOS; ##diaz de diferencia
+SELECT nombre, dayname(fecha) as fecha from USUARIOS;
+SELECT nombre, dayofmonth(fecha) as fecha from USUARIOS;
+SELECT nombre, dayofweek(fecha) as fecha from USUARIOS;
+SELECT nombre, dayofyear(fecha) as fecha from USUARIOS;
+SELECT nombre, month(fecha) as fecha from USUARIOS;
+SELECT nombre, year(fecha) as fecha from USUARIOS;
+SELECT nombre, day(fecha) as fecha from USUARIOS;
+SELECT nombre, hour(fecha) as fecha from USUARIOS;
+SELECT nombre, CURTIME() as fecha from USUARIOS;
+SELECT nombre, SYSDATE() as fecha from USUARIOS;
+SELECT nombre, date_format(fecha, '%d-%m-%Y') from USUARIOS;
+
+
