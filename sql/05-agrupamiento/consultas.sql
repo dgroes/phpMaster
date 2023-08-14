79eb61040064b5758ee05ceea0de86b88c39d304
Diego Pasten <diegopasten78@gmail.com>
2023-08-14 10:15:48 -0400

    Se practica Group BY, HAVING y funciones de agregación en SQL.

--- sql/05-agrupamiento/consultas.sql
+++ sql/05-agrupamiento/consultas.sql
@@ -1,1 +1,13 @@
 select count(titulo) as 'numero de entradas', categoria_id from entradas group by categoria_id;
+
+#Consulta de agrupamiento con condiciones#
+select count(titulo) as 'numero de entradas', categoria_id from entradas 
+group by categoria_id having count(titulo)>=1;
+
+
+select * from entradas;
+select avg(id) as 'Media de entradas' from entradas;
+
+select max(id) as 'Maximo ID', titulo from entradas;
+
+select sum(id) as 'Suma de ID' from entradas;
