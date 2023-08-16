c1d3bd6c281308fb6577522a8843a2fa59ff648a
Diego Pasten <diegopasten78@gmail.com>
2023-08-16 11:03:35 -0400

    Update

--- sql/09-ejercicios/ejercicio2.sql
+++ sql/09-ejercicios/ejercicio2.sql
@@ -1,1 +1,3 @@
-#Modificar la comisión de los vendedores y ponerla al 2% cuando ganan mas de 50.000#
+#Modificar la comisión de los vendedores y ponerla al 0.5% cuando ganan mas de 50.000#
+UPDATE vendedores SET comision=0.5 WHERE sueldo >=50000;
+select * from vendedores;
