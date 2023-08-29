bbbdf38bfb6f52553222007a40d754c35fa85184
Diego Pasten <diegopasten78@gmail.com>
2023-08-29 12:56:44 -0400

    Se crea index.php inicial y se avanza hasta <header>.

---
+++ proyecto-php/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog de Videojuegos</title>
+</head>
+
+<body>
+    <!-- CABECERA -->
+    <header>
+        <div id="logo">
+            <a href="index.php">
+                Blog de VideoJuegos
+            </a>
+        </div>
+    </header>
+    <!-- MENÚ -->
+
+    <!-- BARRAR LATERAL -->
+
+    <!-- CAJA PRINCIPAL -->
+
+    <!-- PIE DE PÁGINA -->
+
+</body>
+
+</html>
