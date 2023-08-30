668bc3c033c3d36698ce945ef581fce36fd6cf5e
Diego Pasten <diegopasten78@gmail.com>
2023-08-30 13:12:16 -0400

    Inlcude de latera de página.

---
+++ proyecto-php/includes/lateral.php
@@ -0,0 +1,31 @@
+<aside id="sidebar">
+    <div id="login" class="bloque">
+        <h3>Identificate</h3>
+        <form action="login.php" method="post">
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email">
+
+            <label for="password">Contraseña</label>
+            <input type="password" name="password">
+            <input type="submit" value="Entrar">
+        </form>
+    </div>
+
+    <div id="register" class="bloque">
+        <h3>Registrate</h3>
+        <form action="registro.php" method="post">
+            <label for="nombre">Nombre</label>
+            <input type="text" name="nombre">
+
+            <label for="apellidos">Apellidos</label>
+            <input type="text" name="apellidos">
+
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email">
+
+            <label for="password">Contraseña</label>
+            <input type="password" name="password">
+            <input type="submit" value="Registrar">
+        </form>
+    </div>
+</aside>
