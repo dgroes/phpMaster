4cc7fcc249d2889df0df833f9f81729eceaba42d
Diego Pasten <diegopasten78@gmail.com>
2023-08-30 13:11:23 -0400

    Inlcude de cabecera de página.

---
+++ proyecto-php/includes/cabecera.php
@@ -0,0 +1,53 @@
+<?php require_once 'conexion.php'; ?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog de Videojuegos</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <!-- CABECERA -->
+    <header id="cabecera">
+        <!-- LOGO -->
+        <div id="logo">
+            <a href="index.php">
+                Blog de VideoJuegos
+            </a>
+        </div>
+
+        <!-- MENÚ -->
+        <nav id="menu">
+            <ul>
+                <li>
+                    <a href="index.php">Inicio</a>
+                </li>
+                <li>
+                    <a href="index.php">Categoria 1</a>
+                </li>
+                <li>
+                    <a href="index.php">Categoria 2</a>
+                </li>
+                <li>
+                    <a href="index.php">Categoria 3</a>
+                </li>
+                <li>
+                    <a href="index.php">Categoria 4</a>
+                </li>
+                <li>
+                    <a href="index.php">Sobre Mi</a>
+                </li>
+                <li>
+                    <a href="index.php">Contacto</a>
+                </li>
+            </ul>
+        </nav>
+
+        <div class="clearfix"></div>
+
+    </header>
+
+<div id="contenedor">
