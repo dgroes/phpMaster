8fc0aa12b7ed59536b60163c08d00acdf60873ee
Diego Pasten <diegopasten78@gmail.com>
2023-08-30 13:11:55 -0400

    Estableciendo la conexión a la BD.

---
+++ proyecto-php/includes/conexion.php
@@ -0,0 +1,11 @@
+<?php
+//CONEXION A LA BD
+$servidor = 'localhost';
+$usuario = 'root';
+$password = '';
+$basededatos = 'blog_master';
+$db = mysqli_connect($servidor, $usuario, $password, $basededatos);
+
+mysqli_query($db, "SET NAMES 'utf8'")
+
+?>
