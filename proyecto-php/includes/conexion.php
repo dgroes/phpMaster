47abbe383f1e03c4f1b661dc3ccbee0424afac8c
Diego Pasten <diegopasten78@gmail.com>
2023-08-31 11:51:38 -0400

    Se agrega la funcion session_start.

--- proyecto-php/includes/conexion.php
+++ proyecto-php/includes/conexion.php
@@ -6,6 +6,9 @@ $password = '';
 $basededatos = 'blog_master';
 $db = mysqli_connect($servidor, $usuario, $password, $basededatos);
 
-mysqli_query($db, "SET NAMES 'utf8'")
+mysqli_query($db, "SET NAMES 'utf8'");
+
+//Iniciar la sesión
+session_start();
 
 ?>
