2ca722496c6c8ac99120fa595da0e286a3f3edd8
Diego Pasten <diegopasten78@gmail.com>
2023-08-31 18:47:31 -0400

    ..

--- proyecto-php/includes/helpers.php
+++ proyecto-php/includes/helpers.php
@@ -13,11 +13,24 @@
     //Eliminiar los mensajes de error almacenados en la sessión.
     function borrarErrores(){
         $borrado = false;
-        
+	
         if(isset($_SESSION['errores'])){
             $_SESSION['errores'] = null;
             $borrado = true;
         }
+        
+       /*  if(isset($_SESSION['errores_entrada'])){
+            $_SESSION['errores_entrada'] = null;
+            $borrado = true;
+        } */
+        
+        if(isset($_SESSION['completado'])){
+            $_SESSION['completado'] = null;
+            $borrado = true;
+        }
+        
+        return $borrado;
+    
     }
     
 
