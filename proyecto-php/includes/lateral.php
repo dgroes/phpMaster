50902fee88801339c2e110b4cbb5346914d217de
Diego Pasten <diegopasten78@gmail.com>
2023-08-31 18:48:47 -0400

    Se agregan alertas de Exito y de Error, además se hacen alertas por cada campo del registro.

--- proyecto-php/includes/lateral.php
+++ proyecto-php/includes/lateral.php
@@ -1,3 +1,5 @@
+<?php require_once 'includes/helpers.php'; ?>
+
 <aside id="sidebar">
     <div id="login" class="bloque">
         <h3>Identificate</h3>
@@ -13,19 +15,40 @@
 
     <div id="register" class="bloque">
         <h3>Registrate</h3>
+            //Alertas
+        //Alerta de EXITO
+        <?php if(isset($_SESSION['completado'])): ?>
+			<div class="alerta alerta-exito">
+				<?=$_SESSION['completado']?>
+			</div>
+            
+        //Alerta de Error
+		<?php elseif(isset($_SESSION['errores']['general'])): ?>
+			<div class="alerta alerta-error">
+				<?=$_SESSION['errores']['general']?>
+			</div>
+		<?php endif; ?>
+
+      
         <form action="registro.php" method="post">
             <label for="nombre">Nombre</label>
             <input type="text" name="nombre">
+            <?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'nombre') : ''; ?>
 
             <label for="apellidos">Apellidos</label>
             <input type="text" name="apellidos">
+            <?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'apellidos') : ''; ?>
 
             <label for="email">Email</label>
             <input type="email" name="email" id="email">
+            <?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'email') : ''; ?>
 
             <label for="password">Contraseña</label>
             <input type="password" name="password">
+            <?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'password') : ''; ?>
+
             <input type="submit" name="submit" value="Registrar">
         </form>
+        <?php borrarErrores(); ?>
     </div>
 </aside>
