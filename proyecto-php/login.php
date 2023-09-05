03c68f96891db268abeec2a1806bd61a61ff2611
Diego Pasten <diegopasten78@gmail.com>
2023-09-05 13:55:13 -0300

    Se agrega condición de recogidad de datos del formulario, consulta para comprobar las credenciales del usuario, comprobar la contraseña, utilizar una sesión par aguardar los datos del usuario logeado, si algo falla enviar una sesión con el fallo y Mensajes de error.

--- proyecto-php/login.php
+++ proyecto-php/login.php
@@ -1,4 +1,35 @@
 <?php
+/* Iniciar la sesión y la conexión a la BD */
+require_once 'includes/conexion.php';
 
+// Recoger datos del formulario
+if (isset($_POST)) {
+    $email = trim($_POST['email']);
+    $password = $_POST['password'];
 
-?>
+    //Consulta para comprobar las credenciales del usuario
+    $sql = "SELECT * FROM usuarios WHERE email = '$email'";
+	$login = mysqli_query($db, $sql);
+	
+	if($login && mysqli_num_rows($login) == 1){
+		$usuario = mysqli_fetch_assoc($login);
+		
+		// Comprobar la contraseña
+		$verify = password_verify($password, $usuario['password']);
+		
+		if($verify){
+			// Utilizar una sesión para guardar los datos del usuario logueado
+			$_SESSION['usuario'] = $usuario;
+			
+		}else{
+			// Si algo falla enviar una sesión con el fallo
+			$_SESSION['error_login'] = "Login incorrecto!!";
+		}
+	}else{
+		// mensaje de error
+		$_SESSION['error_login'] = "Login incorrecto!!";
+	}
+}
+
+// Redirigir al index.php
+header('Location: index.php');
