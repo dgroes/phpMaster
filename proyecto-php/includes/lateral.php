c52689973a9130a17af9cea716f7a0676655d0dc
Diego Pasten <diegopasten78@gmail.com>
2023-09-05 13:57:09 -0300

    Se agrega bloque de bienvenida a usuario.

--- proyecto-php/includes/lateral.php
+++ proyecto-php/includes/lateral.php
@@ -1,6 +1,13 @@
 <?php require_once 'includes/helpers.php'; ?>
 
 <aside id="sidebar">
+
+	<?php if(isset($_SESSION['usuario'])): ?>
+	<div id="usuario-logeado" class="bloque">	
+		<h3>Bienvenido, <?=$_SESSION['usuario']['nombre'].' '.$_SESSION['usuario']['apellidos'];?></h3>
+	</div>
+	<?php endif; ?>
+
     <div id="login" class="bloque">
         <h3>Identificate</h3>
         <form action="login.php" method="post">
