8f856d028c8655090c0e045e524de4b919b2872a
Diego Pasten <diegopasten78@gmail.com>
2023-09-05 14:27:01 -0300

    modificación.

--- proyecto-php/includes/lateral.php
+++ proyecto-php/includes/lateral.php
@@ -2,42 +2,54 @@
 
 <aside id="sidebar">
 
-	<?php if(isset($_SESSION['usuario'])): ?>
-	<div id="usuario-logeado" class="bloque">	
-		<h3>Bienvenido, <?=$_SESSION['usuario']['nombre'].' '.$_SESSION['usuario']['apellidos'];?></h3>
-	</div>
+<?php if(isset($_SESSION['usuario'])): ?>
+		<div id="usuario-logueado" class="bloque">
+			<h3>Bienvenido, <?=$_SESSION['usuario']['nombre'].' '.$_SESSION['usuario']['apellidos'];?></h3>
+			<!--botones-->
+			<a href="crear-entradas.php" class="boton boton-verde">Crear entradas</a>
+			<a href="crear-categoria.php" class="boton">Crear categoria</a>
+			<a href="mis-datos.php" class="boton boton-naranja">Mis datos</a>
+			<a href="cerrar.php" class="boton boton-rojo">Cerrar sesión</a>
+		</div>
 	<?php endif; ?>
 
-    <div id="login" class="bloque">
-        <h3>Identificate</h3>
-        <form action="login.php" method="post">
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email">
+	<div id="login" class="bloque">
+		<h3>Identificate</h3>
 
-            <label for="password">Contraseña</label>
-            <input type="password" name="password">
-            <input type="submit" value="Entrar">
-        </form>
-    </div>
-
-    <div id="register" class="bloque">
-        <h3>Registrate</h3>
-            <!-- //Alertas
-        //Alerta de EXITO -->
-        <?php if(isset($_SESSION['completado'])): ?>
-			<div class="alerta alerta-exito">
-				<?=$_SESSION['completado']?>
-			</div>
-            
-        <!-- //Alerta de Error -->
-		<?php elseif(isset($_SESSION['errores']['general'])): ?>
+		<?php if(isset($_SESSION['error_login'])): ?>
 			<div class="alerta alerta-error">
-				<?=$_SESSION['errores']['general']?>
+				<?=$_SESSION['error_login'];?>
 			</div>
 		<?php endif; ?>
 
-      
-        <form action="registro.php" method="POST"> 
+		<form action="login.php" method="post">
+			<label for="email">Email</label>
+			<input type="email" name="email" id="email">
+
+			<label for="password">Contraseña</label>
+			<input type="password" name="password">
+			<input type="submit" value="Entrar">
+		</form>
+	</div>
+
+	<div id="register" class="bloque">
+		<h3>Registrate</h3>
+		<!-- //Alertas
+        //Alerta de EXITO -->
+		<?php if (isset($_SESSION['completado'])) : ?>
+			<div class="alerta alerta-exito">
+				<?= $_SESSION['completado'] ?>
+			</div>
+
+			<!-- //Alerta de Error -->
+		<?php elseif (isset($_SESSION['errores']['general'])) : ?>
+			<div class="alerta alerta-error">
+				<?= $_SESSION['errores']['general'] ?>
+			</div>
+		<?php endif; ?>
+
+
+		<form action="registro.php" method="POST">
 			<label for="nombre">Nombre</label>
 			<input type="text" name="nombre" />
 			<?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'nombre') : ''; ?>
@@ -56,6 +68,6 @@
 
 			<input type="submit" name="submit" value="Registrar" />
 		</form>
-        <?php borrarErrores(); ?>
-    </div>
+		<?php borrarErrores(); ?>
+	</div>
 </aside>
