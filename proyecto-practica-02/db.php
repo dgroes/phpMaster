55698366eca18a82a5d3d1b23cdf779e12d84fb0
Diego Pasten <diegopasten78@gmail.com>
2023-09-25 10:51:32 -0300

    Conexión a la BD.

---
+++ proyecto-practica-02/db.php
@@ -0,0 +1,17 @@
+<?php
+//  Primero hacer la conexión a la BD
+    $conn = mysqli_connect(
+        'localhost',
+        'root',
+        '',
+        'proyecto_practica_dos'
+    );
+
+    //Comprobar si la conexión ha sido correcta
+    /* if(isset($conn)){
+        echo "Conectado";
+    } */
+
+
+
+?>
