21bfa1be13b06ce6e5406e939fd608becb2062eb
Diego Pasten <diegopasten78@gmail.com>
2023-09-25 13:45:47 -0300

    Se agrega linea de Font Awesome para los iconos.

--- proyecto-practica-02/includes/header.php
+++ proyecto-practica-02/includes/header.php
@@ -7,7 +7,13 @@
     <title>PHP MYSQL CRUD</title>
     <!-- BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-
+     <!-- FONT AWESOME -->
+     <script src="https://kit.fontawesome.com/72d56ef203.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container">
+            <a href="index.php" class="navbar-brand">PHP MySQL CRUD</a>
+        </div>
+    </nav>
