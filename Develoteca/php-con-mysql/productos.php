d3828ae280590ad15c4ae7b1928c75e516a6fb64
Diego Pasten <diegopasten78@gmail.com>
2023-10-27 12:42:13 -0300

    Se agrega primera parte de sección Productos.

---
+++ Develoteca/php-con-mysql/productos.php
@@ -0,0 +1,45 @@
+<?php include("template/cabecera.php"); ?>
+
+
+<section class="libros">
+    <article>
+        <h3>Hunter X Hunter: Vol.18</h3>
+        <img src="img/vol18.jpg" alt="HxH.18">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ipsa! Ex ipsam quibusdam provident quas voluptas officiis ab repellendus quo minus! Animi repudiandae, eaque totam repellendus veniam mollitia quo aliquam.</p>
+    </article>
+
+    <article>
+        <h3>Hunter X Hunter: Vol5</h3>
+        <img src="img/vol5.jpg" alt="HxH.5">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ipsa! Ex ipsam quibusdam provident quas voluptas officiis ab repellendus quo minus! Animi repudiandae, eaque totam repellendus veniam mollitia quo aliquam.</p>
+    </article>
+
+    <article>
+        <h3>Hunter X Hunter: </h3>
+        <img src="img/vol18.jpg" alt="HxH.18">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ipsa! Ex ipsam quibusdam provident quas voluptas officiis ab repellendus quo minus! Animi repudiandae, eaque totam repellendus veniam mollitia quo aliquam.</p>
+    </article>
+
+    <article>
+        <h3>Hunter X Hunter: </h3>
+        <img src="img/vol18.jpg" alt="HxH.18">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ipsa! Ex ipsam quibusdam provident quas voluptas officiis ab repellendus quo minus! Animi repudiandae, eaque totam repellendus veniam mollitia quo aliquam.</p>
+    </article>
+
+    <article>
+        <h3>Hunter X Hunter: </h3>
+        <img src="img/vol18.jpg" alt="HxH.18">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ipsa! Ex ipsam quibusdam provident quas voluptas officiis ab repellendus quo minus! Animi repudiandae, eaque totam repellendus veniam mollitia quo aliquam.</p>
+    </article>
+
+    <article>
+        <h3>Title</h3>
+        <img src="img/vol18.jpg" alt="HxH.18">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ipsa! Ex ipsam quibusdam provident quas voluptas officiis ab repellendus quo minus! Animi repudiandae, eaque totam repellendus veniam mollitia quo aliquam.</p>
+    </article>
+    
+
+</section>
+
+
+<?php include("template/pie.php"); ?>
