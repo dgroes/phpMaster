6aa992a83c0cb15ede08f736a5980665dd9ad68a
Diego Pasten <diegopasten78@gmail.com>
2023-10-27 12:38:34 -0300

    Primera Actualización de sección Nosotros.

---
+++ Develoteca/php-con-mysql/nosotros.php
@@ -0,0 +1,9 @@
+<?php include("template/cabecera.php"); ?>
+
+<main>
+    <h1>nosotros</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore deleniti harum iste iusto eveniet. Quae laborum, nobis labore nesciunt quos incidunt ullam voluptate magnam fugiat earum eaque, esse, perspiciatis nam?</p>
+
+</main>
+
+<?php include("template/pie.php"); ?>
