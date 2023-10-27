f26c960d6e68cb01a2631950f28e85756e1a63a5
Diego Pasten <diegopasten78@gmail.com>
2023-10-27 12:44:16 -0300

    Se agrega primera parte de la Cabecerá de la página.

---
+++ Develoteca/php-con-mysql/template/cabecera.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <a href="#">Develoteca</a>
+            <a href="index.php">Inicio</a>
+            <a href="productos.php">Libros</a>
+            <a href="nosotros.php">Nosotross</a>
+        </nav>
+    </header>
