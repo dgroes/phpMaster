b5f0e3b743232849f8afc3e650737fb9b6704931
Diego Pasten <diegopasten78@gmail.com>
2023-10-27 14:24:51 -0300

    Nueva modificación.

--- Develoteca/php-con-mysql/productos.php
+++ Develoteca/php-con-mysql/productos.php
@@ -4,39 +4,65 @@
 <section class="libros">
     <article>
         <h3>Hunter X Hunter: Vol.18</h3>
-        <img src="img/vol18.jpg" alt="HxH.18">
+        <img id="imagen1" src="img/vol18.jpg" alt="HxH.18">
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ipsa! Ex ipsam quibusdam provident quas voluptas officiis ab repellendus quo minus! Animi repudiandae, eaque totam repellendus veniam mollitia quo aliquam.</p>
     </article>
 
     <article>
         <h3>Hunter X Hunter: Vol5</h3>
-        <img src="img/vol5.jpg" alt="HxH.5">
+        <img src="img/vaga32.jpg" alt="HxH.5">
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ipsa! Ex ipsam quibusdam provident quas voluptas officiis ab repellendus quo minus! Animi repudiandae, eaque totam repellendus veniam mollitia quo aliquam.</p>
     </article>
 
     <article>
         <h3>Hunter X Hunter: </h3>
-        <img src="img/vol18.jpg" alt="HxH.18">
+        <img src="img/20cb21.jpg" alt="HxH.18">
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ipsa! Ex ipsam quibusdam provident quas voluptas officiis ab repellendus quo minus! Animi repudiandae, eaque totam repellendus veniam mollitia quo aliquam.</p>
     </article>
 
     <article>
         <h3>Hunter X Hunter: </h3>
-        <img src="img/vol18.jpg" alt="HxH.18">
+        <img src="img/pluto1.jpg" alt="HxH.18">
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ipsa! Ex ipsam quibusdam provident quas voluptas officiis ab repellendus quo minus! Animi repudiandae, eaque totam repellendus veniam mollitia quo aliquam.</p>
     </article>
 
     <article>
         <h3>Hunter X Hunter: </h3>
-        <img src="img/vol18.jpg" alt="HxH.18">
+        <img src="img/ber1.jpg" alt="HxH.18">
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ipsa! Ex ipsam quibusdam provident quas voluptas officiis ab repellendus quo minus! Animi repudiandae, eaque totam repellendus veniam mollitia quo aliquam.</p>
     </article>
 
     <article>
         <h3>Title</h3>
-        <img src="img/vol18.jpg" alt="HxH.18">
+        <img src="img/ber2.jpg" alt="HxH.18">
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ipsa! Ex ipsam quibusdam provident quas voluptas officiis ab repellendus quo minus! Animi repudiandae, eaque totam repellendus veniam mollitia quo aliquam.</p>
     </article>
+
+    <article>
+        <h3>Title</h3>
+        <img src="img/gto6.jpg" alt="HxH.18">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ipsa! Ex ipsam quibusdam provident quas voluptas officiis ab repellendus quo minus! Animi repudiandae, eaque totam repellendus veniam mollitia quo aliquam.</p>
+    </article>
+    
+
+    <article>
+        <h3>Title</h3>
+        <img src="img/asadora7.jpg" alt="HxH.18">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ipsa! Ex ipsam quibusdam provident quas voluptas officiis ab repellendus quo minus! Animi repudiandae, eaque totam repellendus veniam mollitia quo aliquam.</p>
+    </article>
+
+    <article>
+        <h3>Title</h3>
+        <img src="img/master.jpg" alt="HxH.18">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ipsa! Ex ipsam quibusdam provident quas voluptas officiis ab repellendus quo minus! Animi repudiandae, eaque totam repellendus veniam mollitia quo aliquam.</p>
+    </article>
+
+    <article>
+        <h3>Title</h3>
+        <img src="img/gantz3.jpg" alt="HxH.18">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ipsa! Ex ipsam quibusdam provident quas voluptas officiis ab repellendus quo minus! Animi repudiandae, eaque totam repellendus veniam mollitia quo aliquam.</p>
+    </article>
+    
     
 
 </section>
