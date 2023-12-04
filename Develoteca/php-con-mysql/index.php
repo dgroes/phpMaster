13b7c605699f287989965e93c5bf0d20a5c89912
Diego Pasten <diegopasten78@gmail.com>
2023-12-04 17:20:35 -0300

    Hey

--- Develoteca/php-con-mysql/index.php
+++ Develoteca/php-con-mysql/index.php
@@ -1,6 +1,50 @@
-<?php include("template/cabecera.php");?>
-<main>
-    <h1>Bi</h1>
+<!DOCTYPE html>
+<html lang="es">
 
-</main>
-<?php include("template/pie.php"); ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand lg navbar-dark bg-primary">
+        <ul class="nav navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Develoteca</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">Inicio</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">Libros</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">Nosotros</a>
+            </li>
+        </ul>
+    </nav>
+    <section class="container">
+        <article class="row">
+
+            <div class="jumbotron">
+                <h1 class="display-3">Jumbo heading</h1>
+                <p class="lead">Jumbo helper text</p>
+                <hr class="my-2">
+                <p>More info</p>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+                </p>
+            </div>
+
+        </article>
+
+    </section>
+
+</body>
+
+</html>
---
+++ conserjeria/includes/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conserjería</title>
+    <link rel="stylesheet" href="style.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <a href="#">Mirador de los Andes</a>
+            <a href="index.php">Residentes</a>
+            <a href="productos.php">Visitas</a>
+            <a href="nosotros.php">Vehiculos</a>
+        </nav>
+    </header>
