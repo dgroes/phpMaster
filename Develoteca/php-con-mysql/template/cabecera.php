721bba56127e97ad2b6de30dc846e0ed5877daa9
Diego Pasten <diegopasten78@gmail.com>
2023-12-04 17:52:20 -0300

    Header de la página.

---
+++ Develoteca/php-con-mysql/template/cabecera.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand lg navbar-dark bg-primary">
+        <ul class="nav navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Develoteca</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Inicio</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="productos.php">Libros</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="nosotros.php">Nosotros</a>
+            </li>
+        </ul>
+    </nav>
+    <div class="container">
+        <br/><br/>
+        <div class="row">
