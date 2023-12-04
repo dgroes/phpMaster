94f0a446cc8a02012911257e1cd66690de2f498d
Diego Pasten <diegopasten78@gmail.com>
2023-12-04 17:51:09 -0300

    Sección de productos de la página.

---
+++ Develoteca/php-con-mysql/productos.php
@@ -0,0 +1,53 @@
+<?php include("template/cabecera.php"); ?>
+
+<dvi class="col-md-3">
+
+    <div class="card">
+        <img class="card-img-top" src="img/pluto1.jpg" alt="">
+        <div class="card-body">
+            <h4 class="card-title">Pluto (1)</h4>
+            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver Más</a>
+
+        </div>
+    </div>
+
+</dvi>
+<dvi class="col-md-3">
+
+    <div class="card">
+        <img class="card-img-top" src="img/pluto2.jpg" alt="">
+        <div class="card-body">
+            <h4 class="card-title">Pluto (2)</h4>
+            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver Más</a>
+
+        </div>
+    </div>
+
+</dvi>
+<dvi class="col-md-3">
+
+    <div class="card">
+        <img class="card-img-top" src="img/pluto3.jpg" alt="">
+        <div class="card-body">
+            <h4 class="card-title">Pluto (3)</h4>
+            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver Más</a>
+
+        </div>
+    </div>
+
+</dvi>
+<dvi class="col-md-3">
+
+    <div class="card">
+        <img class="card-img-top" src="img/pluto4.jpg" alt="">
+        <div class="card-body">
+            <h4 class="card-title">Pluto (4)</h4>
+            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver Más</a>
+
+        </div>
+    </div>
+
+</dvi>
+
+
+<?php include("template/pie.php"); ?>
