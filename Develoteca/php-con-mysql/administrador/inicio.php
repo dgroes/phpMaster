f65e8181ae89b4a4ccf0c68a6e72cbf0bd1863f9
Diego Pasten <diegopasten78@gmail.com>
2023-12-05 19:44:21 -0300

    Se actualiza la cabecerá

--- Develoteca/php-con-mysql/administrador/inicio.php
+++ Develoteca/php-con-mysql/administrador/inicio.php
@@ -1,47 +1,19 @@
-<!doctype html>
-<html lang="en">
+<?php include ("template/cabecera.php"); ?>
+<div class="col-md-12">
 
-<head>
-    <title>Inicio</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
-
-<body>
-    <nav class="navbar navbar-expand navbar-light bg-light">
-        <div class="nav navbar-nav">
-            <a class="nav-item nav-link active" href="#">Administrador<span class="sr-only">(current)</span></a>
-            <a class="nav-item nav-link" href="#">Inicio</a>
-            <a class="nav-item nav-link" href="#">Libros</a>
-            <a class="nav-item nav-link" href="#">Cerrar</a>
-            <a class="nav-item nav-link" href="#">Ver sitio WEB</a>
-        </div>
-    </nav>
-
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-
-                <div class="jumbotron">
-                    <h1 class="display-3">INICIO</h1>
-                    <p class="lead">Jumbo helper text</p>
-                    <hr class="my-2">
-                    <p>More info</p>
-                    <p class="lead">
-                        <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
-                    </p>
-                </div>
-
-            </div>
-
-        </div>
+    <div class="jumbotron">
+        <h1 class="display-3">INICIO</h1>
+        <p class="lead">Jumbo helper text</p>
+        <hr class="my-2">
+        <p>More info</p>
+        <p class="lead">
+            <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+        </p>
     </div>
 
+</div>
 
-</body>
+</div>
+</div>
 
-</html>
+<?php include ("template/pie.php"); ?>
---
+++ Develoteca/php-con-mysql/administrador/template/cabecera.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Inicio</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<?php $url = "http://" . $_SERVER['HTTP_HOST'] . "/phpmaster/develoteca/php-con-mysql" ?>
+
+<body>
+    <nav class="navbar navbar-expand navbar-light bg-light">
+        <div class="nav navbar-nav">
+            <a class="nav-item nav-link active" href="#">Administrador<span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="<?php echo $url; ?>/administrador/inicio.php">Inicio</a>
+            <a class="nav-item nav-link" href="<?php echo $url;?>/administrador/seccion/productos.php">Libros</a>
+            <a class="nav-item nav-link" href="<?php echo $url;?>/administrador/seccion/cerrar.php">Cerrar</a>
+            <a class="nav-item nav-link" href="<?php echo $url; ?>">Ver sitio WEB</a>
+        </div>
+    </nav>
+
+    <div class="container">
+        <br><br>
+        <div class="row">
