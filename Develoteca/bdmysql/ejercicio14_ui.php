28229061924c23a04c379eb5b40f6abecf18169e
Diego Pasten <diegopasten78@gmail.com>
2023-12-11 10:42:26 -0300

    Inferfaz HTML del ejercicio14.

---
+++ Develoteca/bdmysql/ejercicio14_ui.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        Nombre de la Tarea:
+        <input type="text" name="tarea" id="tarea">
+        <br>
+        <input type="submit" value="Agregar">
+    </form>
+    <table>
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Tarea</th>
+                <th>Accion</th>
+            </tr>
+        </thead>
+        <tr>
+            <td>2</td>
+            <td>Aprendiend PHP con MySQLi</td>
+            <td>[BORAR]</td>
+        </tr>
+    </table>
+
+</body>
+
+</html>
