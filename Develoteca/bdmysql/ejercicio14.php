5e6280d0b3b216f2055f9fa1ac4c501a5637bc73
Diego Pasten <diegopasten78@gmail.com>
2023-12-11 13:12:03 -0300

    DELETE registro de una tabla por medio de un <form> en HTML.

--- Develoteca/bdmysql/ejercicio14.php
+++ Develoteca/bdmysql/ejercicio14.php
@@ -10,6 +10,7 @@ if ($conexion->connect_error) {
     die("No se conectó :(..." . $conexion->connect_error);
 }
 
+//Agregar una Tarea
 echo "Archivo para ingresar tareas" . "<br>";
 if ($_POST) {
     /* print_r($_POST); */
@@ -19,3 +20,16 @@ if ($_POST) {
     $stmt->execute();
     echo "Sea ha insertado un registro a la base de datos.";
 }
+
+//Borrar una tarea
+if ($_GET) {
+    $id = $_GET['id'];
+    $stmt = $conexion->prepare("DELETE FROM tarea WHERE id=?");
+    $stmt->bind_param("s", $id);
+    $stmt->execute();
+}
+
+//Leer registros de la tabla tarea
+$sql = "SELECT * FROM tarea ORDER BY id ASC";
+$resultado  = $conexion->query($sql);
+$datos = $resultado->fetch_all();
