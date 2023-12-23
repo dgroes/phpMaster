59c7278207a1f487b7b2ebe3fc0ba735e4e09222
Diego Pasten <diegopasten78@gmail.com>
2023-12-23 19:27:05 -0300

    SELECT con WHERE y ORDER BY con PDO.2

---
+++ Develoteca/bdmysql/pdo_ejercicio10.php
@@ -0,0 +1,23 @@
+<?php
+$server = "localhost";
+$user = "root";
+$password = "";
+$database = "pdodeveloteca";
+
+try {
+    $connection = new PDO("mysql:host=$server; dbname=$database", $user, $password);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connection Established..." . "<br>";
+
+    $sentencia = $connection->prepare("SELECT * FROM students WHERE id > 6 ORDER BY name ASC");
+    $sentencia->execute();
+    $sentencia->setFetchMode(PDO::FETCH_ASSOC);
+    $resultado = ($sentencia->fetchAll());
+
+    foreach ($resultado as $registro) {
+        echo "id: " . $registro['id'] . ": " . $registro['name'] . "  " .  $registro['email']  . "<br>";
+    }
+} catch (PDOException $error) {
+
+    echo "ERROR: " . $error->getMessage();
+}
