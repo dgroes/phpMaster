31d2e350ce5a9eb1e89e5e513a952dbd6bb8c610
Diego Pasten <diegopasten78@gmail.com>
2023-12-25 19:18:29 -0300

    Actualización de fichero.

--- Develoteca/apptodo/agregarTarea.php
+++ Develoteca/apptodo/agregarTarea.php
@@ -2,7 +2,25 @@
 
 try {
     $conn = new PDO('mysql:host=localhost;dbname=todolist', 'root', '');
-    echo "Conexión establecida.<br/>";
 } catch (PDOException $e) {
-    echo "error de conexión: " . $e->getMessage();
+    echo "error de conexión";
 }
+
+if(isset($_POST['agregar_tarea'])){
+    $tarea = ($_POST['tarea']);
+    $sql = "INSERT INTO tareas (tarea) VALUE (?)";
+    $sentencia = $conn->prepare($sql);
+    $sentencia->execute([$tarea]);
+}
+
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+    $sql = "DELETE FROM tareas WHERE id=?";
+    $sentencia=$conn->prepare($sql);
+    $sentencia->execute([$id]);
+}
+
+
+$sql = "SELECT * FROM tareas ";
+$registros = $conn->query($sql);
+
