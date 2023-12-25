bbe3384369337c07b0a91234dddd61ead1fb8db7
Diego Pasten <diegopasten78@gmail.com>
2023-12-25 14:43:44 -0300

    TRY CATCH para saber si la conexín establecida con la BD está funcionando.

--- Develoteca/apptodo/agregarTarea.php
+++ Develoteca/apptodo/agregarTarea.php
@@ -1,4 +1,8 @@
-<?php 
+<?php
 
-
-?>
+try {
+    $conn = new PDO('mysql:host=localhost;dbname=todolist', 'root', '');
+    echo "Conexión establecida.<br/>";
+} catch (PDOException $e) {
+    echo "error de conexión: " . $e->getMessage();
+}
