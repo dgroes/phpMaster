7780f9561036d01c572e6670db9a0f72a2719f3d
Diego Pasten <diegopasten78@gmail.com>
2023-12-29 19:29:29 -0300

    Finalización de APP TODO LIST con MySQL PDO.

--- Develoteca/apptodo/agregarTarea.php
+++ Develoteca/apptodo/agregarTarea.php
@@ -6,21 +6,28 @@ try {
     echo "error de conexión";
 }
 
-if(isset($_POST['agregar_tarea'])){
+if (isset($_POST['id'])) {
+    $id = $_POST['id'];
+    $completado = (isset($_POST['completado']))?1:0;
+    $sql = "UPDATE tareas SET completado=? WHERE id=?";
+    $sentencia = $conn->prepare($sql);
+    $sentencia->execute([$completado,$id]);
+}
+
+if (isset($_POST['agregar_tarea'])) {
     $tarea = ($_POST['tarea']);
     $sql = "INSERT INTO tareas (tarea) VALUE (?)";
     $sentencia = $conn->prepare($sql);
     $sentencia->execute([$tarea]);
 }
 
-if(isset($_GET['id'])){
+if (isset($_GET['id'])) {
     $id = $_GET['id'];
     $sql = "DELETE FROM tareas WHERE id=?";
-    $sentencia=$conn->prepare($sql);
+    $sentencia = $conn->prepare($sql);
     $sentencia->execute([$id]);
 }
 
 
 $sql = "SELECT * FROM tareas ";
 $registros = $conn->query($sql);
-
