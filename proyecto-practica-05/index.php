fa370f34a4339d34bc235e07de0fc051966bbb31
Diego Pasten <diegopasten78@gmail.com>
2023-12-30 21:39:51 -0300

    Se agregan el campo de Form para el llenado de registro y de Table para la lectra de los registros.

--- proyecto-practica-05/index.php
+++ proyecto-practica-05/index.php
@@ -1,11 +1,76 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-    
+    <header>
+        <h1>Mirador de Los Andes</h1>
+    </header>
+    <section>
+        <article>
+            <h2>Registro</h2>
+            <form action="">
+                <label for="GET-name">Departamento:</label>
+                <input id="GET-name" type="text" name="name" />
+                <label for="GET-name">Nombre:</label>
+                <input id="GET-name" type="text" name="name" />
+                <label for="GET-name">Rut:</label>
+                <input id="GET-name" type="text" name="name" />
+                <label for="GET-name">Matricula:</label>
+                <input id="GET-name" type="text" name="name" />
+                <label for="GET-name">Marca:</label>
+                <input id="GET-name" type="text" name="name" />
+                <label for="GET-name">Llegada:</label>
+                <input id="GET-name" type="text" name="name" />
+                <label for="GET-name">Salida:</label>
+                <input id="GET-name" type="text" name="name" />
+                <input type="submit" value="Registrar" />
+            </form>
+        </article>
+        <article>
+            <h2>Historial</h2>
+            <table>
+                <table>
+                    <thead>
+                        <tr>
+                            <th colspan="7">Visitas</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Llegada</td>
+                            <td>Depto</td>
+                            <td>Nombre</td>
+                            <td>Rut</td>
+                            <td>Matricula</td>
+                            <td>Marca</td>
+                            <td>Salida</td>
+                        </tr>
+                        <tr>
+                            <td>09:23</td>
+                            <td>403</td>
+                            <td>Andrés Lopez</td>
+                            <td>19.432.739-k</td>
+                            <td>HGDZ32</td>
+                            <td>Chevrolet</td>
+                            <td>14:57</td>
+                        </tr>
+
+                    </tbody>
+                </table>
+
+
+            </table>
+        </article>
+
+    </section>
+
 </body>
+
 </html>
