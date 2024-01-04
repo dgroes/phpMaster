fb9b0ac56585cc364b0af4b24aed1e32354da32d
Diego Pasten <diegopasten78@gmail.com>
2024-01-03 21:11:56 -0300

    Se actualiza el código, primer avance el proyecto completado.

--- proyecto-practica-05/index.php
+++ proyecto-practica-05/index.php
@@ -1,3 +1,5 @@
+<?php include "quieries.php" ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,59 +15,77 @@
         <h1>Mirador de Los Andes</h1>
     </header>
     <section>
-        <article>
+        <article class="registro">
             <h2>Registro</h2>
-            <form action="">
-                <label for="GET-name">Departamento:</label>
-                <input id="GET-name" type="text" name="name" />
-                <label for="GET-name">Nombre:</label>
-                <input id="GET-name" type="text" name="name" />
-                <label for="GET-name">Rut:</label>
-                <input id="GET-name" type="text" name="name" />
-                <label for="GET-name">Matricula:</label>
-                <input id="GET-name" type="text" name="name" />
-                <label for="GET-name">Marca:</label>
-                <input id="GET-name" type="text" name="name" />
-                <label for="GET-name">Llegada:</label>
-                <input id="GET-name" type="text" name="name" />
-                <label for="GET-name">Salida:</label>
-                <input id="GET-name" type="text" name="name" />
-                <input type="submit" value="Registrar" />
+            <form action="" method="post">
+                <label for="fecha">Fecha:</label>
+                <input type="date" name="fecha" id="fecha" required>
+                <br>
+
+                <label for="hora_llegada">Hora de Llegada:</label>
+                <input type="time" name="hora_llegada" id="hora_llegada" required>
+                <br>
+
+                <label for="depto">Departamento:</label>
+                <input type="text" name="depto" id="depto" required>
+                <br>
+
+                <label for="nombre">Nombre:</label>
+                <input type="text" name="nombre" id="nombre" required>
+                <br>
+
+                <label for="run">Run:</label>
+                <input type="text" name="run" id="run" required>
+                <br>
+
+                <label for="matricula">Matricula:</label>
+                <input type="text" name="matricula" id="matricula">
+                <br>
+
+                <label for="marca">Marca:</label>
+                <input type="text" name="marca" id="marca">
+                <br>
+
+                <label for="parentesco">Parentesco:</label>
+                <input type="text" name="parentesco" id="parentesco">
+                <br>
+
+                <input type="submit" value="Agregar">
             </form>
+
         </article>
-        <article>
-            <h2>Historial</h2>
+
+        <article class="visitas">
             <table>
-                <table>
-                    <thead>
+                <thead>
+                    <tr>
+                        <th colspan="8">Visitas</th>
+                    </tr>
+                    <tr>
+                        <th colspan="1">Fecha</th>
+                        <th colspan="1">Hora Llegada</th>
+                        <th colspan="1">Hora Salida</th>
+                        <th colspan="1">Depto</th>
+                        <th colspan="1">Run</th>
+                        <th colspan="1">Matriula</th>
+                        <th colspan="1">Marca</th>
+                        <th colspan="1">Parentesco</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($date as $dato) { ?>
                         <tr>
-                            <th colspan="7">Visitas</th>
+                            <td><?php echo $dato['fecha']; ?></td>
+                            <td><?php echo $dato['hora_llegada']; ?></td>
+                            <td><?php echo $dato['hora_salida']; ?></td>
+                            <td><?php echo $dato['depto']; ?></td>
+                            <td><?php echo $dato['id_persona']; ?></td>
+                            <td><?php echo $dato['id_vehiculo']; ?></td>
+                            <td><?php echo $dato['marca']; ?></td>
+                            <td><?php echo $dato['parentesco']; ?></td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Llegada</td>
-                            <td>Depto</td>
-                            <td>Nombre</td>
-                            <td>Rut</td>
-                            <td>Matricula</td>
-                            <td>Marca</td>
-                            <td>Salida</td>
-                        </tr>
-                        <tr>
-                            <td>09:23</td>
-                            <td>403</td>
-                            <td>Andrés Lopez</td>
-                            <td>19.432.739-k</td>
-                            <td>HGDZ32</td>
-                            <td>Chevrolet</td>
-                            <td>14:57</td>
-                        </tr>
-
-                    </tbody>
-                </table>
-
-
+                    <?php } ?>
+                </tbody>
             </table>
         </article>
 
