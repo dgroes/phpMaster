c84085a3541dda1bf8290afc4b9b816e48a0046d
Diego Pasten <diegopasten78@gmail.com>
2024-01-08 16:51:57 -0300

    Separación de código con require.

---
+++ proyecto-php/includes/cabecera.php
@@ -0,0 +1,50 @@
+<?php require_once 'conexion.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proyecto PHP</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <!-- CABECERA -->
+    <header id="cabecera">
+        <!-- LOGO -->
+        <div id="logo">
+            <a href="index.php">
+                Blog de videojuegos
+            </a>
+        </div>
+
+
+        <!-- MENÚ -->
+        <nav id="menu">
+            <ul>
+                <li>
+                    <a href="index.php">Categoria 1</a>
+                </li>
+                <li>
+                    <a href="index.php">Categoria 2</a>
+                </li>
+                <li>
+                    <a href="index.php">Categoria 3</a>
+                </li>
+                <li>
+                    <a href="index.php">Categoria 4</a>
+                </li>
+                <li>
+                    <a href="index.php">Sobre Mi</a>
+                </li>
+                <li>
+                    <a href="index.php">Contacto</a>
+                </li>
+            </ul>
+
+        </nav>
+
+        <div class="clearfix"></div>
+    </header>
+    <div id="contenedor">
