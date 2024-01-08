8934ae236880046af7d2ebee62bcf7be1d10657d
Diego Pasten <diegopasten78@gmail.com>
2024-01-08 16:51:13 -0300

    Primera parte maquetación terminada.

---
+++ proyecto-php/index.php
@@ -0,0 +1,47 @@
+<?php require_once 'includes/cabecera.php' ?>
+<?php require_once 'includes/lateral.php'; ?>
+
+<!-- CAJA PRINCIPAL -->
+<div id="principal">
+    <h1>Ultimas entradas</h1>
+    <article class="entrada">
+        <a href="">
+            <h2>Titulo de mi entrada</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste delectus, suscipit labore necessitatibus praesentium voluptate temporibus, quasi blanditiis a ipsa soluta vero est optio, ab consequatur asperiores magnam libero officia.
+            </p>
+        </a>
+    </article>
+
+    <article class="entrada">
+        <a href="">
+            <h2>Titulo de mi entrada</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste delectus, suscipit labore necessitatibus praesentium voluptate temporibus, quasi blanditiis a ipsa soluta vero est optio, ab consequatur asperiores magnam libero officia.
+            </p>
+        </a>
+    </article>
+    <article class="entrada">
+        <a href="">
+            <h2>Titulo de mi entrada</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste delectus, suscipit labore necessitatibus praesentium voluptate temporibus, quasi blanditiis a ipsa soluta vero est optio, ab consequatur asperiores magnam libero officia.
+            </p>
+        </a>
+    </article>
+
+    <article class="entrada">
+        <a href="">
+            <h2>Titulo de mi entrada</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste delectus, suscipit labore necessitatibus praesentium voluptate temporibus, quasi blanditiis a ipsa soluta vero est optio, ab consequatur asperiores magnam libero officia.
+            </p>
+        </a>
+    </article>
+
+    <div id="ver-todas">
+        <a href="">Ver todas las entradas</a>
+    </div>
+
+</div> <!-- FIN PRINCIPAL -->
+<?php require_once 'includes/pie.php'; ?>
