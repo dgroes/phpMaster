d932dc4a77849e9b0b553c48924635bf0c2b9f0d
Diego Pasten <diegopasten78@gmail.com>
2024-01-08 10:51:16 -0300

    Moví proyectos a la carpeta proyectos-de-practica

--- proyecto-practica/includes/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="pie">
-    <p>Desarrollado por Diego Pastén &copy; 2023</p>
-</footer>
-</body>
-
-</html>
--- proyecto-practica/index.php
+++
@@ -1,8 +0,0 @@
-<!-- Include del Header -->
-<?php require_once 'includes/header.php'; ?>
-
-<!-- Include del Section -->
-<?php require_once 'includes/section.php'; ?>
-
-<!-- Include del Footer -->
-<?php require_once 'includes/footer.php'; ?>
