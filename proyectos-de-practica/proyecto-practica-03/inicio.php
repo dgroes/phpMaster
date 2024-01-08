777209452152aba922581019e1cde532d4c1bb7a
Diego Pasten <diegopasten78@gmail.com>
2024-01-08 10:46:56 -0300

    Se cambia la ruta del proyecto de /phpmaster a proyectos-de-practica.

---
+++ proyectos-de-practica/proyecto-practica-03/inicio.php
@@ -0,0 +1,29 @@
+<?php
+
+//CONFIRMAR SESION
+session_start();
+if (!isset($_SESSION['loggedin'])) {
+    header('Location: index.html');
+    exit;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
+    integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+
+</body>
+
+</html>
