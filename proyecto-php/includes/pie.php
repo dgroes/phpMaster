e7506323f76a3f40d37ed204a083fb5a7d4de54d
Diego Pasten <diegopasten78@gmail.com>
2024-01-08 16:52:28 -0300

    Separación de código con require .

---
+++ proyecto-php/includes/pie.php
@@ -0,0 +1,12 @@
+<div class="clearfix"></div>
+
+</div> <!-- FIN DEL CONTENEDOR -->
+
+<!-- PIE DE PÁGINA -->
+<footer id="pie">
+    <p>Desarrollado por Diego Pastén Uribe &copy; 2024</p>
+</footer>
+
+</body>
+
+</html>
