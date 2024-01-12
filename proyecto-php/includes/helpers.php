6c6d48f31482c46df2f03ad05d2c36fa333e4703
Diego Pasten <diegopasten78@gmail.com>
2024-01-12 11:50:21 -0300

    Se mejora la función borrarErrores, agregando nuevas popciones en base a los tipos de error que pueden surgir.

--- proyecto-php/includes/helpers.php
+++ proyecto-php/includes/helpers.php
@@ -13,8 +13,22 @@ function mostrarError($errores, $campo)
 
 function borrarErrores()
 {
-    $_SESSION['errores'] = null;
-    session_unset();
+    $borrado = false;
 
-    return true;  // o podrías devolver directamente session_unset()
+    if (isset($_SESSION['errores'])) {
+        $_SESSION['errores'] = null;
+        $borrado = true;
+    }
+
+    if (isset($_SESSION['errores_entrada'])) {
+        $_SESSION['errores_entrada'] = null;
+        $borrado = true;
+    }
+
+    if (isset($_SESSION['completado'])) {
+        $_SESSION['completado'] = null;
+        $borrado = true;
+    }
+
+    return $borrado;
 }
