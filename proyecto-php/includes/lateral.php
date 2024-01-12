79cddb315f13950dee5f110fd7117da1a806b95d
Diego Pasten <diegopasten78@gmail.com>
2024-01-12 11:48:28 -0300

    Se agregea Mostrar errores en el lateral.

--- proyecto-php/includes/lateral.php
+++ proyecto-php/includes/lateral.php
@@ -1,23 +1,34 @@
 <!-- BARRA LATERAL -->
 <?php require_once 'includes/helpers.php'; ?>
 <aside id="sidebar">
-    <div id="login" class="bloque">
-        <h3>Identificate</h3>
-        <form action="login.php" method="post">
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email">
+	<div id="login" class="bloque">
+		<h3>Identificate</h3>
+		<form action="login.php" method="post">
+			<label for="email">Email</label>
+			<input type="email" name="email" id="email">
 
-            <label for="password">Contraseña</label>
-            <input type="password" name="password" id="password">
+			<label for="password">Contraseña</label>
+			<input type="password" name="password" id="password">
 
-            <input type="submit" value="Entrar">
-        </form>
-    </div>
+			<input type="submit" value="Entrar">
+		</form>
+	</div>
 
 
-    <div id="register" class="bloque">
-        <h3>Registrate</h3>
-        <form action="registro.php" method="POST"> 
+	<div id="register" class="bloque">
+		<h3>Registrate</h3>
+		<!-- Mostrar errores -->
+		<?php if (isset($_SESSION['completado'])) : ?>
+			<div class="alerta alerta-exito">
+				<?= $_SESSION['completado'] ?>
+			</div>
+		<?php elseif (isset($_SESSION['errores']['general'])) : ?>
+			<div class="alerta alerta-error">
+				<?= $_SESSION['errores']['general'] ?>
+			</div>
+		<?php endif; ?>
+
+		<form action="registro.php" method="POST">
 			<label for="nombre">Nombre</label>
 			<input type="text" name="nombre" />
 			<?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'nombre') : ''; ?>
@@ -37,5 +48,5 @@
 			<input type="submit" name="submit" value="Registrar" />
 		</form>
 		<?php borrarErrores(); ?>
-    </div>
+	</div>
 </aside>
