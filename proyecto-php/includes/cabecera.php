2253adad217fbc0a49748d116e43159e694b6459
Diego Pasten <diegopasten78@gmail.com>
2024-01-13 15:21:01 -0300

    Se agrega require de helpers.php, además se agrega un while el cual lista las categorias de video juegos dentro del nav.

--- proyecto-php/includes/cabecera.php
+++ proyecto-php/includes/cabecera.php
@@ -1,4 +1,5 @@
 <?php require_once 'conexion.php'; ?>
+<?php require_once 'includes/helpers.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -24,16 +25,21 @@
         <nav id="menu">
             <ul>
                 <li>
-                    <a href="index.php">Categoria 1</a>
+                    <a href="index.php">Inicio</a>
                 </li>
-                <li>
-                    <a href="index.php">Categoria 2</a>
-                </li>
-                <li>
-                    <a href="index.php">Categoria 3</a>
-                </li>
-                <li>
-                    <a href="index.php">Categoria 4</a>
+                <?php
+                $categorias = conseguirCategorias($db);
+                if (!empty($categorias)) :
+                    while ($categoria = mysqli_fetch_assoc($categorias)) :
+                ?>
+                        <li>
+                            <a href="categoria.php?id=<?= $categoria['id'] ?>"><?= $categoria['nombre'] ?></a>
+                        </li>
+                <?php
+                    endwhile;
+                endif;
+                ?>
+
                 </li>
                 <li>
                     <a href="index.php">Sobre Mi</a>
