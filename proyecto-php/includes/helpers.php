9c959fd317044d465e491de036fd2d940b1fb527
Diego Pasten <diegopasten78@gmail.com>
2024-01-18 09:34:32 -0300

    Se modifica la consulta de la funcion conseguirUltimasEntradas, se cambia: 'c*', por: 'c.nombre AS 'categoria', esto para mejorar la lectura de los datos y para poder llamas los datos faltantes en index.php en la etiqueta span clase fecha.

--- proyecto-php/includes/helpers.php
+++ proyecto-php/includes/helpers.php
@@ -49,7 +49,7 @@ function conseguirCategorias($conexion)
 
 function conseguirUltimasEntradas($conexion)
 {
-    $sql = "SELECT e.*, c.* FROM entradas e INNER JOIN categorias c ON e.categoria_id = c.id 
+    $sql = "SELECT e.*, c.nombre AS 'categoria' FROM entradas e INNER JOIN categorias c ON e.categoria_id = c.id 
     ORDER BY e.id DESC LIMIT 4;";
 
     $entradas = mysqli_query($conexion, $sql);
