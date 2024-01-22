deca163d34acb6133a454a74180df72b0b8c75a5
Diego Pasten <diegopasten78@gmail.com>
2024-01-22 10:19:42 -0300

    Se actualiza la function Conseguir Entradas, agregando nuevos parametros para poder hacer la busqueda de una entrada, agregando además un if de busqueda, este cambio está relacionado con buscar.php.}

--- proyecto-php/includes/helpers.php
+++ proyecto-php/includes/helpers.php
@@ -61,25 +61,34 @@ function conseguirCategoria($conexion, $id)
 
 function conseguirEntrada($conexion, $id)
 {
-	$sql = "SELECT e.*, c.nombre AS 'categoria' FROM entradas e " .
+	$sql = "SELECT e.*, c.nombre AS 'categoria', CONCAT(u.nombre, ' ', u.apellidos) AS usuario"
+		. " FROM entradas e " .
 		"INNER JOIN categorias c ON e.categoria_id = c.id " .
-		"WHERE e.id = '$id'";
+		"INNER JOIN usuarios u ON e.usuario_id = u.id " .
+		"WHERE e.id = $id";
 	$entrada = mysqli_query($conexion, $sql);
+
 	$resultado = array();
 	if ($entrada && mysqli_num_rows($entrada) >= 1) {
 		$resultado = mysqli_fetch_assoc($entrada);
 	}
+
 	return $resultado;
 }
 
 
-function conseguirEntradas($conexion, $limit  = null, $categoria = null)
+function conseguirEntradas($conexion, $limit  = null, $categoria = null, $busqueda = null)
 {
 	$sql = "SELECT e.*, c.nombre AS 'categoria' FROM entradas e " .
 		"INNER JOIN categorias c ON e.categoria_id = c.id ";
 	if (!empty($categoria)) {
 		$sql .= "WHERE e.categoria_id = $categoria ";
 	}
+
+	if (!empty($busqueda)) {
+		$sql .= "WHERE e.titulo LIKE '%$busqueda%' ";
+	}
+
 	$sql  .= " ORDER BY e.id DESC";
 	if ($limit) {
 		// $sql = $sql." LIMIT 4";
