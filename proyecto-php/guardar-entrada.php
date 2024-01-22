28cdc4c602b8c5ec3ad423a6400a49a192880679
Diego Pasten <diegopasten78@gmail.com>
2024-01-22 10:15:52 -0300

    Se odifica la edentación del fichero.

--- proyecto-php/guardar-entrada.php
+++ proyecto-php/guardar-entrada.php
@@ -1,44 +1,54 @@
 <?php
-if (isset($_POST)) {
-    //Conexión a la Base de Datos
-    require_once 'includes/conexion.php';
 
-    $titulo = isset($_POST['titulo']) ? mysqli_real_escape_string($db, $_POST['titulo']) : false;
-    $descripcion = isset($_POST['descripcion']) ? mysqli_real_escape_string($db, $_POST['descripcion']) : false;
-    $categoria = isset($_POST['categoria']) ? (int)$_POST['categoria'] : false;
-    $usuario = $_SESSION['usuario']['id'];
+if(isset($_POST)){
+	
+	require_once 'includes/conexion.php';
+	
+	$titulo = isset($_POST['titulo']) ? mysqli_real_escape_string($db, $_POST['titulo']) : false;
+	$descripcion = isset($_POST['descripcion']) ? mysqli_real_escape_string($db, $_POST['descripcion']) : false;
+	$categoria = isset($_POST['categoria']) ? (int)$_POST['categoria'] : false;
+	$usuario = $_SESSION['usuario']['id'];
+	
+	// Validación
+	$errores = array();
+	
+	if(empty($titulo)){
+		$errores['titulo'] = 'El titulo no es válido';
+	}
+	
+	if(empty($descripcion)){
+		$errores['descripcion'] = 'La descripción no es válida';
+	}
+	
+	if(empty($categoria) && !is_numeric($categoria)){
+		$errores['categoria'] = 'La categoría no es válida';
+	}
+	
+	
+	if(count($errores) == 0){
+		if(isset($_GET['editar'])){
+			$entrada_id = $_GET['editar'];
+			$usuario_id = $_SESSION['usuario']['id'];
+			
+			$sql = "UPDATE entradas SET titulo='$titulo', descripcion='$descripcion', categoria_id=$categoria ".
+					" WHERE id = $entrada_id AND usuario_id = $usuario_id";
 
-    //ARRAY DE ERRORES:
-    //Almacenar los errores en un array es una buena práctica, de esta manera se manejarían los errores fácilmente y presentar dichos errores al usuario de una manera coherente
-    $errores = array();
+		}else{
+			$sql = "INSERT INTO entradas VALUES(null, $usuario, $categoria, '$titulo', '$descripcion', CURDATE());";
+		}
+		$guardar = mysqli_query($db, $sql);
 
+		header("Location: index.php");
+	}else{
 
-    //VALIDAR LOS DATOS ANTES DE GUARDARLOS EN LA BASE DE DATOS:
-    //La condición del if define si no está vaciom no es numérico y no contiene ningún dígito. en $nombre, solo así el dato es valido.
-
-    // VALIDAR LOS DATOS ANTES DE GUARDARLOS EN LA BASE DE DATOS:
-    if (empty($titulo)) {
-        $errores['titulo'] = 'El titulo no es válido';
-    }
-
-    if (empty($descripcion)) {
-        $errores['descripcion'] = 'La descripción no es válida';
-    }
-
-    // Cambiar la condición para validar la categoría
-    if (!is_numeric($categoria) || $categoria <= 0) {
-        $errores['categoria'] = 'La categoría no es válida';
-    }
-    /* var_dump($errores);
-    die(); */
-
-
-    if (count($errores) == 0) {
-        $sql = "INSERT INTO entradas VALUES(NULL, '$usuario', '$categoria',  '$titulo', '$descripcion', CURDATE());";
-        $guardar = mysqli_query($db, $sql);
-        header("Location: index.php");
-    } else {
-        $_SESSION['errores_entrada'] = $errores;
-        header("Location: crear-entradas.php");
-    }
+		$_SESSION["errores_entrada"] = $errores;
+		
+		if(isset($_GET['editar'])){
+			header("Location: editar-entrada.php?id=".$_GET['editar']);
+		}else{
+			header("Location: crear-entradas.php");
+		}
+	}
+	
 }
+
