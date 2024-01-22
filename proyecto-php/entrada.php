bead30af2b5bd6e27df008e91c5d74b932de8497
Diego Pasten <diegopasten78@gmail.com>
2024-01-22 10:15:20 -0300

    Se modifica el fichero.

--- proyecto-php/entrada.php
+++ proyecto-php/entrada.php
@@ -14,11 +14,21 @@ if (!isset($entrada_actual['id'])) {
 <div id="principal">
 
     <h1><?= $entrada_actual['titulo'] ?></h1>
+
     <a href="categoria.php?id=<?= $entrada_actual['categoria_id'] ?>">
         <h2><?= $entrada_actual['categoria'] ?></h2>
     </a>
+    <h4><?= $entrada_actual['fecha'] ?> | <?= $entrada_actual['usuario'] ?></h4>
+    <p>
+        <?= $entrada_actual['descripcion'] ?>
+    </p>
 
-    <h4><?= $entrada_actual['fecha'] ?></h4>
-    <p><?= $entrada_actual['descripcion'] ?></p>
+    <?php if (isset($_SESSION["usuario"]) && $_SESSION['usuario']['id'] == $entrada_actual['usuario_id']) : ?>
+        <br />
+        <a href="editar-entrada.php?id=<?= $entrada_actual['id'] ?>" class="boton boton-verde">Editar entrada</a>
+        <a href="borrar-entrada.php?id=<?= $entrada_actual['id'] ?>" class="boton">Eliminar entrada</a>
+    <?php endif; ?>
 
-    <?php require_once 'includes/pie.php'; ?>
+</div> <!--fin principal-->
+
+<?php require_once 'includes/pie.php'; ?>
