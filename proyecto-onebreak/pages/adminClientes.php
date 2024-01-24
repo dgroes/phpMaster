76b6011f0b0c6f163e959685e3642e2796bc1073
Diego Pasten <diegopasten78@gmail.com>
2024-01-24 00:23:37 -0300

    Fichero de administación de clientes.

---
+++ proyecto-onebreak/pages/adminClientes.php
@@ -0,0 +1,40 @@
+<article>
+    <h3>Administración de clientes</h3>
+    <p>Contenido de la página de administración de clientes...</p>
+    <form action="">
+        <div class="form-bloque-izquierda">
+            <label for="run">Run</label>
+            <input type="text" name="run" id="run">
+
+            <label for="razonSocial">Razón Social</label>
+            <input type="text" name="razonSocial" id="">
+
+            <label for="nombreContacto">Nombre de Contacto</label>
+            <input type="text" name="nombreContacto">
+
+            <label for="mailContacto">Mail de Contacto</label>
+            <input type="email" name="mailContacto" id="">
+
+            <label for="direccion">Dirección</label>
+            <input type="text" name="direccion" id="">
+
+            <label for="telefono">Teléfono</label>
+            <input type="tel" name="telefono" id="">
+
+            <label for="actividad">Actividad</label>
+            <input type="text" name="actividad">
+
+            <label for="tipo">Tipo</label>
+            <input type="text" name="tipo">
+            <button type="submit">Guardar</button>
+        </div>
+        <div class="form-bloque-derecha">
+            <label for="buscar-run">Buscar por Run</label>
+            <input type="text" name="buscar-run" id="">
+            <button type="submit">Buscar</button>
+
+            <button type="button">Borrar</button>
+            <button type="button">Mostrar Lista</button>
+        </div>
+    </form>
+</article>
