531aac143ecb9d4479db1c4b0bd45d332418a75b
Diego Pasten <diegopasten78@gmail.com>
2024-01-24 00:24:27 -0300

    Fichero de administración de Contratos.

---
+++ proyecto-onebreak/pages/adminContratos.php
@@ -0,0 +1,44 @@
+<article>
+    <h3>Administración de contratos</h3>
+    <p>Contenido de la página de administración de contratos...</p>
+    <form action="">
+        <div class="form-bloque-izquierda">
+            <label for="run">Numero de Contrato</label>
+            <input type="text" name="run" id="run">
+
+            <label for="razonSocial">Creación</label>
+            <input type="text" name="razonSocial" id="">
+
+            <label for="nombreContacto">Termino</label>
+            <input type="text" name="nombreContacto">
+
+            <label for="mailContacto">Fecha y Hora de Inicio</label>
+            <input type="email" name="mailContacto" id="">
+
+            <label for="direccion">Fecha y Hora de Término</label>
+            <input type="text" name="direccion" id="">
+
+            <label for="telefono">Dirección</label>
+            <input type="tel" name="telefono" id="">
+
+            <label for="actividad">Estado de Vigencia</label>
+            <input type="text" name="actividad">
+
+            <label for="tipo">Tipo</label>
+            <input type="text" name="tipo">
+
+            <label for="tipo">Observaciones</label>
+            <input type="text" name="tipo">
+
+            <button type="submit">Guardar</button>
+        </div>
+        <div class="form-bloque-derecha">
+            <label for="buscar-run">Buscar por Numero de Contrato</label>
+            <input type="text" name="buscar-run" id="">
+            <button type="submit">Buscar</button>
+
+            <button type="button">Borrar</button>
+            <button type="button">Listar Contratos</button>
+        </div>
+    </form>
+</article>
