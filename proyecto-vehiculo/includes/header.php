930e8133293bdee2eb9ff62152bb8abe8bf69c9f
Diego Pasten <diegopasten78@gmail.com>
2024-01-27 15:57:31 -0300

    Header del sistema.

---
+++ proyecto-vehiculo/includes/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Examen Tranversal</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Silkscreen&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+    <section class="container">
+        <article class="form-container">
+            <h2>Procesos</h2>
+            <a href="?page=agregar">Agregar</a>
+            <a href="?page=listar">Listar</a>
