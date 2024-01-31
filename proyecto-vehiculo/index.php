ce860011de9a6ff0bbaabc67fe9de693e2eb9ed1
Diego Pasten <diegopasten78@gmail.com>
2024-01-30 23:01:41 -0300

    Se agrega la pagina pages/buscar.php a la varible .

--- proyecto-vehiculo/index.php
+++ proyecto-vehiculo/index.php
@@ -1,19 +1,16 @@
 <?php require_once 'includes/header.php'; ?>
-        
+
 <?php
 // Obtener la página solicitada
 $page = isset($_GET['page']) ? $_GET['page'] : 'agegar';
 
 // Validar y cargar la página correspondiente
-$allowedPages = ['crear-registro', 'listar'];
+$allowedPages = ['crear-registro', 'listar', 'buscar'];
 if (in_array($page, $allowedPages)) {
     include("pages/{$page}.php");
 } else {
     include("pages/crear-registro.php");
 }
 
-/* $allowedPages = ['home', 'adminClientes', 'listadoClientes', 'adminContratos', 'listadoContratos', 'usuario'];: Aquí se crea un array llamado $allowedPages que contiene los nombres de las páginas que están permitidas. Este array se utiliza para validar si la página solicitada es válida.*/
-
-/* if (in_array($page, $allowedPages)) {: Esta es una estructura condicional (if). Verifica si el valor de $page se encuentra en el array $allowedPages. Si es cierto, significa que la página solicitada es válida y se ejecuta el bloque de código dentro de las llaves {}.*/
 ?>
 <?php require_once 'includes/footer.php'; ?>
