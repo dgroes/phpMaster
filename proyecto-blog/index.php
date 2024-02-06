cd1393242e09aba1eac7753e062b7f0bddc7217a
Diego Pasten <diegopasten78@gmail.com>
2024-02-06 19:55:57 -0300

    Se separa el Header y el Footer en la carpeta includes.

---
+++ proyecto-blog/index.php
@@ -0,0 +1,132 @@
+<?php require_once 'includes/header.php'; ?>
+
+<article class="post">
+    <div class="user-info">
+        <div class="user-info-izquierda">
+            <img src="images/users/disk.png" alt="Nombre del Usuario">
+
+            <p class="username">dirtyMan</p>
+        </div>
+        <div class="user-info-derecha">
+            <p class="post-date">04/02/2024</p>
+
+        </div>
+
+    </div>
+    <hr class="separador">
+    <h2>Kid A</h2>
+    <img class="media" src="images/media/kidA.png" alt="">
+    <p class="descripcion">Kid A is the fourth studio album by the English rock band Radiohead, released on 2 October 2000 by
+        Parlophone.
+        It was recorded with their producer, Nigel Godrich, in Paris, Copenhagen, Gloucestershire and
+        Oxfordshire.
+    </p>
+    <div class="like-dislike-section">
+        <button class="like-btn">Like</button>
+        <button class="dislike-btn">Dislike</button>
+        <p class="like-count">10 Likes</p>
+    </div>
+</article>
+
+
+<article class="post">
+
+    <div class="user-info">
+        <div class="user-info-izquierda">
+            <img src="images/users/logan.png" alt="Nombre del Usuario">
+
+            <p class="username">logan98c</p>
+        </div>
+        <div class="user-info-derecha">
+            <p class="post-date">05/02/2024</p>
+
+        </div>
+    </div>
+
+    <hr class="separador">
+    <h2>A Beautiful Lie by 30 Seconds to Mars considered a Post Hardcore Album?</h2>
+    <p class="descripcion">30STM’s first three albums— which I all love to pieces and are some of my favorite albums ever— are
+        primarily progressive rock, but A Beautiful Lie has a LOT of post-hardcore influence and some of the
+        songs on it are totally post-hardcore, like “The Kill”. They were a prog band but loved to experiment by
+        mixing in other rock genres.
+
+        I’ve always thought the self titled album was progressive metal, A Beautiful Lie was prog-pxh and This
+        Is War is just prog. Love Lust Faith + Dreams is electronic rock and still listenable while a huge step
+        down from the first three and America is hot garbage and the musical equivalent of getting a dead rat or
+        used condom in the mail.
+    </p>
+    <div class="like-dislike-section">
+        <button class="like-btn">Like</button>
+        <button class="dislike-btn">Dislike</button>
+        <p class="like-count">46 Likes</p>
+    </div>
+</article>
+
+<article class="post">
+
+    <div class="user-info">
+        <div class="user-info-izquierda">
+            <img src="images/users/guts.png" alt="Nombre del Usuario">
+
+            <p class="username">behelit</p>
+        </div>
+        <div class="user-info-derecha">
+            <p class="post-date">06/02/2024</p>
+
+        </div>
+    </div>
+
+    <hr class="separador">
+    <h2>Attempt to reproduce this universe that I loved so much 💛</h2>
+    <img class="media" src="images/users/sable.png" alt="">
+    <div class="like-dislike-section">
+        <button class="like-btn">Like</button>
+        <button class="dislike-btn">Dislike</button>
+        <p class="like-count">9 Likes</p>
+    </div>
+</article>
+
+<article class="post">
+
+    <div class="user-info">
+        <div class="user-info-izquierda">
+            <img src="images/users/20.png" alt="Nombre del Usuario">
+
+            <p class="username">CharacterRant</p>
+        </div>
+        <div class="user-info-derecha">
+            <p class="post-date">05/02/2024</p>
+
+        </div>
+    </div>
+
+    <hr class="separador">
+    <h2 class="titulo">The manga 20th Century Boys is a masterpiece and one of the best examples of how to perfectly pull off a
+        complex story</h2>
+    <p class="descripcion">For those who don't know, 20th Century Boys is a manga written by Naoki Urasawa and divided into 250
+        chapters. It can be labeled as a thriller, but as it often happens with the best stories, it delves into
+        other stuff as well, namely some slight sci-fi and supernatural elements, with even some slice of life
+        thrown in.
+        The premise is that in 1997 a young man named Kenji discovers the existence of a mysterious conspiracy
+        cult that seems to be related to a game he came up with when he was a kid with his friends. The
+        conspiracy seems to be responsible for crimes such as disappearances and the release of a dangerous
+        virus, and is led by a mysterious figure named "Friend" whose identity is unknown.
+        The story however gets really convoluted, since the point of the manga is that it's full of techniques
+        such as timeskips, flashbacks and flashforwards, for an overarching plot that spans from 1969 to 2018.
+        It also follow the pov of different characters, all of whom are written extremely well.
+        Urasawa does a phenomenal job at constantly bombarding the reader with plot points and questions, and at
+        the same time answering previously hanging questions. This way the reader can feel a strong sense of
+        mystery and confusion (in an engaging way) while at the same time keeping track of what's going on. It's
+        a brilliant achievement that many authors fail at, if you ask me.
+        The other brilliant thing about 20CB is that everything makes sense at the end. Even some minor plot
+        points that the reader might have forgot about are resolved perfectly.
+    </p>
+    <div class="like-dislike-section">
+        <button class="like-btn">Like</button>
+        <button class="dislike-btn">Dislike</button>
+        <p class="like-count">6 Likes</p>
+    </div>
+</article>
+
+
+<?php require_once 'includes/footer.php'; ?>
