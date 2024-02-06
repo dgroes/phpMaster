c16f3a3c7a07bfdc258fc7273196ade9d2373a8e
Diego Pasten <diegopasten78@gmail.com>
2024-02-06 19:57:38 -0300

    Header del sitio.

---
+++ proyecto-blog/includes/header.php
@@ -0,0 +1,25 @@
+<?php require_once 'config/conexion.php';?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proyecto Blog</title>
+    <link rel="icon" href="images/logo/logo.gif" type="image/x-icon">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <header id="titulo">
+        <a href="index.php" class="logo">
+            <img src="images/logo/logo.gif" alt="logo">
+            <h2 class="name-page">Switzer</h2>
+        </a>
+        <nav>
+            <a href="index.php" class="nav-link">Inicio</a>
+            <a href="" class="nav-link">Categoria</a>
+            <a href="login.php" class="nav-ling">Iniciar Sesión</a>
+        </nav>
+    </header>
+    <section class="container">
