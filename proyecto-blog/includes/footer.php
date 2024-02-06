e0dd35a49f2c8a3e3fc557bf521792f40201f024
Diego Pasten <diegopasten78@gmail.com>
2024-02-06 19:57:59 -0300

    Footer del sitio.

---
+++ proyecto-blog/includes/footer.php
@@ -0,0 +1,13 @@
+</section>
+
+<footer>
+    <section>
+        <a href="#titulo">Ir al comienzo</a>
+        <a href="mailto:diegopasten78@gmail.com">Contacto</a>
+    </section>
+    <p>Copyrigh 2024</p>
+</footer>
+
+</body>
+
+</html>
