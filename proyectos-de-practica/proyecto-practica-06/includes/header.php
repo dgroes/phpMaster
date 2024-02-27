279a40829ba6fe95aaea707ea7a3adc83847fd4c
Diego Pasten <diegopasten78@gmail.com>
2024-02-27 14:08:00 -0300

    Header del proyecto.

---
+++ proyectos-de-practica/proyecto-practica-06/includes/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+
+    <title>Cirice</title>
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- _FUENTES_ -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
+        rel="stylesheet">
+
+
+</head>
+
+<body>
+    <!-- Here is our main header that is used across all the pages of our website -->
+
+    <header>
+        <h1>Cirice</h1>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="register.php">Registrarse</a>
+            <a href="login.php">Inicar Sesión</a>
+            <!-- A Search form is another common non-linear way to navigate through a website. -->
+
+            <form>
+                <input type="search" name="q" placeholder="Search query" />
+                <input type="submit" value="Go!" />
+            </form>
+        </nav>
+    </header>
+
+
+
+    <!-- Here is our page's main content -->
+    <section class="container">
