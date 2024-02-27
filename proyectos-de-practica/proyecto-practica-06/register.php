4e92f74486f59ae079b3514f77ce233254faaab0
Diego Pasten <diegopasten78@gmail.com>
2024-02-27 14:02:48 -0300

    Fichero del registro de la parte del cliente.

---
+++ proyectos-de-practica/proyecto-practica-06/register.php
@@ -0,0 +1,22 @@
+<?php require_once 'includes/header.php' ?>
+
+<form class="register" method="POST" action="controllers/authController.php">
+    <h2>Registrarse</h2>
+    <div class="input-group">
+        <label for="nombre">Nombre de usuario</label>
+        <input type="text" name="nombre" id="nombre">
+
+    </div>
+    <div class="input-group">
+        <label for="email">Email</label>
+        <input type="email" id="email"  name="email">
+
+    </div>
+    <div class="input-group">
+        <label for="password">Contraseña</label>
+        <input type="password" name="password" id="password" required>
+
+    </div>
+    
+    <button type="submit">Registrar</button>
+</form>
