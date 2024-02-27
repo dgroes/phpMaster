d3c2c3e41bfff68fda7364ffdacb5ed5fb024050
Diego Pasten <diegopasten78@gmail.com>
2024-02-27 14:07:41 -0300

    Footer del proyecto.

---
+++ proyectos-de-practica/proyecto-practica-06/includes/footer.php
@@ -0,0 +1,10 @@
+</section>
+
+<!-- And here is our main footer that is used across all the pages of our website -->
+
+<footer>
+    <p>©Copyright 2050 by nobody. All rights reversed.</p>
+</footer>
+</body>
+
+</html>
