2b0bb90bff3a94440c1dd682ee80c5bec01995ab
Diego Pasten <diegopasten78@gmail.com>
2024-02-27 14:02:12 -0300

    Fichero login de parte del cliente para logearse.

---
+++ proyectos-de-practica/proyecto-practica-06/login.php
@@ -0,0 +1,18 @@
+<?php require_once 'includes/header.php' ?>
+
+<form class="register">
+    <h2>Iniciar Sesión</h2>
+    <div class="input-group">
+        <label>Nombre de usuario</label>
+        <input type="text" required>
+
+    </div>
+
+    <div class="input-group">
+        <label>Contraseña</label>
+        <input type="password" required>
+
+    </div>
+   
+    <button type="submit">Iniciar</button>
+</form>
