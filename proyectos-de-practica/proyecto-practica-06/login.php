7f34566e9e85167c9f6abbd6721ddb079cba784d
Diego Pasten <diegopasten78@gmail.com>
2024-02-28 23:04:14 -0300

    Se agregan los atributos correspondientes de los labels e inputs.

--- proyectos-de-practica/proyecto-practica-06/login.php
+++ proyectos-de-practica/proyecto-practica-06/login.php
@@ -1,18 +1,18 @@
 <?php require_once 'includes/header.php' ?>
 
-<form class="register">
+<form class="register" method="POST" action="controllers/loginController.php">
     <h2>Iniciar Sesión</h2>
     <div class="input-group">
-        <label>Nombre de usuario</label>
-        <input type="text" required>
+        <label for="nombre">Nombre de usuario</label>
+        <input type="text" name="nombre" id="nombre">
 
     </div>
 
     <div class="input-group">
-        <label>Contraseña</label>
-        <input type="password" required>
+        <label for="password">Contraseña</label>
+        <input type="password" name="password" id="password">
 
     </div>
-   
+
     <button type="submit">Iniciar</button>
 </form>
