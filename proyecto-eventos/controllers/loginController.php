3334783b914d0a652b895721854b27982532abb4
Diego Pasten <diegopasten78@gmail.com>
2024-02-29 20:27:56 -0300

    Controlador del login.}

---
+++ proyecto-eventos/controllers/loginController.php
@@ -0,0 +1,41 @@
+<?php
+
+//Iniciar la Sesión y la conexión a la BD:
+require_once '../includes/conexion.php';
+
+//Recoger los datos del Formulario:
+if (isset($_POST)) {
+
+    //Borrar errores antiguos
+    if (isset($_SESSION['error_login'])) {
+        unset($_SESSION['error_login']);
+    }
+
+    //Recoger datos del formulario por POST:
+    $email = trim($_POST['email']);
+    $contrasena = $_POST['contrasena'];
+
+    //Consulta para combrobar si existen la credenciales del usuario:
+    $sql = "SELECT * FROM usuarios WHERE email = '$email'";
+    $login = mysqli_query($db, $sql);
+
+    if ($login && mysqli_num_rows($login) == 1) {
+        $usuario = mysqli_fetch_assoc($login);
+
+        //Comprobar la contraseña:
+        $verify = password_verify($contrasena, $usuario['contrasena']);
+
+        if ($verify) {
+            //Utilizar una SESIÓN para Guardar los datos del usuario logeado:
+            $_SESSION['usuario'] = $usuario;
+        } else {
+            $_SESSION['error_login'] = "Login incorrecto!";
+        }
+    } else {
+        //Mensaje de error
+        $_SESSION['error_login'] = "Login Incorrecto!";
+    }
+}
+
+//Redirigir al index
+header('Location: ../index.php');
