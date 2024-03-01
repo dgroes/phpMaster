6299819d1b5c9a35ded0d173f6e6b405a2bd3707
Diego Pasten <diegopasten78@gmail.com>
2024-03-01 20:15:21 -0300

    Se crea el controlador de el registro de un evento.

---
+++ proyecto-eventos/controllers/crearEventoController.php
@@ -0,0 +1,93 @@
+<?php
+if (isset($_POST)) {
+    require_once '../includes/conexion.php';
+
+    //Recoger los valores del formulario de creación de eventos:
+    $titulo = isset($_POST['titulo']) ? mysqli_real_escape_string($db, $_POST['titulo']) : false;
+    $descripcion = isset($_POST['descripcion']) ? mysqli_real_escape_string($db, $_POST['descripcion']) : false;
+    $fecha = isset($_POST['fecha']) ? mysqli_real_escape_string($db, $_POST['fecha']) : false;
+    $hora = isset($_POST['hora']) ? mysqli_real_escape_string($db, $_POST['hora']) : false;
+    $ubicacion = isset($_POST['ubicacion']) ? mysqli_real_escape_string($db, $_POST['ubicacion']) : false;
+    $organizador = $_SESSION['usuario']['id'];
+
+    //Array de errores:
+    $errores = array();
+
+    //Validación del Titulo:
+    if (!empty($titulo)) {
+        $titulo_validado = true;
+    } else {
+        $titulo_validado = false;
+        $errores['titulo'] = "El Nombre Del Titulo No Es Valido";
+    }
+
+    //Validación de la Descripción
+    if (!empty($descripcion)) {
+        $descripcion_valido = true;
+    } else {
+        $descripcion_valido = false;
+        $errores['descripcion'] = "La Descripción Del Evento No Es Valido";
+    }
+
+    //Validación de Fecha:
+    if (!empty($fecha)) {
+
+        //Obtener la Fecha Actual
+        $fecha_actual = date('Y-m-d');
+
+        //Calcular la fecha mínima permitida para el evento (2 días después de la fecha actual):
+        $fecha_minima = date('Y-m-d', strtotime('+2 days', strtotime($fecha_actual)));
+
+        //Verificar si la fecha del evento es posterior a la fecha mínima permitida:
+        if ($fecha >= $fecha_minima) {
+            $fecha_valido = true;
+        } else {
+            $fecha_valido = false;
+            $errores['fecha'] = "La fecha del evento debe ser como mínimo 2 días después de la fecha actual";
+        }
+    } else {
+        $fecha_valido = false;
+        $errores['fecha'] = "La Fecha Del Evento No Es Valdia";
+    }
+
+    //Validación de Hora:
+    if (!empty($hora)) {
+        $hora_valido = true;
+    } else {
+        $hora_valido = false;
+        $errores['hora'] = "La Hora no es valida";
+    }
+
+    //Validación de Ubicación:
+    if (!empty($ubicacion)) {
+        $ubicacion_valido = true;
+    } else {
+        $ubicacion_valido = false;
+        $errores['ubicacion'] = "La ubicación ingresada no es valida";
+    }
+
+    //Insertar en la BD:
+    if (count($errores) == 0 && $organizador == $_SESSION['usuario']['id']) {
+        $sql = "INSERT INTO eventos VALUES(null, '$titulo', '$descripcion', '$fecha', '$hora', '$ubicacion', '$organizador');";
+        $guardar = mysqli_query($db, $sql);
+
+        if ($guardar) {
+            $_SESSION['completado'] = "El registro se ha completado con éxito";
+            // Redirigir solo después de procesar el formulario
+            header('Location: ../?page=crear_evento');
+            exit; // Salir del script después de la redirección
+        } else {
+            $_SESSION['errores']['evento'] = "Fallo al guardar el evento";
+        }
+    } else {
+        $_SESSION['errores'] = $errores;
+    }
+}
+
+// Redirigir a crear_evento.php si no se enviaron datos por POST
+// Redirigir solo si la página actual no es crear_evento.php para evitar bucles de redirección
+if ($_GET['page'] != 'crear_evento') {
+    header('Location: ?page=crear_evento');
+    exit; // Salir del script después de la redirección
+}
+?>
