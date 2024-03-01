1091c269386587f337922c322b4865b75c949df0
Diego Pasten <diegopasten78@gmail.com>
2024-03-01 20:07:35 -0300

    Se actualiza el fichero.

--- proyecto-eventos/index.php
+++ proyecto-eventos/index.php
@@ -1,9 +1,15 @@
 <?php require_once 'templates/header.php' ?>
 
+<?php
+    // Obtener la página solicitada
+    $page = isset($_GET['page']) ? $_GET['page'] : 'home';
 
-
-
-
-</section>
-
+    // Validar y cargar la página correspondiente
+    $allowedPages = ['home', 'login', 'logout', 'perfil', 'registro', 'notificaciones', 'crear_evento', 'calendario'];
+    if (in_array($page, $allowedPages)) {
+        include("pages/{$page}.php");
+    } else {
+        include("pages/home.php");
+    }
+    ?>
 <?php require_once 'templates/footer.php' ?>
