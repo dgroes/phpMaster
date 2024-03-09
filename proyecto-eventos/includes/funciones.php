a1a911f8f381b823ff3a053d3a67aa7b539747d9
Diego Pasten <diegopasten78@gmail.com>
2024-03-08 23:29:45 -0300

    Funcion de mostrarError y borrarError.

---
+++ proyecto-eventos/includes/funciones.php
@@ -0,0 +1,21 @@
+<?php
+
+function mostrarError($errores, $campo)
+{
+    $alerta = '';
+    if (isset($errores[$campo]) && !empty($campo)) {
+        $alerta = "<div class ='alerta-error'>" . $errores[$campo] . '</div>';
+    }
+    return $alerta;
+}
+
+function borrarErrores()
+{
+    $borrado = false;
+    if(isset($_SESSION['errores'])) {
+        $_SESSION['errores'] = null;
+        $borrado = true;
+    }
+
+    return $borrado;
+}
