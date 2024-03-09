9b80e2d8e9de4407791b5cc06176b68cab9435ce
Diego Pasten <diegopasten78@gmail.com>
2024-03-08 23:27:37 -0300

    Se agregan los alertas de errores.

--- proyecto-eventos/pages/registro.php
+++ proyecto-eventos/pages/registro.php
@@ -2,22 +2,27 @@
 
 <form class="register" method="POST" action="controllers/registerController.php">
     <h2>Registrase</h2>
+
     <div class="input-group">
         <label for="nombre">Nombre de usuario</label>
         <input type="text" name="nombre" id="nombre">
+        <?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'nombre') : ''; ?>
+
     </div>
 
     <div class="input-group">
         <label for="email">Email</label>
-        <input type="email" name="email" id="email">
+        <input type="text" name="email" id="email">
+        <?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'email') : ''; ?>
     </div>
+
 
     <div class="input-group">
         <label for="contrasena">Contraseña</label>
         <input type="password" name="contrasena" id="contrasena">
+        <?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'contrasena') : ''; ?>
     </div>
-    
-
 
     <button type="submit">Iniciar</button>
+    <?php borrarErrores(); ?>
 </form>
