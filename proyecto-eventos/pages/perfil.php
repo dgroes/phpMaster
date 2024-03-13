9f04e0951797b4b6a9b894020bd9435ad4615647
Diego Pasten <diegopasten78@gmail.com>
2024-03-12 23:44:59 -0300

    Se termina de mejorar con mensajes de errores y exito.

--- proyecto-eventos/pages/perfil.php
+++ proyecto-eventos/pages/perfil.php
@@ -1,26 +1,39 @@
 <?php require_once 'templates/header.php'; ?>
 <article>
-    <h1>Mis Datos</h1>
-    <?php if (isset($_SESSION['completado'])) : ?>
-        <div class="alerta alerta-exito">
-            <?= $_SESSION['completado'] ?>
-        </div>
-    <?php elseif (isset($_SESSION['errores']['general'])) : ?>
-        <div class="alerta alerta-error">
-            <?= $_SESSION['errores']['general'] ?>
-        </div>
-    <?php endif; ?>
+
+
 
     <form class="register" action="controllers/actualizarUsuarioController.php" method="POST">
-        <label for="nombre">Nombre de Usuario:</label>
-        <input type="text" name="nombre" id="nombre" value="<?= $_SESSION['usuario']['nombre']; ?>">
+        <h2>Mis Datos</h2>
+        <div class="input-group">
+            <label for="nombre">Nombre de Usuario:</label>
+            <input type="text" name="nombre" id="nombre" value="<?= $_SESSION['usuario']['nombre']; ?>">
+            <?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'editar_nombre') : ''; ?>
+        </div>
 
-        <label for="emil">Email</label>
-        <input type="email" name="email" id="email" value="<?= $_SESSION['usuario']['email']; ?>">
+        <div class="input-group">
 
-        <input type="submit" name="submit" value="Editar">
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" value="<?= $_SESSION['usuario']['email']; ?>">
+            <?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'editar_email') : ''; ?>
+
+        </div>
+
+        <?php if (isset($_SESSION['completado'])) : ?>
+            <div class="alerta alerta-exito">
+                <?= $_SESSION['completado'] ?>
+            </div>
+        <?php elseif (isset($_SESSION['errores']['general'])) : ?>
+            <div class="alerta alerta-error">
+                <?= $_SESSION['errores']['general'] ?>
+            </div>
+        <?php endif; ?>
+
+        <button type="submit">Editar</button>
+        <?php borrarErrores(); ?>
+
     </form>
-    
+
 </article>
 
 <?php require_once 'templates/footer.php'; ?>
