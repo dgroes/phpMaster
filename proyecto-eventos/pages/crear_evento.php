0a11677dc517d74f27f9e8584a0032a5466b3b0f
Diego Pasten <diegopasten78@gmail.com>
2024-03-12 23:45:44 -0300

    Se mejora al incluir mensajes de errores y completado.

--- proyecto-eventos/pages/crear_evento.php
+++ proyecto-eventos/pages/crear_evento.php
@@ -1,34 +1,54 @@
+<?php require_once 'controllers/redireccion.php'; ?>
 <article>
-    <h2>Crear Evento</h2>
+    <form class="register-evento" method="POST" action="controllers/crearEventoController.php">
 
-    <?php if (isset($_SESSION['completado'])) : ?>
-        <div class="alerta alerta-exito">
-            <?= $_SESSION['completado'] ?>
+
+        <h2>Crear Evento</h2>
+
+        <div class="input-group">
+            <label for="titulo">Nombre del Evento</label>
+            <input type="text" name="titulo" id="titulo">
+            <?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'titulo') : ''; ?>
         </div>
-    <?php elseif (isset($_SESSION['errores']['general'])) : ?>
-        <div class="alerta alerta-error">
-            <?= $_SESSION['errores']['general'] ?>
+
+        <div class="input-group">
+            <label for="descripcion">Descripción</label>
+            <input type="text" name="descripcion" id="descripcion">
+            <?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'descripcion') : ''; ?>
         </div>
-    <?php endif; ?>
 
-    <form class="register" method="POST" action="controllers/crearEventoController.php">
-        <label for="titulo">Nombre del Evento</label>
-        <input type="text" name="titulo" id="titulo">
+        <div class="input-group">
+            <label for="fecha">Fecha</label>
+            <input type="date" name="fecha" id="fecha">
+            <?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'fecha') : ''; ?>
+        </div>
 
-        <label for="descripcion">Descripcion</label>
-        <input type="text" name="descripcion" id="descripcion">
+        <div class="input-group">
+            <label for="hora">Hora</label>
+            <input type="time" name="hora" id="hora">
+            <?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'hora') : ''; ?>
+        </div>
 
-        <label for="fecha">Fecha</label>
-        <input type="date" name="fecha" id="fecha">
+        <div class="input-group">
+            <label for="ubicacion">Ubicación</label>
+            <input type="text" name="ubicacion" id="ubicacion">
+            <?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'ubicacion') : ''; ?>
+        </div>
 
-        <label for="hora">Hora</label>
-        <input type="time" name="hora" id="hora">
-
-        <label for="ubicacion">Ubicación</label>
-        <input type="text" name="ubicacion" id="ubicacion">
+        <?php if (isset($_SESSION['completado'])) : ?>
+            <div class="alerta-exito">
+                <?= $_SESSION['completado'] ?>
+            </div>
+        <?php elseif (isset($_SESSION['errores']['general'])) : ?>
+            <div class="alerta-error">
+                <?= $_SESSION['errores']['evento'] ?>
+            </div>
+        <?php endif; ?>
 
         <button type="submit">Crear Evento</button>
 
+        <?php borrarErrores(); ?>
+
     </form>
-   
+
 </article>
