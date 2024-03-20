8c0f25f0a4ab6ad7ba393aa827c018ed8321053c
Diego Pasten <diegopasten78@gmail.com>
2024-03-20 18:40:04 -0300

    Se agrega la función que hace un SELECT a los eventos creados en la BD.

--- proyecto-eventos/includes/funciones.php
+++ proyecto-eventos/includes/funciones.php
@@ -12,20 +12,45 @@ function mostrarError($errores, $campo)
 function borrarErrores()
 {
     $borrado = false;
-    if(isset($_SESSION['errores'])) {
+    if (isset($_SESSION['errores'])) {
         $_SESSION['errores'] = null;
         $borrado = true;
     }
 
     if (isset($_SESSION['error_login'])) {
-		$_SESSION['error_login'] = null;
-		$borrado = true;
-	}
+        $_SESSION['error_login'] = null;
+        $borrado = true;
+    }
 
     if (isset($_SESSION['completado'])) {
-		$_SESSION['completado'] = null;
-		$borrado = true;
-	}
+        $_SESSION['completado'] = null;
+        $borrado = true;
+    }
 
     return $borrado;
+}
+
+function conseguirEventos($conexion) {
+    // Ejecutar la consulta SQL
+    // $sql = "SELECT id, titulo, descripcion, CONCAT(fecha, ' ', hora) AS datetime, ubicacion, organizador_id FROM eventos";
+    $sql = "SELECT * FROM eventos";
+    $eventos = mysqli_query($conexion, $sql);
+
+    
+    // Verificar si la consulta se ejecutó correctamente
+    if (!$eventos) {
+        // Manejar el error si la consulta falla
+        echo "Error al ejecutar la consulta: " . mysqli_error($conexion);
+        return false;
+    }
+
+    // Verificar si se encontraron resultados
+    if (mysqli_num_rows($eventos) == 0) {
+        // Manejar el caso de que no haya eventos
+        echo "No se encontraron eventos.";
+        return false;
+    }
+
+    // Devolver el resultado de la consulta
+    return $eventos;
 }
