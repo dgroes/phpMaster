3a522b4eddb4bf97fc649fe91d429223d982f525
Diego Pasten <diegopasten78@gmail.com>
2024-03-20 18:45:31 -0300

    index de prueba para el fullcalendar.

---
+++ proyectos-de-practica/lost/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.5/locales-all.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.5/main.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.5/main.css"></head>
+
+    <script src="script.js"></script>
+<body>
+
+<div id="calendar"></div>
+    
+</body>
+</html>
