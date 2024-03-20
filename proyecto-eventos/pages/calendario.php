fcb47a224fb1380d43726149c33658971d7f7e6c
Diego Pasten <diegopasten78@gmail.com>
2024-03-20 18:43:37 -0300

    Fichero de la página del calendario.

--- proyecto-eventos/pages/calendario.php
+++ proyecto-eventos/pages/calendario.php
@@ -1,21 +1,7 @@
-<!DOCTYPE html>
-<html lang='en'>
-  <head>
-    <meta charset='utf-8' />
-    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.11/index.global.min.js'></script>
-    <script>
 
-      document.addEventListener('DOMContentLoaded', function() {
-        var calendarEl = document.getElementById('calendar');
-        var calendar = new FullCalendar.Calendar(calendarEl, {
-          initialView: 'dayGridMonth'
-        });
-        calendar.render();
-      });
+<article class="calendario">
 
-    </script>
-  </head>
-  <body>
-    <div id='calendar'></div>
-  </body>
-</html>
+
+  <div id="calendar" class="calendario-div"></div>
+
+</article>
