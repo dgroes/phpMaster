bfecf1787a2630dda56958414911d38381e6a4cc
Diego Pasten <diegopasten78@gmail.com>
2024-03-20 18:37:06 -0300

    Se crea fichero el cual maneja los datos de la consulta realizada en funciones.php, el cual pasa los datos a un JSON para que pueda ser leido por script.js.

---
+++ proyecto-eventos/controllers/calendarioController.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once '../includes/funciones.php';
+require_once '../includes/conexion.php';
+
+// Obtener los eventos
+$eventos = conseguirEventos($db);
+
+// Array para almacenar los eventos en formato JSON
+$eventos_json = array();
+
+// Convertir los eventos a formato JSON
+while ($evento = mysqli_fetch_assoc($eventos)) {
+    $eventos_json[] = array(
+        'id' => $evento['id'],
+        'title' => $evento['titulo'],
+        'description' => $evento['descripcion'],
+        // 'datetime' => $evento['datetime'],
+        'date' => $evento['fecha'],
+        'time' => $evento['hora'],
+        'location' => $evento['ubicacion'],
+        'organizer_id' => $evento['organizador_id']
+    );
+}
+
+// Establecer el tipo de contenido como JSON
+header('Content-Type: application/json');
+
+// Enviar los eventos como JSON al cliente
+echo json_encode($eventos_json); // Corregido el envío de $eventos_json en lugar de $eventos
