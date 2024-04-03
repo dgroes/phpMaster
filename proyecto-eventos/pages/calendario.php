fd8b918f4d56dc3633e68fc45ab03be66c0902fc
dgroes <diegopasten78@gmail.com>
2024-04-03 16:46:42 -0300

    Se completa el calendario agregando un modal, el cual se controla con el fichero script.js.

--- proyecto-eventos/pages/calendario.php
+++ proyecto-eventos/pages/calendario.php
@@ -1,7 +1,17 @@
-
 <article class="calendario">
-
-
   <div id="calendar" class="calendario-div"></div>
 
+  <!-- Modal para mostrar detalles del evento -->
+  <div id="eventoModal" class="modal">
+    <div class="modal-content">
+      <span class="close">&times;</span>
+      <h2 id="eventoTitulo"></h2>
+      <hr id="eventosSeparador">
+      <p id="eventoDescripcion"></p>
+      <p id="eventoFecha"></p>
+      <p id="eventoHora"></p>
+      <p id="eventoUbicacion"></p>
+      <p id="eventoOrganizador"></p>
+    </div>
+  </div>
 </article>
