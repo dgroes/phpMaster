89e77ac13092316b04e4a9ce4f6798a8f05c7fb7
dgroes <diegopasten78@gmail.com>
2024-04-03 16:50:00 -0300

    Se actualiza la dirección.

--- proyecto-eventos/index.php
+++ proyecto-eventos/index.php
@@ -5,7 +5,7 @@
     $page = isset($_GET['page']) ? $_GET['page'] : 'home';
 
     // Validar y cargar la página correspondiente
-    $allowedPages = ['home', 'login', 'logout', 'perfil', 'registro', 'notificaciones', 'crear_evento', 'calendario'];
+    $allowedPages = ['home', 'login', 'logout', 'perfil', 'registro', 'invitar_usuarios', 'crear_evento', 'calendario'];
     if (in_array($page, $allowedPages)) {
         include("pages/{$page}.php");
     } else {
