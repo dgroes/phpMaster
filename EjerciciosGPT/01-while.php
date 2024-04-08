fef9ec54a9dcd83ed17cdb087980cca1b4cd436b
dgroes <diegopasten78@gmail.com>
2024-04-08 18:29:42 -0400

    Nivel Medio :(

--- EjerciciosGPT/01-while.php
+++ EjerciciosGPT/01-while.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>WHILE</title>
     <!-- Centered viewport -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css" />
 </head>
@@ -26,7 +26,69 @@
         <p>
             La condición se evalúa antes de cada iteración del bucle. Si la condición es verdadera (true), el bloque de código dentro del bucle se ejecuta. Después de cada iteración, la condición se vuelve a evaluar. Si la condición sigue siendo verdadera, el bucle continúa ejecutándose. El bucle se detiene cuando la condición se evalúa como falsa (false).F
         </p>
-        <h4>Ejercicio 1</h4>
+
+        <hr>
+        <h2>Medio</h2>
+        <h4>Ejercicio 1: Medio</h4>
+        <p>
+            Escribe un programa que solicite al usuario un número y luego determine si es un número primo o no.
+        </p>
+        <form action="01-while.php" method="GET">
+            <label for="primo">Introducir número</label>
+            <input type="number" placeholder="Introducir Número para saber si es número primo o no">
+            <input type="submit" value="¿Primo o No?">
+        </form>
+        <?php
+        if (isset($_GET['primo'])) {
+
+            $primo = $_GET['primo'];
+
+            while ($i == 0) {
+                $i = $primo;
+                $primo % $i;
+                $i--;
+            }
+            /* if (){
+                echo "Es primo";
+            } else {
+                echo "No es primo";
+            } */
+        }
+
+        ?>
+
+        <h4>Ejercicio 2: Medio</h4>
+        <p>
+            Desarrolla un programa que genere la secuencia de Fibonacci hasta un número ingresado por el usuario.
+        </p>
+        <form action="01-while.php" method="GET">
+            <label for="fibo">Introducir número</label>
+            <input type="number" id="fibo" name="fibo" placeholder="Introducir número">
+            <input type="submit" value="Fibonacci">
+        </form>
+        <?php
+        if (isset($_GET['fibo'])) {
+            $fibo = $_GET['fibo'];
+            $a = 0;
+            $b = 1;
+            echo $a . " / ";
+            echo $b . " / ";
+            while ($b <= $fibo) {
+                $temp = $a + $b;
+                echo $temp . " / ";
+                $a = $b;
+                $b = $temp;
+            }
+        }
+
+
+
+        ?>
+
+
+        <hr>
+        <h2>Fácil</h2>
+        <h4>Ejercicio 1: Fácil</h4>
         <p>Ejecutar todos los números del 0 al 100</p>
         <?php
         $numero = 0;
@@ -38,7 +100,7 @@
         ?>
         <br>
 
-        <h4>Ejercicio 2</h4>
+        <h4>Ejercicio 2: Fácil</h4>
         <p>
             Escribe un programa que genere una secuencia de números pares del 2 al 20 utilizando un bucle while. Cada número de la secuencia debe imprimirse en una nueva línea.
         </p>
@@ -52,7 +114,7 @@
         ?>
         <br>
 
-        <h4>Ejercicio 3</h4>
+        <h4>Ejercicio 3: Fácil</h4>
         <p>
             Desarrolla un programa que solicite al usuario un número y luego imprima todos los números desde 1 hasta ese número ingresado.
         </p>
@@ -76,7 +138,7 @@
 
         <br>
 
-        <h4>Ejercicio 4</h4>
+        <h4>Ejercicio 4: Fácil</h4>
         <p>
             Escribe un programa que pida al usuario un número y luego imprima la tabla de multiplicar correspondiente a ese número (del 1 al 10).
         </p>
@@ -100,7 +162,7 @@
 
         <br>
 
-        <h4>Ejercicio 5</h4>
+        <h4>Ejercicio 5: Fácil</h4>
         <p>
             Crea un programa que solicite al usuario un número y luego imprima la suma de todos los números desde 1 hasta ese número ingresado.
         </p>
