c2b4886ea65626b42055fe686e6e738919625179
dgroes <diegopasten78@gmail.com>
2024-04-16 21:13:59 -0400

    Footer de la página.

---
+++ proyecto-unifile/templates/footer.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+    <div class="container">
+        <p>
+            © 2024 Todos los derechos reservados |
+            <a target="_blank" href="https://github.com/dgroes/phpMaster/tree/master/proyecto-unifile" data-tooltip="Acceso el respositorio"><i class="fa-brands fa-github"></i></a>
+            <a href="mailto:diegopasten78@gmail.com" data-tooltip="Correo de contacto"><i class="fa-regular fa-paper-plane"></i></a>
+            <a target="_blank" href="https://fontawesome.com/"data-tooltip="Font Awesome"><i class="fa-solid fa-font-awesome"></i></a>
+            <a target="_blank" href="https://picocss.com/" data-tooltip="Pico CSS" style="text-decoration: none;">✨</a>
+        </p>
+    </div>
+</footer>
+</body>
+
+</html>
