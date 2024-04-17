23b15cd209f5f14c583abc70f5cb911d55620680
dgroes <diegopasten78@gmail.com>
2024-04-17 19:27:06 -0400

    Se crea el formulario de registro, aun de manera estatica.

---
+++ proyecto-unifile/pages/registro.php
@@ -0,0 +1,26 @@
+<section class="section-login">
+    <h1 class="sub_titulo">Registrarse</h1>
+    <form class="form-login" method="post">
+        <fieldset>
+            <label>
+                Nombre de usuario
+                <input name="nombre" placeholder="nombre" autocomplete="given-name" required />
+            </label>
+            <label>
+                Email
+                <input type="email" name="email" placeholder="Email" autocomplete="email" required />
+            </label>
+            <label>
+                Contraseña
+                <input type="password" name="password" placeholder="ejemplo: contraseña1234" required />
+            </label>
+        </fieldset>
+
+        <input type="submit" value="Registrarse" />
+    </form>
+    <h3 class="sub_titulo">¿Ya tienes una cuenta?</h3>
+    <p>
+        Inicia sesión en este enlace: <a href="">¡Inicia Sesión aquí!</a>
+    </p>
+
+</section>
