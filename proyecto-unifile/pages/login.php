3ccbd29c95f16ff95cd937a1dd9ac2df618d617c
dgroes <diegopasten78@gmail.com>
2024-04-16 21:10:43 -0400

    Login de Unifile.

---
+++ proyecto-unifile/pages/login.php
@@ -0,0 +1,9 @@
+<section class="">
+    <form>
+        <fieldset role="group">
+            <input name="email" type="email" placeholder="Email" autocomplete="email" />
+            <input name="password" type="password" placeholder="Password" />
+            <input type="submit" value="Log in" />
+        </fieldset>
+    </form>
+</section>
