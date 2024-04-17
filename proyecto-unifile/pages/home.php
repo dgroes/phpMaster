a87bed8e9e0488125f8c3ef5a780f5615569b8da
dgroes <diegopasten78@gmail.com>
2024-04-16 21:10:18 -0400

    Home de la página inical cuando se accede sin un log previo.

---
+++ proyecto-unifile/pages/home.php
@@ -0,0 +1,48 @@
+<section>
+    <span class="sub_titulo">Únete a los más de 700 millones de usuarios registrados que confían en Unifile</span>
+    <p class="parrafo">
+        Fácil de utilizar, confiable, privado y seguro. No es de extrañar que Unifile sea la elección para almacenar y compartir tus archivos más importantes.
+        <br>
+        Nuestro <ins>Sistema de Gestión de Archivos </ins>es una plataforma web diseñada para ofrecer una solución completa y segura para el manejo de archivos en la Universidad XYZ. Con nuestra aplicación, los usuarios pueden cargar, organizar, compartir y colaborar en archivos de manera eficiente y sin complicaciones.
+    </p>
+    <span class="sub_titulo">¿Qué puedes hacer con Unifile?</span>
+    <div class="bordes">
+        
+        <details>
+            <summary>Almacena y protege tus archivos <i class="fa-solid fa-file-arrow-up"></i></summary>
+            <p>
+                Obtén el almacenamiento que tú y tus equipos necesitan con características de seguridad como recuperación de archivos, protección con contraseña, marcas de agua e historial de lectores.
+            </p>
+        </details>
+        <hr />
+        <details>
+            <summary>Mantén el control del contenido compartido <i class="fa-solid fa-share-nodes"></i></summary>
+            <p>
+                Los vínculos rastreables muestran cuando alguien abre un archivo compartido y cuánto tiempo estuvo interactuando con él. Además, puedes desactivar el acceso de cualquier persona en cualquier momento sin afectar los permisos de los demás.
+            </p>
+        </details>
+        <hr />
+        <details>
+            <summary>Seguridad Garantizada <i class="fa-solid fa-lock"></i></summary>
+            <p>
+                Implementamos medidas de seguridad robustas para proteger la privacidad y la integridad de los archivos y los datos de los usuarios.
+            </p>
+        </details>
+        <hr />
+        <details>
+            <summary>Organización Intuitiva <i class="fa-solid fa-folder-tree"></i></summary>
+            <p>
+                Ofrecemos opciones para organizar archivos en carpetas y categorías, facilitando la gestión y navegación.
+            </p>
+        </details>
+    </div>
+
+
+    <span class="sub_titulo">¿Cómo Empezar?</span>
+    <p class="parrafo">
+        Si eres nuevo en nuestra plataforma, regístrate ahora para comenzar a disfrutar de todas las funciones que ofrecemos. Si ya tienes una cuenta, ¡inicia sesión y explora todo lo que nuestro Sistema de Gestión de Archivos tiene para ofrecer!
+    </p>
+
+
+
+</section>
