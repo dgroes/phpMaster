601948077f6f95ce201970850dca5faba28aa18e
dgroes <diegopasten78@gmail.com>
2024-04-16 21:14:31 -0400

    Header de la página. }

---
+++ proyecto-unifile/templates/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/335ff06f37.js" crossorigin="anonymous"></script>
+    <title>Unifile</title>
+    <!-- PICO CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css" />
+    <link rel="stylesheet" href="../public/css/styles.css">
+
+
+
+
+</head>
+
+<body>
+    <header class="glass">
+        <nav>
+            <ul>
+                <li><h2><i class="fa-solid fa-folder-open"></i> Unifile </h2></li>
+            </ul>
+            <ul>
+                <li><a href="?page=login" class="secondary">Login</a></li>
+                <li>
+                    <details class="dropdown">
+                        <summary>
+                            Account
+                        </summary>
+                        <ul dir="rtl">
+                            <li><a href="#">Profile</a></li>
+                            <li><a href="#">Settings</a></li>
+                            <li><a href="#">Logout</a></li>
+                        </ul>
+                    </details>
+                </li>
+            </ul>
+        </nav>
+
+    </header>
