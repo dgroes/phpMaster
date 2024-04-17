42f337bce8f7d1a5ba062db6828eb5d4508414ee
dgroes <diegopasten78@gmail.com>
2024-04-17 19:26:14 -0400

    Se crea el formulario de Login, aun de manera estatica.

--- proyecto-unifile/pages/login.php
+++ proyecto-unifile/pages/login.php
@@ -1,9 +1,22 @@
-<section class="">
-    <form>
-        <fieldset role="group">
-            <input name="email" type="email" placeholder="Email" autocomplete="email" />
-            <input name="password" type="password" placeholder="Password" />
-            <input type="submit" value="Log in" />
+<section class="section-login">
+    <h1 class="sub_titulo">Inicia sesión</h1>
+    <p></p>
+    <form class="form-login" method="post">
+        <fieldset>
+            <label>
+                Correo Electronico
+                <input name="first_name" placeholder="ejem: usuario@correo.com" autocomplete="given-name" />
+            </label>
+            <label>
+                Contraseña
+                <input type="password" name="password" placeholder="ejem: contraseña1234"/>
+            </label>
         </fieldset>
+        <input type="submit" value="Iniciar Sesión" />
     </form>
+    <h3 class="sub_titulo">¿Aún no estás registrado?</h3>
+    <p>
+        Registrate en este enlace para poder obtener tu nueva cuenta de Unifile: <a href="">¡Registrate Aquí!</a>
+    </p>
+    
 </section>
