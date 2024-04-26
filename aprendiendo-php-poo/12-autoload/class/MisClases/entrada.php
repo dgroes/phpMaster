99c7f46fdf4f774f455924ec5b494ff8a9f0dae2
dgroes <diegopasten78@gmail.com>
2024-04-26 11:05:36 -0400

    Clase entrada  para el uso del ejemplo de 'use'

---
+++ aprendiendo-php-poo/12-autoload/class/MisClases/entrada.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace MisClases;
+
+class Entrada
+{
+    public $titulo;
+    public $fecha;
+
+    public function __construct()
+    {
+        $this->titulo = "Review del GAT VI";
+        $this->fecha = "5 de Abril 2025";
+    }
+}
