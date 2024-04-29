34386a5382a662f7177e11d56607f7de3637a7d0
dgroes <diegopasten78@gmail.com>
2024-04-29 19:49:36 -0400

    Actualización

--- aprendiendo-php-poo/12-autoload/index.php
+++ aprendiendo-php-poo/12-autoload/index.php
@@ -30,13 +30,63 @@ class Principal
         $this->categoria = new Categoria();
         $this->entrada = new Entrada();
     }
+
+    // Métodos getter
+    public function getUsuario()
+    {
+        return $this->usuario;
+    }
+
+    public function getCategoria()
+    {
+        return $this->categoria;
+    }
+
+    public function getEntrada()
+    {
+        return $this->entrada;
+    }
+
+    // Métodos setter
+    public function setUsuario($usuario)
+    {
+        $this->usuario = $usuario;
+    }
+
+    public function setCategoria($categoria)
+    {
+        $this->categoria = $categoria;
+    }
+
+    public function setEntrada($entrada)
+    {
+        $this->entrada = $entrada;
+    }
 }
 
 //Objeto principal
 $principal = new Principal();
-var_dump($principal->usuario);
+// var_dump($principal->usuario);
+
+$metodos = (get_class_methods($principal));
+
+
+$busqueda = array_search('setApellido', $metodos);
+
+echo "<pre>";
+var_dump($busqueda);
+echo "</pre>";
 
 //Objeto de otro paquete
-$usuario = new UsuarioAdmin();
+/* $usuario = new UsuarioAdmin();
 
 var_dump($usuario);
+ */
+
+//__Comprobar si existe una clase__
+$clase = @class_exists("PanelAdministrador\Usuario");
+if ($clase) {
+    echo "<h3>La clase si existe!</h3>";
+} else {
+    echo "<h3>La clase no existe :(</h3>";
+}
