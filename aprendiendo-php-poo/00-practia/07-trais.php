b4f9a738fb73c7422d865390b9214e3256f3572b
dgroes <diegopasten78@gmail.com>
2024-05-04 18:03:10 -0400

    Trails en Objetos

---
+++ aprendiendo-php-poo/00-practia/07-trais.php
@@ -0,0 +1,6 @@
+<?php 
+//Encabezado: Clase Registro Descripción: Crea una clase Registro que utilice un trait para agregar funcionalidades de registro de fecha y hora a las instancias de la clase. Utiliza métodos mágicos para registrar automáticamente la fecha y hora de creación de cada instancia.
+
+class Registro {
+    
+}
