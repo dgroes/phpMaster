444f46665ef444b9af3fd46605b94f0fae9c8e87
dgroes <diegopasten78@gmail.com>
2024-05-07 10:33:06 -0400

    Ejercicio utilizando Traits.

--- aprendiendo-php-poo/00-practia/07-trais.php
+++ aprendiendo-php-poo/00-practia/07-trais.php
@@ -1,6 +1,30 @@
-<?php 
+<?php
 //Encabezado: Clase Registro Descripción: Crea una clase Registro que utilice un trait para agregar funcionalidades de registro de fecha y hora a las instancias de la clase. Utiliza métodos mágicos para registrar automáticamente la fecha y hora de creación de cada instancia.
 
-class Registro {
-    
+trait Utilidades
+{
+    public function mostrarFechaHora()
+    {
+        echo "La fecha es: " . date('d-m-Y H:i:s');
+    }
 }
+
+class Registro
+{
+    public string $id;
+
+    public function __construct()
+    {
+        // Genera un ID único para cada instancia
+        $this->id = uniqid();
+    }
+
+    // Usa el trait Utilidades
+    use Utilidades;
+}
+
+// Crea una instancia de Registro
+$registro = new Registro();
+
+// Muestra la fecha y hora asociada con la instancia
+$registro->mostrarFechaHora();
