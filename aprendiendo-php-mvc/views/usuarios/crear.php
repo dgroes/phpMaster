459b3bb99174bf7157a271fe059d83ef150e915d
dgroes <diegopasten78@gmail.com>
2024-05-09 17:33:47 -0400

    Formulario de usuario, es irrelevante para el proyecto de practica.

---
+++ aprendiendo-php-mvc/views/usuarios/crear.php
@@ -0,0 +1,10 @@
+<form action="">
+    <label for="Nombre"></label>
+    <input type="text" placeholder="Apellido">
+
+    <label for="Apellido"></label>
+    <input type="text" placeholder="Nombre">
+    
+</form>
+
+<?php
