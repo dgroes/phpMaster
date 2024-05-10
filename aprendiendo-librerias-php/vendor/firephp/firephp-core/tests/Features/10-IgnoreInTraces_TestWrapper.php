c39f6c9b2c6e8ffdec1f27d458273f81293bbbc8
dgroes <diegopasten78@gmail.com>
2024-05-10 12:25:52 -0400

    Eliminar archivos en vendor/firephp/firephp-core

--- aprendiendo-librerias-php/vendor/firephp/firephp-core/tests/Features/10-IgnoreInTraces_TestWrapper.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-class MyFirePHPWrapper2 extends FirePHP_TestWrapper {
-
-    public function mytrace ($message) {
-        return $this->trace($message);
-    }
-}
