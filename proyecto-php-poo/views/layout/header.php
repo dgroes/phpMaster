4d7586eb9a6ebbb9ada4279df2d7fc9c0a850adc
dgroes <diegopasten78@gmail.com>
2024-05-21 11:05:28 -0400

    Header del proyecto (Actualizado en base a los cursos 266, 267 y 268)

---
+++ proyecto-php-poo/views/layout/header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda de Camisetas</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+    <div id="container">
+        <!-- CABECERA -->
+        <header id="header">
+            <div id="logo">
+                <img src="assets/img/camiseta.png" alt="Camiseta Logo" />
+                <a href="index.php">
+                    Tienda de camisetas
+                </a>
+            </div>
+        </header>
+
+        <!-- MENU -->
+        <nav id="menu">
+            <ul>
+                <li>
+                    <a href="#">Inicio</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 1</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 2</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 3</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 4</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 5</a>
+                </li>
+            </ul>
+        </nav>
+
+        <div id="content">
