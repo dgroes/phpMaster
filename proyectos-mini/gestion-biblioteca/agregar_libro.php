97c3aaea55dc33fc9d8748d7d7baf235e4c0b52c
dgroes <diegopasten78@gmail.com>
2024-05-21 10:03:15 -0400

    Página agregar Libro

---
+++ proyectos-mini/gestion-biblioteca/agregar_libro.php
@@ -0,0 +1,16 @@
+<?php require_once 'header.php'; ?>
+<form method="POST" action="">
+    <fieldset>
+        <input type="text" name="Titulo" placeholder="Titulo" aria-label="Titulo" />
+        <input type="text" name="Autor" placeholder="Autor" aria-label="Autor" />
+        <input type="text" name="Genero" placeholder="Genero" aria-label="Genero" />
+    </fieldset>
+
+    <input type="submit" value="Agregar Libro" />
+</form>
+</main>
+</body>
+
+
+
+</html>
