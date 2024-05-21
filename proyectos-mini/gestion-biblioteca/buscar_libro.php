1f0ffbc8ba37fcba9f7f0ca3bf3b9427156741e7
dgroes <diegopasten78@gmail.com>
2024-05-21 10:02:55 -0400

    A

---
+++ proyectos-mini/gestion-biblioteca/buscar_libro.php
@@ -0,0 +1,7 @@
+<?php require_once 'header.php'; ?>
+<h3>Bucar libro o autor</h3>
+<form role="search">
+
+    <input name="search" type="search" placeholder="Libro / Autor" />
+    <input type="submit" value="Buscar" />
+</form>
