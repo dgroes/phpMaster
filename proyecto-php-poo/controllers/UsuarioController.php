3b1cec6f110717fefd1728878c0176cee840d1f3
dgroes <diegopasten78@gmail.com>
2024-05-21 13:35:03 -0400

    Se cambia la primera letra de la clase por una mayuscula

--- proyecto-php-poo/controllers/UsuarioController.php
+++ proyecto-php-poo/controllers/UsuarioController.php
@@ -1,9 +1,16 @@
 <?php
 
-class usuarioController
+class UsuarioController
 {
     public function index()
     {
         echo "Controlador Usuarios, Accion Index";
     }
+
+    public function registro()
+    {
+        require_once 'views/usuario/registro.php';
+    }
+
+    public function save(){}
 }
