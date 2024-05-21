e3378c35e29b42a0873b2e286535612c344f9480
dgroes <diegopasten78@gmail.com>
2024-05-21 18:02:49 -0400

    Creaicón del fichero con las constantes del proyecto, en base a la seción 67 del curso de Udemy

---
+++ proyecto-php-poo/config/parameters.php
@@ -0,0 +1,7 @@
+<?php
+
+//Para que el formateo limpiio de las URLs funcinen será importante que en el servidor de apache esté activado el "rewrite_module"
+define("base_url", "http://localhost/phpMaster/proyecto-php-poo/"); //Importante que la ruta completa del localhost sea correcta.
+define("controller_default", "productoController");
+define("action_default", "index");
+
