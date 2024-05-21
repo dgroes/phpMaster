5ea719779c7bfa40fe5f463442b017645a3e9e67
dgroes <diegopasten78@gmail.com>
2024-05-21 11:02:46 -0400

    Index del proyecto (Actualizado en base a los cursos 266, 267 y 268)

--- proyecto-php-poo/index.php
+++ proyecto-php-poo/index.php
@@ -1,108 +1,31 @@
-<!DOCTYPE html>
-<html lang="es">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tienda de Camisetas</title>
-    <link rel="stylesheet" href="assets/css/styles.css">
-</head>
+require_once 'autoload.php';
+require_once 'views/layout/header.php';
+require_once 'views/layout/sidebar.php';
 
-<body>
-    <div id="container">
-        <!-- CABECERA -->
-        <header id="header">
-            <div id="logo">
-                <img src="assets/img/camiseta.png" alt="Camiseta Logo" />
-                <a href="index.php">
-                    Tienda de camisetas
-                </a>
-            </div>
-        </header>
-
-        <!-- MENU -->
-        <nav id="menu">
-            <ul>
-                <li>
-                    <a href="#">Inicio</a>
-                </li>
-                <li>
-                    <a href="#">Categoria 1</a>
-                </li>
-                <li>
-                    <a href="#">Categoria 2</a>
-                </li>
-                <li>
-                    <a href="#">Categoria 3</a>
-                </li>
-                <li>
-                    <a href="#">Categoria 4</a>
-                </li>
-                <li>
-                    <a href="#">Categoria 5</a>
-                </li>
-            </ul>
-        </nav>
-
-        <div id="content">
-
-            <!-- BARRA LATERAL -->
-            <aside id="lateral">
-
-                <div id="login" class="block_aside">
-                    <h3>Entrar a la web</h3>
-                    <form action="#" method="post">
-                        <label for="email">Email</label>
-                        <input type="email" name="email" />
-                        <label for="password">Contraseña</label>
-                        <input type="password" name="password" />
-                        <input type="submit" value="Enviar" />
-                    </form>
-
-                    <ul>
-                        <li><a href="#">Mis pedidos</a></li>
-                        <li><a href="#">Gestionar pedidos</a></li>
-                        <li><a href="#">Gestionar categorias</a></li>
-                    </ul>
-                </div>
-
-            </aside>
-
-            <!-- CONTENIDO CENTRAL -->
-            <div id="central">
-                <h1>Productos destacados</h1>
-
-                <div class="product">
-                    <img src="assets/img/camiseta.png" />
-                    <h2>Camiseta Azul Ancha</h2>
-                    <p>30 euros</p>
-                    <a href="" class="button">Comprar</a>
-                </div>
-
-                <div class="product">
-                    <img src="assets/img/camiseta.png" />
-                    <h2>Camiseta Azul Ancha</h2>
-                    <p>30 euros</p>
-                    <a href="" class="button">Comprar</a>
-                </div>
-
-                <div class="product">
-                    <img src="assets/img/camiseta.png" />
-                    <h2>Camiseta Azul Ancha</h2>
-                    <p>30 euros</p>
-                    <a href="" class="button">Comprar</a>
-                </div>
-
-            </div>
-        </div>
+if (isset($_GET['controller'])) {
+    $nombreControlador = $_GET['controller'] . 'Controller';
+} else {
+    echo "La página que buscas no existe";
+    exit();
+}
 
 
-        <!-- __PIE DE PÁGINA__ -->
-        <footer id="footer">
-            <p>Desarrollado por Diego Pastén &copy; <?= date('Y') ?></p>
-        </footer>
-    </div>
+if (isset($nombreControlador) && class_exists($nombreControlador)) {
 
-</body>
+    $controlador = new $nombreControlador();
 
-</html>
+    if (isset($_GET['action']) && method_exists($controlador, $_GET['action'])) {
+        $action = $_GET['action'];
+
+        $controlador->$action();
+    } else {
+        "La pagina no existe";
+    }
+} else {
+    echo "La página no existe";
+}
+
+
+require_once 'views/layout/footer.php';
