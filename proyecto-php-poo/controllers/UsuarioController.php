38baae0492961a7695fe56bd607d561f194ce080
dgroes <diegopasten78@gmail.com>
2024-05-21 18:01:07 -0400

    Se actualiza con: - require de modesl/usuario - Completar la función save - Actulización den base a la sección 67 del curso de Udemy

--- proyecto-php-poo/controllers/UsuarioController.php
+++ proyecto-php-poo/controllers/UsuarioController.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'models/usuario.php';
+
 class UsuarioController
 {
     public function index()
@@ -12,5 +14,34 @@ class UsuarioController
         require_once 'views/usuario/registro.php';
     }
 
-    public function save(){}
+    public function save()
+    {
+        if (isset($_POST)) {
+
+            $nombre = isset($_POST['nombre']) ? $_POST['nombre'] : false;
+            $apellidos = isset($_POST['apellidos']) ? $_POST['apellidos'] : false;
+            $email = isset($_POST['email']) ? $_POST['email'] : false;
+            $password = isset($_POST['password']) ? $_POST['password'] : false;
+
+            if ($nombre && $apellidos && $email && $password) {
+                $usuario = new Usuario();
+                $usuario->setNombre($nombre);
+                $usuario->setApellidos($apellidos);
+                $usuario->setEmail($email);
+                $usuario->setPassword($password);
+
+                $save = $usuario->save();
+                if ($save) {
+                    $_SESSION['register'] = "complete";
+                } else {
+                    $_SESSION['register'] = "failed";
+                }
+            } else {
+                $_SESSION['register'] = "failed";
+            }
+        } else {
+            $_SESSION['register'] = "failed";
+        }
+        header("Location:" . base_url . 'usuario/registro');
+    }
 }
