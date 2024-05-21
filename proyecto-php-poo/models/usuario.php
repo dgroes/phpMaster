ce70063e6b306b11d2aa15f7b245ee8e2925b30a
dgroes <diegopasten78@gmail.com>
2024-05-21 17:58:53 -0400

    Creación del fichero en base a la sección 67 del curso de Udemy

---
+++ proyecto-php-poo/models/usuario.php
@@ -0,0 +1,107 @@
+<?php
+
+class Usuario
+{
+    private $id;
+    private $nombre;
+    private $apellidos;
+    private $email;
+    private $password;
+    private $rol;
+    private $imagen;
+    private $db; //Conexión a la BD
+
+    public function __construct()
+    {
+        $this->db = Database::connect();
+    }
+
+    // Getter y Setter para id
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    // Getter y Setter para nombre
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre($nombre)
+    {
+        $this->nombre = $this->db->real_escape_string($nombre);
+    }
+
+    // Getter y Setter para apellidos
+    public function getApellidos()
+    {
+        return $this->apellidos;
+    }
+
+    public function setApellidos($apellidos)
+    {
+        $this->apellidos = $this->db->real_escape_string($apellidos);
+    }
+
+    // Getter y Setter para email
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $this->db->real_escape_string($email);
+    }
+
+    // Getter y Setter para password
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = password_hash($this->db->real_escape_string($password), PASSWORD_BCRYPT, ['cost' => 4]);
+    }
+
+    // Getter y Setter para rol
+    public function getRol()
+    {
+        return $this->rol;
+    }
+
+    public function setRol($rol)
+    {
+        $this->rol = $rol;
+    }
+
+    // Getter y Setter para imagen
+    public function getImagen()
+    {
+        return $this->imagen;
+    }
+
+    public function setImagen($imagen)
+    {
+        $this->imagen = $imagen;
+    }
+
+    public function save()
+    {
+        $sql = "INSERT INTO usuarios VALUES(NULL, '{$this->getNombre()}', '{$this->getApellidos()}', '{$this->getEmail()}', '{$this->getPassword()}', 'user', null);";
+        $save = $this->db->query($sql);
+        $result = false;
+        if ($save) {
+            $result = true;
+        }
+       
+        return $result;
+    }
+}
