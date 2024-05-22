b45adbef101718d3cf788803abd22906b4763c4c
dgroes <diegopasten78@gmail.com>
2024-05-22 18:39:59 -0400

    Se actualiza con SESSION (Sección 68 del curso)

--- proyecto-php-poo/views/layout/sidebar.php
+++ proyecto-php-poo/views/layout/sidebar.php
@@ -1,26 +1,36 @@
+<!-- BARRA LATERAL -->
+<aside id="lateral">
 
+    <div id="login" class="block_aside">
+        <?php if (!isset($_SESSION['identity'])) : ?>
+            <h3>Entrar a la web</h3>
+            <form action="<?= base_url ?>usuario/login" method="post">
+                <label for="email">Email</label>
+                <input type="email" name="email" />
+                <label for="password">Contraseña</label>
+                <input type="password" name="password" />
+                <input type="submit" value="Enviar" />
+            </form>
+        <?php else : ?>
+            <h3><?= $_SESSION['identity']->nombre . " " . $_SESSION['identity']->apellidos ?></h3>
+        <?php endif; ?>
 
-            <!-- BARRA LATERAL -->
-            <aside id="lateral">
+        <ul>
 
-                <div id="login" class="block_aside">
-                    <h3>Entrar a la web</h3>
-                    <form action="#" method="post">
-                        <label for="email">Email</label>
-                        <input type="email" name="email" />
-                        <label for="password">Contraseña</label>
-                        <input type="password" name="password" />
-                        <input type="submit" value="Enviar" />
-                    </form>
+            <?php if (isset($_SESSION['admin'])) : ?>
+                <li><a href="#">Gestionar Categorias</a></li>
+                <li><a href="#">Gestionar Productos</a></li>
+                <li><a href="#">Gestionar Pedidos</a></li>
+            <?php endif; ?>
 
-                    <ul>
-                        <li><a href="#">Mis pedidos</a></li>
-                        <li><a href="#">Gestionar pedidos</a></li>
-                        <li><a href="#">Gestionar categorias</a></li>
-                    </ul>
-                </div>
+            <?php if (isset($_SESSION['identity'])) : ?>
+                <li><a href="#">Mis Pedidos</a></li>
+                <li><a href="<?= base_url ?>usuario/logout">Cerrar Sesión</a></li>
+            <?php endif; ?>
+        </ul>
+    </div>
 
-            </aside>
+</aside>
 
-            <!-- CONTENIDO CENTRAL -->
-            <div id="central">
+<!-- CONTENIDO CENTRAL -->
+<div id="central">
