9056d3160b52626e857f159880ff1c351bc7b38a
dgroes <diegopasten78@gmail.com>
2024-05-22 18:41:31 -0400

    Se agrega el metodo login y logout (Sección 68 del curso)

--- proyecto-php-poo/controllers/UsuarioController.php
+++ proyecto-php-poo/controllers/UsuarioController.php
@@ -44,4 +44,43 @@ class UsuarioController
         }
         header("Location:" . base_url . 'usuario/registro');
     }
+
+    public function login()
+    {
+        if (isset($_POST)) {
+            // Identificar al usuario
+
+            //Consulta a la BD
+            $usuario = new Usuario();
+            $usuario->setEmail($_POST['email']);
+            $usuario->setPassword($_POST['password']);
+            $identity = $usuario->login();
+
+            if ($identity && is_object($identity)) {
+                $_SESSION['identity'] = $identity;
+
+                if ($identity->rol == 'admin') {
+                    $_SESSION['admin'] = true;
+                }
+            } else {
+                $_SESSION['error_login'] = 'Identificación Fallida!!';
+            }
+        }
+        header("Location:" . base_url);
+    }
+
+    public function logout()
+    {
+        if (isset($_SESSION['identity'])) {
+            unset($_SESSION['identity']);
+        }
+
+        if (isset($_SESSION['admin'])) {
+            unset($_SESSION['admin']);
+        }
+
+        header("Location:" . base_url);
+    }
+
+    //Fin de la Clase
 }
