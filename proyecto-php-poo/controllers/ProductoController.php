8f031a8466c0e1c25881b69a63a010f8226e4851
dgroes <diegopasten78@gmail.com>
2024-05-23 19:24:30 -0400

    Se actualiza el controlador (Sección 70 cap 279 y 280)

--- proyecto-php-poo/controllers/ProductoController.php
+++ proyecto-php-poo/controllers/ProductoController.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'models/producto.php';
 
 class ProductoController
 {
@@ -6,5 +7,27 @@ class ProductoController
     {
         //Renderizar Vista
         require_once 'views/producto/destacados.php';
+    }
+
+    public function gestion()
+    {
+        Utils::isAdmin();
+        $producto = new Producto;
+        $productos = $producto->getAll();
+        require_once 'views/producto/gestion.php';
+    }
+
+    public function crear()
+    {
+        Utils::isAdmin();
+        require_once 'views/producto/crear.php';
+    }
+
+    public function save()
+    {
+        Utils::isAdmin();
+        if (isset($_POST)) {
+            var_dump($_POST);
+        }
     }
 }
