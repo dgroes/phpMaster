a48771cd6a88c2126746e94cf643d4252803c756
dgroes <diegopasten78@gmail.com>
2024-05-23 17:05:12 -0400

    Se agrega: /Metodo static de isAdmin el cual comprueba por SESSION si el user es Admin o No lo es. /Método static de showCategorias el cual hace un getAll de todas las categorías para mostrarlas en el header.php

--- proyecto-php-poo/helpers/utils.php
+++ proyecto-php-poo/helpers/utils.php
@@ -10,4 +10,22 @@ class Utils
         }
         return $name;
     }
+
+    //Con este método estatic se podrá comprobar si extiste una SESSION de administrador
+    public static function isAdmin()
+    {
+        if (!isset($_SESSION['admin'])) {
+            header("Location:" . base_url);
+        } else {
+            return true;
+        }
+    }
+
+    public static function showCategorias()
+    {
+        require_once 'models/categoria.php';
+        $categoria = new Categoria();
+        $categorias = $categoria->getAll();
+        return $categorias;
+    }
 }
