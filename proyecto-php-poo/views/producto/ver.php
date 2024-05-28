0363bbb01658f870ca4e7b60ffd4780b40e9ccdd
dgroes <diegopasten78@gmail.com>
2024-05-28 13:40:16 -0400

    View de producto individual (Sección 71 del Curso)

---
+++ proyecto-php-poo/views/producto/ver.php
@@ -0,0 +1,21 @@
+<?php if (isset($product)) : ?>
+    <h1><?= $product->nombre ?></h1>
+
+    <div id="detail-product">
+        <div class="image">
+            <?php if ($product->imagen != null) : ?>
+                <img src="<?= base_url ?>uploads/images/<?= $product->imagen ?>" />
+            <?php else : ?>
+                <img src="<?= base_url ?>assets/img/camiseta.png" alt="">
+            <?php endif; ?>
+        </div>
+        <div class="data">
+            <p class="description"><?= $product->descripcion ?></p>
+            <p class="price"><?= $product->precio ?> CLP</p>
+            <a href="" class="button">Comprar</a>
+        </div>
+    </div>
+
+<?php else : ?>
+    <h1>El Producto No Existe</h1>
+<?php endif; ?>
