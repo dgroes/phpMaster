ac0881af503c6ec608aa6b2d79e2f40589f93c31
dgroes <diegopasten78@gmail.com>
2024-05-28 13:38:54 -0400

    Se modifican las rutas de logo e inicio además de las categorías (Sección 71 del Curso)

--- proyecto-php-poo/views/layout/header.php
+++ proyecto-php-poo/views/layout/header.php
@@ -14,7 +14,7 @@
         <header id="header">
             <div id="logo">
                 <img src="<?= base_url ?>assets/img/camiseta.png" alt="Camiseta Logo" />
-                <a href="index.php">
+                <a href="<?= base_url ?>">
                     Tienda de camisetas
                 </a>
             </div>
@@ -25,11 +25,11 @@
         <nav id="menu">
             <ul>
                 <li>
-                    <a href="#">Inicio</a>
+                    <a href="<?= base_url ?>">Inicio</a>
                 </li>
                 <?php while ($cat = $categorias->fetch_object()) : ?>
                     <li>
-                        <a href=""><?=$cat->nombre?></a>
+                        <a href="<?= base_url ?>categoria/ver&id=<?= $cat->id ?>"><?= $cat->nombre ?></a>
                     </li>
                 <?php endwhile; ?>
             </ul>
