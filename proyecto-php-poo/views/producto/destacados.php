a377c6316a9891ce65376322eda2fa39b8b6472f
dgroes <diegopasten78@gmail.com>
2024-05-30 17:31:05 -0400

    Se modifica el enlace del botón comprar (Sección 72 del Curos)

--- proyecto-php-poo/views/producto/destacados.php
+++ proyecto-php-poo/views/producto/destacados.php
@@ -12,7 +12,7 @@
             <h2 title="<?= $product->nombre ?>"><?= strlen($product->nombre) > 30 ? substr($product->nombre, 0, 30) . '...' : $product->nombre; ?></h2>
         </a>
         <p><?= $product->precio ?></p>
-        <a href="" class="button">Comprar</a>
+        <a href="<?= base_url ?>carrito/add&id=<?= $product->id ?>" class="button">Comprar</a>
     </div>
 
 <?php endwhile; ?>
