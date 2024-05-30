de5ea1e070351664a258d2f3fbdbe353f2385bf9
dgroes <diegopasten78@gmail.com>
2024-05-30 17:37:50 -0400

    Se agrega la función estatica startCarrito, la cual gestiona la cantidad total de los productos añadidos al carro y el costo total de dichos pedidos (Sección 72 del Curos)

--- proyecto-php-poo/helpers/utils.php
+++ proyecto-php-poo/helpers/utils.php
@@ -28,4 +28,25 @@ class Utils
         $categorias = $categoria->getAll();
         return $categorias;
     }
+
+    public static function statsCarrito()
+    {
+        $stats = array(
+            'count' => 0,
+            'total' => 0
+        );
+
+        if (isset($_SESSION['carrito'])) {
+            $stats['count'] = count($_SESSION['carrito']);
+
+            foreach ($_SESSION['carrito'] as $producto) {
+                $stats['total'] += $producto['precio'] * $producto['unidades'];
+            }
+
+            // Formatear el total
+            $stats['total'] = number_format($stats['total'], 0, ',', '.');
+        }
+
+        return $stats;
+    }
 }
