cbb67ad82e4ad2fea4b00ce41fcdc42a96ade4c9
dgroes <diegopasten78@gmail.com>
2024-05-31 16:57:55 -0400

    Se modifica el enlace de Hacer Pedido (Sección 73 del Curso)

--- proyecto-php-poo/views/carrito/index.php
+++ proyecto-php-poo/views/carrito/index.php
@@ -30,5 +30,5 @@
 <div class="total-carrito">
     <?php $stats = Utils::statsCarrito() ?>
     <h3>Precio Total: <?= $stats['total'] ?> CLP</h3>
-    <a href="" class="button button-pedido">Hacer Pedido</a>
+    <a href="<?= base_url ?>pedido/hacer" class="button button-pedido">Hacer Pedido</a>
 </div>
