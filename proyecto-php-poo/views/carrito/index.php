ee21d4c662b233bf424b4a8bc918d285c81fa50d
dgroes <diegopasten78@gmail.com>
2024-06-03 13:50:20 -0400

    Se modifica la View agregando los botónes de agregar y quicar más productos, además del bobtón de elminiar el carrito completo (Sección 74 del curos y final del proyecto).

--- proyecto-php-poo/views/carrito/index.php
+++ proyecto-php-poo/views/carrito/index.php
@@ -1,34 +1,54 @@
 <h1>Carrito de la compra</h1>
-<table>
-    <tr>
-        <th>Imagen</th>
-        <th>Nombre</th>
-        <th>Precio</th>
-        <th>Unidades</th>
-    </tr>
-    <?php foreach ($carrito as $indice => $elemento) :
-        $producto = $elemento['producto'];
-
-    ?>
+<?php if (isset($_SESSION['carrito']) && count($_SESSION['carrito']) >= 1) : ?>
+    <table>
         <tr>
-            <td>
-                <?php if ($producto->imagen != null) : ?>
-                    <img src="<?= base_url ?>uploads/images/<?= $producto->imagen ?>" class="img_carrito" />
-                <?php else : ?>
-                    <img src="<?= base_url ?>assets/img/camiseta.png" alt="" class="img_carrito">
-                <?php endif; ?>
-            </td>
-            <td>
-                <a href="<?= base_url ?>producto/ver&id=<?= $producto->id ?>"><?= $producto->nombre ?></a>
-            </td>
-            <td><?= $producto->precio ?> CLP</td>
-            <td><?= $elemento['unidades'] ?></td>
+            <th>Imagen</th>
+            <th>Nombre</th>
+            <th>Precio</th>
+            <th>Unidades</th>
+            <th>Eliminar</th>
         </tr>
-    <?php endforeach; ?>
-</table>
-<br>
-<div class="total-carrito">
-    <?php $stats = Utils::statsCarrito() ?>
-    <h3>Precio Total: <?= $stats['total'] ?> CLP</h3>
-    <a href="<?= base_url ?>pedido/hacer" class="button button-pedido">Hacer Pedido</a>
-</div>
+        <?php foreach ($carrito as $indice => $elemento) :
+            $producto = $elemento['producto'];
+
+        ?>
+            <tr>
+                <td>
+                    <?php if ($producto->imagen != null) : ?>
+                        <img src="<?= base_url ?>uploads/images/<?= $producto->imagen ?>" class="img_carrito" />
+                    <?php else : ?>
+                        <img src="<?= base_url ?>assets/img/camiseta.png" alt="" class="img_carrito">
+                    <?php endif; ?>
+                </td>
+                <td>
+                    <a href="<?= base_url ?>producto/ver&id=<?= $producto->id ?>"><?= $producto->nombre ?></a>
+                </td>
+                <td>
+                    <?= $producto->precio ?> CLP
+                </td>
+                <td>
+                    <?= $elemento['unidades'] ?>
+                    <div class="updown-unidades">
+                        <a href="<?= base_url ?>carrito/down&index=<?= $indice ?>" class="button">-</a>
+                        <a href="<?= base_url ?>carrito/up&index=<?= $indice ?>" class="button">+</a>
+                    </div>
+                </td>
+                <td>
+                    <a href="<?= base_url ?>carrito/delete&index=<?= $indice ?>" class="button button-carrito button-red">Quitar Producto</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+
+    <br>
+    <div class="delete-carrito">
+        <a href="<?= base_url ?>carrito/delete_all" class="button button-delete button-red">Vaciar Carrito</a>
+    </div>
+    <div class="total-carrito">
+        <?php $stats = Utils::statsCarrito() ?>
+        <h3>Precio Total: <?= $stats['total'] ?> CLP</h3>
+        <a href="<?= base_url ?>pedido/hacer" class="button button-pedido">Hacer Pedido</a>
+    </div>
+<?php else : ?>
+    <p>El carrito está vacío, añade algún producto</p>
+<?php endif; ?>
