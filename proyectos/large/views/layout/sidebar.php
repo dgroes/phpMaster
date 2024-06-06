b09c8bccb01650830f5d029dd3596e397170bdfc
dgroes <diegopasten78@gmail.com>
2024-06-06 18:08:19 -0400

    Sidebar Inicial del proyecto

---
+++ proyectos/large/views/layout/sidebar.php
@@ -0,0 +1,35 @@
+<article class="sidebar">
+    <section class="about">
+        <h5 class="about_title">About</h5>
+        <p class="about_paragraph">
+            Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.
+        </p>
+    </section>
+
+    <section class="archives">
+        <h5 class="archives_title">Archives</h5>
+        <ul class="archives_list_ul">
+            <li class="archives_list_li"><a href="">March 2014</a></li>
+            <li class="archives_list_li"><a href="">February 2014</a></li>
+            <li class="archives_list_li"><a href="">January 2014</a></li>
+            <li class="archives_list_li"><a href="">December 2013</a></li>
+            <li class="archives_list_li"><a href="">November 2013</a></li>
+            <li class="archives_list_li"><a href="">October 2013</a></li>
+            <li class="archives_list_li"><a href="">September 2013</a></li>
+            <li class="archives_list_li"><a href="">August 2013</a></li>
+            <li class="archives_list_li"><a href="">July 2013</a></li>
+            <li class="archives_list_li"><a href="">June 2013</a></li>
+            <li class="archives_list_li"><a href="">May 2013</a></li>
+            <li class="archives_list_li"><a href="">April 2013</a></li>
+        </ul>
+    </section>
+
+    <section class="elsewhere">
+        <h5 class="elsewhere_title">Elsewhere</h5>
+        <ul class="elsewhere_list_ul">
+            <li class="elsewhere_list_li"><a href="">GitHub</a></li>
+            <li class="elsewhere_list_li"><a href="">Twitter</a></li>
+            <li class="elsewhere_list_li"><a href="">Facebook</a></li>
+        </ul>
+    </section>
+</article>
