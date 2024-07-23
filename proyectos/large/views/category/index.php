05bfd665eec47f8151c490d776b037c07d42385e
dgroes <diegopasten78@gmail.com>
2024-07-23 18:31:47 -0400

    Se crean las alertas de mensajes para la acciones de añadir, eliminar y editar.

--- proyectos/large/views/category/index.php
+++ proyectos/large/views/category/index.php
@@ -1,17 +1,68 @@
 <h2 class="title-category">Gestionar Categorías</h2>
 
-<br>
-
 <h2 class="subtitle-category">Crear Nueva Categoría </h2>
 
 <form action="<?= base_url ?>category/save" method="POST">
     <fieldset role="group">
-        <input type="text" name="name" placeholder="Nombre de la categoría" required />
+        <input type="text" name="name" placeholder="Nombre de la categoría" />
         <input type="submit" value="Crear" />
     </fieldset>
 </form>
 
 <!-- ALERTA DE CREACIÓN DE UNA NUEVA CATEGORÍA -->
+<?php if (isset($_SESSION['register-category'])) : ?>
+    <?php if ($_SESSION['register-category'] == 'complete') : ?>
+        <div class="alert alert-success">
+            <i class="fa-regular fa-square-check"></i> Categoría creada correctamente.
+        </div>
+    <?php elseif ($_SESSION['register-category'] == 'duplicate') : ?>
+        <div class="alert alert-warning">
+            <i class="fa-solid fa-triangle-exclamation"></i> El nombre de la categoría que desea guardar ya está en uso.
+        </div>
+    <?php elseif ($_SESSION['register-category'] == 'failed_save') : ?>
+        <div class="alert alert-danger">
+            <i class="fa-solid fa-xmark"></i> Error al guardar la categoría. Por favor, intenta nuevamente.
+        </div>
+    <?php elseif ($_SESSION['register-category'] == 'empty_name') : ?>
+        <div class="alert alert-warning">
+            <i class="fa-solid fa-triangle-exclamation"></i> El nombre de la categoría no puede estar vacío.
+        </div>
+    <?php endif; ?>
+    <?php unset($_SESSION['register-category']); ?>
+<?php endif; ?>
+
+
+<!-- ALERTA DE ELMINACIÓN DE UN REGISTRO -->
+<?php if (isset($_SESSION['delete-category'])) : ?>
+    <?php if ($_SESSION['delete-category'] == 'complete') : ?>
+        <div class="alert alert-success">
+            <i class="fa-regular fa-square-check"></i> Categoría eliminada correctamente.
+        </div>
+    <?php elseif ($_SESSION['delete-category'] == 'failed_delete') : ?>
+        <div class="alert alert-danger">
+            <i class="fa-solid fa-xmark"></i> Error al eliminar la categoría. Por favor, intenta nuevamente.
+        </div>
+    <?php endif; ?>
+    <?php unset($_SESSION['delete-category']); ?>
+<?php endif; ?>
+
+<!-- ALERTA DE EDICIÓN DE UN REGISTRO -->
+<?php if (isset($_SESSION['update-category'])) : ?>
+    <?php if ($_SESSION['update-category'] == 'complete') : ?>
+        <div class="alert alert-success">
+            <i class="fa-regular fa-square-check"></i> Categoría editada correctamente.
+        </div>
+    <?php elseif ($_SESSION['update-category'] == 'failed') : ?>
+        <div class="alert alert-danger">
+            <i class="fa-solid fa-xmark"></i> Error al editar la categoría. Por favor, intenta nuevamente.
+        </div>
+    <?php elseif ($_SESSION['update-category'] == 'duplicate') : ?>
+        <div class="alert alert-warning">
+            <i class="fa-solid fa-triangle-exclamation"></i> El nombre de la categoría que desea guardar ya está en uso.
+        </div>
+    <?php endif; ?>
+    <?php unset($_SESSION['update-category']); ?>
+<?php endif; ?>
 
 
 <table class="striped">
