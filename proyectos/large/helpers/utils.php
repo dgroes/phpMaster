3e04b9c55d5d0e6d74fa37f5fe8a88f22e993408
dgroes <diegopasten78@gmail.com>
2024-07-29 23:34:33 -0400

    Se agregan los métodos showAllCategories e isIdentity

--- proyectos/large/helpers/utils.php
+++ proyectos/large/helpers/utils.php
@@ -28,4 +28,24 @@ class Utils
         $categories = $category->getSome();
         return $categories;
     }
+
+    public static function showAllCategories(){
+        require_once 'models/category.php';
+        $category = new Category();
+        $categories = $category->getAll();
+        return $categories;
+    }
+
+    // Función para verficar el ususario a travez de su id de sessión
+    public static function isIdentity()
+    {
+        if (!isset($_SESSION['identity'])) {
+            header('Location:' . base_url);
+            exit();
+        } else {
+            return true;
+        }
+    }
+
+    
 }
