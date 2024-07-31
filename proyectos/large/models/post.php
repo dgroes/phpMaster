2c24fac542262ad31438b23a4773aa14f17780b3
dgroes <diegopasten78@gmail.com>
2024-07-30 21:26:23 -0400

    Se agrega un nuevo atributo a la clase, sub_title, además se agrega el cambio a la function save

--- proyectos/large/models/post.php
+++ proyectos/large/models/post.php
@@ -5,6 +5,7 @@ class Post
     private $id;
     private $user_id;
     private $title;
+    private $sub_title;
     private $content;
     private $image;
     private $status;
@@ -31,6 +32,11 @@ class Post
     public function getTitle()
     {
         return $this->title;
+    }
+
+    public function getSubTitle()
+    {
+        return $this->sub_title;
     }
 
     public function getContent()
@@ -74,6 +80,11 @@ class Post
         $this->title = $title;
     }
 
+    public function setSubTitle($sub_title)
+    {
+        $this->sub_title = $sub_title;
+    }
+
     public function setContent($content)
     {
         $this->content = $content;
@@ -114,11 +125,12 @@ class Post
     {
         $userId = $this->db->real_escape_string($this->getUserId());
         $title = $this->db->real_escape_string($this->getTitle());
+        $sub_title = $this->db->real_escape_string($this->getSubTitle());
         $content = $this->db->real_escape_string($this->getContent());
         $image = $this->getImage() ? "'" . $this->db->real_escape_string($this->getImage()) . "'" : "NULL";
         $status = $this->getStatus() ? "'" . $this->db->real_escape_string($this->getStatus()) . "'" : "'Visible'";
         $categoryId = $this->getCategoryId() ? "'" . $this->db->real_escape_string($this->getCategoryId()) . "'" : "NULL";
-        $sql = "INSERT INTO posts (id, user_id, title, content, image, status, created_at, category_id) VALUES (NULL, '$userId', '$title', '$content', $image, $status, NOW(), $categoryId)";
+        $sql = "INSERT INTO posts (id, user_id, title, sub_title, content, image, status, created_at, category_id) VALUES (NULL, '$userId', '$title', '$sub_title', '$content', $image, $status, NOW(), $categoryId)";
         $save = $this->db->query($sql);
         /* var_dump($sql);
         exit(); */
