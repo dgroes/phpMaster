d51eda087f54160ab82ef677d986de7c0af4bc2a
dgroes <diegopasten78@gmail.com>
2024-08-12 23:29:14 -0400

    Se ajusta el if de article, utilizando un operador ternario, además se ajusta la clase y el estatus con un flex

--- proyectos/large/views/post/general_management.php
+++ proyectos/large/views/post/general_management.php
@@ -6,30 +6,37 @@
             <?php $categoryClass = strtolower(str_replace(' ', '', $post->category_name)); ?>
             <?php $statusClass = strtolower($post->status); ?>
 
+
             <a href="<?= base_url ?>post/see&id=<?= $post->id ?>" class="post_link <?= $categoryClass; ?>">
-            <article 
-            <?php if ($post->status == 'Oculto') : ?> 
-                class="post <?= $statusClass; ?>">
-            <?php elseif ($post->status == 'Visible') : ?> 
-                    class="post <?= $categoryClass ?>">
-            <?php endif; ?>
+                <article class="post <?= $post->status == 'Oculto' ? $statusClass : $categoryClass ?>">
 
-            <section class="post_head">
-            <?php if ($post->status == 'Oculto') : ?>
-                    <?php $categoryClass == 'Oculto'; ?>
+                    <section class="post_head">
+                    <div class="class_status">
                     <div>
-                        <i class="fa-solid fa-eye-slash"></i> Post Oculto
-                    </div>
-
-            <?php endif; ?>
                         <p class="category_post"><?= $post->category_name ?></p>
+                    </div>
+                    <div>
+                        <?php if ($post->status == 'Oculto') : ?>
+                            <?php $categoryClass == 'Oculto'; ?>
+                            <div>
+                                <i class="fa-solid fa-eye-slash"></i> Post Oculto
+                            </div>
+
+                        <?php endif; ?> 
+                    </div>
+                </div>
+
                         <h3 class="title_post"><?= substr($post->title, 0, 30) ?>...</h3>
                         <p class="post_detail">Publicado el: <?= date('Y-m-d H:i', strtotime($post->created_at)) ?></p>
+
                     </section>
+
                     <p><?= nl2br(htmlspecialchars(substr($post->content, 0, 200))) ?>...</p>
-                    <cite>— <?= $post->creator?></cite> 
+                    <cite>— <?= $post->creator ?></cite>
                 </article>
             </a>
+
+
         <?php endwhile; ?>
     <?php else : ?>
         <p>No tienes posts.</p>
