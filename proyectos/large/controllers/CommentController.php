17a257f2ebe59bf424990e7cbe63314a0b19f88e
dgroes <diegopasten78@gmail.com>
2024-08-14 23:51:33 -0400

    Se agrega la function save(), aun se debe modificar y ajustar

--- proyectos/large/controllers/CommentController.php
+++ proyectos/large/controllers/CommentController.php
@@ -1,17 +1,40 @@
 <?php
 require_once 'models/comment.php';
 
-class CommentController 
+class CommentController
 {
     public function create()
     {
         Utils::isIdentity();
     }
 
-    public function showComments(){
-        Utils::isAdmin();
-        $comment = new Comment();
-        $allComments = $comment->getAllComments();
-        require_once 'views/post/general_management.php';
+    public function save()
+    {
+        Utils::isIdentity();
+
+        // Guardar la categoría a la BD
+        if (isset($_POST) && isset($_POST['comentario']) && !empty($_POST['comentario'])) {
+            $comment = new Category();
+            $comment->setName($_POST['comentario']);
+
+            if ($comment->nameExists()) {
+                $_SESSION['register-comment'] = "duplicate"; // Mensaje de alerta
+            } else {
+                $save = $comment->save();
+
+                // Mensajes de alerta
+                if ($save) {
+                    $_SESSION['register-comment'] = "complete";
+                } else {
+                    $_SESSION['register-comment'] = "failed_save";
+                }
+            }
+        } else {
+            $_SESSION['register-comment'] = "empty_name";
+        }
+
+        header("Location:" . base_url . "comment/index");
+        exit();
     }
+
 }
