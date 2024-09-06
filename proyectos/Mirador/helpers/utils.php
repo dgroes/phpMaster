4d8e3ea6d0c36f4440288e7bd7a023775e4d5e89
dgroes <diegopasten78@gmail.com>
2024-09-05 22:35:38 -0400

    Clase utils de proyecto, además se crean los métodos deleteSession y isAdmin

---
+++ proyectos/Mirador/helpers/utils.php
@@ -0,0 +1,26 @@
+<?php
+
+class Utils
+{
+
+    // Delete Session
+    public static function deleteSession($name)
+    {
+        if (isset($_SESSION[$name])) {
+            $_SESSION[$name] = null;
+            unset($_SESSION[$name]);
+        }
+        return $name;
+    }
+
+    // Is Admin
+    public static function isAdmin()
+    {
+        if (!isset($_SESSION['admin'])) {
+            header("Location:" . base_url);
+            exit();
+        } else {
+            return true;
+        }
+    }
+}
