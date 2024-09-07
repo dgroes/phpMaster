1f9935b722c8f281c2a1b9f24c618ad6a58ce505
dgroes <diegopasten78@gmail.com>
2024-09-06 23:45:19 -0400

    View de login del proyecto

---
+++ proyectos/Mirador/views/usuario/login.php
@@ -0,0 +1,16 @@
+<article class="login">
+    <form>
+        <fieldset>
+            <label>
+                Correo
+                <input type="email" name="email" placeholder="Ejemplo: mirador@gmail.com" required />
+            </label>
+            <label>
+                Contraseña
+                <input type="password" name="password" id=""  placeholder="Ejemplo: papimiperro" required>
+            </label>
+        </fieldset>
+
+        <input type="submit" value="Subscribe" />
+    </form>
+</article>
