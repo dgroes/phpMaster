17547ad1991690b14a3e386e01cbbddfe236a709
dgroes <diegopasten78@gmail.com>
2024-09-06 23:42:38 -0400

    Footer del proyecto.

---
+++ proyectos/Mirador/views/layout/footer.php
@@ -0,0 +1,12 @@
+</section>
+        <footer class="footer">
+
+            <p> <a href="https://github.com/dgroes" data-tooltip="Visitar perfil" target="_blank"><i class="fa-brands fa-github"> </i> Dgroes</a> © 2024 </p>
+        </footer>
+</main>
+
+
+
+</body>
+
+</html>
