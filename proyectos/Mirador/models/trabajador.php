251bcf52b77d7726bc2aa82b24e9ec34abb8a4cf
dgroes <diegopasten78@gmail.com>
2024-09-09 23:18:18 -0300

    Se agregan todas las nuevas add del proyecto, antes de que de otra pantala azúl

---
+++ proyectos/Mirador/models/trabajador.php
@@ -0,0 +1,11 @@
+<?php
+
+class Trabajador{
+    private $id;
+    private $persona_id;
+    private $cargo_id;
+    private $email;
+    private $contrasena;
+    private $rol;
+    private $db;
+}
