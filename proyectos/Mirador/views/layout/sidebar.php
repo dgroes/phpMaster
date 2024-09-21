b836225060e37426271e48df48d485f20be0a2aa
Diego Pasten <diegopasten78@gmail.com>
2024-09-20 22:26:18 -0300

    Sidebar del sistema

---
+++ proyectos/Mirador/views/layout/sidebar.php
@@ -0,0 +1,24 @@
+<section class="aside">
+    <article class="notes">
+        <header class="notes__subtitle">Notas rapidas</header>
+        <form action="">
+            <label for="notes"></label>
+            <textarea name="" id="">
+            - Hoalsd forward_static_call <br>
+            - maria jose/ 1002/ 12.412.515-1 / 18:02 <br>
+        </textarea>
+            <input type="submit" value="Guardar">
+        </form>
+    </article>
+    <article>
+        <header class="notes__subtitle">Mensajes a la conserjería Norte</header>
+        <form action="">
+            <label for="notes"></label>
+            <textarea name="" id="">
+            Oye, de casualidad tienes el rut de la utlima señora que entró, era una señora que entró con un perro y un choche, lo tienes?
+        </textarea>
+            <input type="submit" value="Enviar">
+        </form>
+
+    </article>
+</section>
