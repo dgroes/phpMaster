c4a65b1dfda29d9e0e3281121daa4134f2ee2fa4
Diego Pasten <diegopasten78@gmail.com>
2024-10-10 22:21:50 -0300

    controlador actualizado.

--- proyectos-mini/trabajadores/controllers/personaController.php
+++ proyectos-mini/trabajadores/controllers/personaController.php
@@ -7,15 +7,32 @@ class PersonaController
     public function index()
     {
         $persona = new Persona();
-        $personas = $persona->listarTodos();  // Asegúrate de obtener los resultados
-        require_once 'views/gestion.php';  // Pasas la vista después de obtener los datos
+        $personas = $persona->listarTodos();
+
+        // Inicializamos $trabajadores como null si no estamos en el controlador de trabajadores
+        $trabajadores = null;
+
+        require_once 'views/gestion.php';
     }
 
 
     public function guardar()
     {
-        if (isset($_POST) && isset($_POST['nombres']) && isset($_POST['apellidos']) && isset($_POST['cedula']) && isset($_POST['cargo'])) {
-            $trabajador = new Trabajador();
+        if (isset($_POST) && isset($_POST['nombres']) && isset($_POST['apellidos']) && isset($_POST['cedula'])) {
+            $nombres = isset($_POST['nombres']) ? $_POST['nombres'] : false;
+            $apellidos = isset($_POST['apellidos']) ? $_POST['apellidos'] : false;
+            $cedula = isset($_POST['cedula']) ? $_POST['cedula'] : false;
+
+            if ($nombres && $apellidos && $cedula) {
+                $persona = new Persona();
+                $persona->setNombres($nombres);
+                $persona->setApellidos($apellidos);
+                $persona->setCedula($cedula);
+
+                $save = $persona->guardar();
+            }
         }
+        header("Location:" . base_url . 'persona/index');
+        exit();
     }
 }
