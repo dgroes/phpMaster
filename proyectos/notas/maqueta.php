49409dec08c6d57f9febedb9c163421b7b924677
Diego Pasten <diegopasten78@gmail.com>
2024-10-11 23:55:48 -0300

    Maqueta en HTML de lo que será la página inicial y princial del proyecto.

---
+++ proyectos/notas/maqueta.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Notes</title>
+
+    <!-- Font Awesome, para la carga de iconos personalizados: -->
+    <script src="https://kit.fontawesome.com/335ff06f37.js" crossorigin="anonymous"></script>
+
+    <!-- Pico CSS -->
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"> -->
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.yellow.min.css">
+
+    <!-- Pico CSS Amarillo -->
+    <!-- <link rel="stylesheet" href="css/pico.yellow.min.css" /> -->
+
+</head>
+
+<body>
+    <main class="container-fluid">
+        <nav>
+            <ul>
+                <li><a href=""><i class="fa-solid fa-otter fa-2xl"></i> Notes</a></li>
+            </ul>
+            <ul>
+                <li><a href="#" class="secondary">Services</a></li>
+                <li>
+                    <details class="dropdown">
+                        <summary>
+                            Account
+                        </summary>
+                        <ul dir="rtl">
+                            <li><a href="#">Profile</a></li>
+                            <li><a href="#">Settings</a></li>
+                            <li><a href="#">Security</a></li>
+                            <li><a href="#">Logout</a></li>
+                        </ul>
+                    </details>
+                </li>
+            </ul>
+        </nav>
+        <section>
+            <article>
+                <h2>Notas</h2>
+                <input type="search" name="search" placeholder="Search" aria-label="Search" />
+            </article>
+        </section>
+    </main>
+</body>
+
+</html>
