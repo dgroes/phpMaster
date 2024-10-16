2bf50003190965839a118d1b1c2c5c09ea9202e9
Diego Pasten <diegopasten78@gmail.com>
2024-10-15 23:58:42 -0300

    Parameters del proyecto

---
+++ proyectos/notas/config/parameters.php
@@ -0,0 +1,7 @@
+<?php
+
+//Para que el formateo limpiio de las URLs funcinen será importante que en el servidor de apache esté activado el "rewrite_module"
+define("base_url", "http://localhost/phpMaster/proyectos/notas/"); //Importante que la ruta completa del localhost sea correcta.
+define("controller_default", "notaController");
+define("action_default", "index");
+
