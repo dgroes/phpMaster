3a85310551937f66ad1d9824ef514b83035098ab
Diego Pasten <diegopasten78@gmail.com>
2024-10-15 23:59:04 -0300

    Conexión a la BD.

---
+++ proyectos/notas/config/db.php
@@ -0,0 +1,11 @@
+<?php
+class Database
+{
+    // ESTABLECER LA CONEXIÓN A LA BD
+    public static function connect()
+    {
+        $db = new mysqli('localhost', 'root', '', 'notes');
+        $db->query("SET NAMES 'utf8");
+        return $db;
+    }
+}
