329e4be4c4ffa982c3c1242a82bbc7e43f26d44e
Diego Pasten <diegopasten78@gmail.com>
2024-10-15 23:58:21 -0300

    Script de la BD

---
+++ proyectos/notas/database/database.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE notes;
+
+USE notes;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+) ENGINE = InnoDB;
+
+CREATE TABLE notes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    color VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    CONSTRAINT fk_note_user FOREIGN KEY (user_id) REFERENCES users(id)
+) ENGINE = InnoDB;
