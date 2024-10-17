6dfb48b066dd7a8332222ed5cedcf6644a6e3995
Diego Pasten <diegopasten78@gmail.com>
2024-10-16 23:54:21 -0300

    View del registro de usuario

---
+++ proyectos/notas/views/login/register.php
@@ -0,0 +1,19 @@
+<section class="register">
+    <form action="" class="register__form">
+        <fieldset class="form__fielset">
+            <legend class="form__legend">Registro de Usuario</legend>
+            <label for="username" class="form__label">Nombre de Usuario</label>
+            <input type="text" name="usernam" class="form__input">
+
+            <label for="email" class="form__label">Correo Electronico</label>
+            <input type="email" name="email" class="form__input">
+
+            <label for="password" class="form__label">Contraseña</label>
+            <input type="password" name="password" class="form__input">
+
+            <input type="submit" value="Registrarse" class="form__submit">
+        </fieldset>
+
+    </form>
+
+</section>
