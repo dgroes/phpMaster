55fdd168a193ccacca51f0ffc2a8e2e303f4a4fe
Diego Pasten <diegopasten78@gmail.com>
2024-10-16 23:56:28 -0300

    Index actualizado del sistema

--- proyectos/notas/index.php
+++ proyectos/notas/index.php
@@ -3,8 +3,8 @@ ob_start();
 session_start();
 
 require_once 'autoload.php';
-require_once 'condif/db.php';
-require_once 'condif/parameters.php';
+require_once 'config/db.php';
+require_once 'config/parameters.php';
 require_once 'views/layout/header.php';
 
 function show_error()
@@ -23,15 +23,16 @@ if (isset($_GET['controller'])) {
     exit();
 }
 
+
 //Verificar la clase del controlador existente y crear una instancia si existe
 if (class_exists($name_controller)) {
     $controller = new $name_controller();
 
-    //Verifica si existe el parámetro 'action' en URL y el método correspondiete en el controlador
+    //Verifica si existe el parámetro 'action' en URL y el método correspondiente en el controlador
     if (isset($_GET['action']) && method_exists($controller, $_GET['action'])) {
-        $action = $_GET['action']; //$action será el nombre del método de las clases, por ejemplo si la clase tiene le metodo getAll, dentro de $action estará guardado 'getAll'
+        $action = $_GET['action']; //El $action será el nombre del metodo de las clases, por ejemplo si la clase tiene le metodo getAll, dentro de $action estará guardado 'getAll'
         $controller->$action(); //$name_controller es una variable que contiene el nombre del controlador, concatenando el nombre del controlador pasado por la URL con la palabra Controller. Por ejemplo, si $_GET['controller'] es user, entonces $name_controller será UserController.
-    } elseif (isset($_GET['controller']) && !isset($_GET['action'])) {
+    } elseif (!isset($_GET['controller']) && !isset($_GET['action'])) {
         $action_default = action_default;
         $controller->$action_default();
     } else {
@@ -40,7 +41,6 @@ if (class_exists($name_controller)) {
 } else {
     show_error();
 }
-
 
 require_once 'views/layout/footer.php';
 
