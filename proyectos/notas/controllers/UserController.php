4a983f3af6fd645f6ebd8eba052688f8f4141ab3
Diego Pasten <diegopasten78@gmail.com>
2024-10-19 18:34:01 -0300

    Se agrega el método verifyLogin y loginForm

--- proyectos/notas/controllers/UserController.php
+++ proyectos/notas/controllers/UserController.php
@@ -9,6 +9,11 @@ class UserController
         require_once 'views/login/register.php';
     }
 
+    public function loginForm()
+    {
+        require_once 'views/login/login.php';
+    }
+
     public function save()
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -35,7 +40,7 @@ class UserController
 
             // Validar la contraseña (mínimo 6 caracteres)
             if ($password && strlen($password) >= 6) {
-                $password = password_hash($password, PASSWORD_BCRYPT, ['cost' => 4]);
+                $password = password_hash($password, PASSWORD_DEFAULT);
             } else {
                 $errors[] = "La contraseña debe tener al menos 6 caracteres.";
             }
@@ -46,7 +51,8 @@ class UserController
                 $user->setUsername($username);
                 $user->setPassword($password);
                 $user->setEmail($email);
-                $user->setRol('user'); // Definido por defecto
+                // $user = new User($username, $email, $password);
+
 
                 // Guardar los datos en la BD
                 $save = $user->save();
@@ -72,6 +78,53 @@ class UserController
 
         // Redireccionar y detener la ejecución
         header("Location:" . base_url . 'user/register');
+        exit();
+    }
+
+    //Verificación de login e inicio de usuario
+    public function verifyLogin()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['username']) && isset($_POST['password'])) {
+            $user = new User();
+            $user->setUsername($_POST['username']);
+            $user->setPassword($_POST['password']);
+
+            $identity = $user->login();
+
+            // Verificar si el login fue exitoso
+            if ($identity && is_object($identity)) {
+                $_SESSION['identity'] = $identity;
+
+                // Descomentar para depurar y verificar el contenido de $_SESSION['identity']
+                /* var_dump($_SESSION['identity']); */ // Esto imprimirá el objeto del usuario en la salida
+                exit(); // Asegúrate de que esto no cause problemas, es solo para depurar
+
+                header("Location:" . base_url); // Redirigir si el login fue exitoso
+                exit();
+            } else {
+                $_SESSION['error_login'] = "Identificación Fallida";
+                header("Location:" . base_url . 'user/loginForm'); // Redirigir si el login falló
+                exit();
+            }
+        } else {
+            $_SESSION['error_login'] = "Correo electrónico y contraseña son requeridos";
+            header("Location:" . base_url . 'user/loginForm'); // Redirigir si no se enviaron datos
+            exit();
+        }
+    }
+
+
+
+    public function logout()
+    {
+        if (isset($_SESSION['identity'])) {
+            unset($_SESSION['identity']);
+        }
+
+        // Destruir todas las variables de sesión
+        session_destroy();
+
+        header("Location:" . base_url);
         exit();
     }
 }
