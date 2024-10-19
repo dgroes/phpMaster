94dc01204da24edcbe4c3239a9774a1674390533
Diego Pasten <diegopasten78@gmail.com>
2024-10-18 21:59:42 -0300

    Se añade el require de helpers/utils.php

--- proyectos/notas/index.php
+++ proyectos/notas/index.php
@@ -5,6 +5,7 @@ session_start();
 require_once 'autoload.php';
 require_once 'config/db.php';
 require_once 'config/parameters.php';
+require_once 'helpers/utils.php';
 require_once 'views/layout/header.php';
 
 function show_error()
