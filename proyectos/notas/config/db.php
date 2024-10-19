0649dbfa4f3f74509c7097d3973ad74d87823bc0
Diego Pasten <diegopasten78@gmail.com>
2024-10-18 21:58:17 -0300

    Se modifica el error de sintaxis, añadiendo la comilla simple faltante

--- proyectos/notas/config/db.php
+++ proyectos/notas/config/db.php
@@ -5,7 +5,7 @@ class Database
     public static function connect()
     {
         $db = new mysqli('localhost', 'root', '', 'notes');
-        $db->query("SET NAMES 'utf8");
+        $db->query("SET NAMES 'utf8'");
         return $db;
     }
 }
