74e250e2f816a52588877bc493d5942550787652
Diego Pasten <diegopasten78@gmail.com>
2024-10-18 21:53:14 -0300

    Se agregan los mensajes de alertas en la view

--- proyectos/notas/views/login/register.php
+++ proyectos/notas/views/login/register.php
@@ -1,19 +1,31 @@
 <section class="register">
-    <form action="" class="register__form">
+    <?php if (isset($_SESSION['register']) && $_SESSION['register'] == 'complete') : ?>
+        <div class="alert alert-success">
+            <i class="fa-regular fa-square-check"></i> Registro completado correctamente
+        </div>
+    <?php elseif (isset($_SESSION['register']) && $_SESSION['register'] == 'failed') : ?>
+        <div class="alert alert-error">
+            <i class="fa-solid fa-ban"></i> El registro no se pudo completar, completa los campos de manera correcta
+        </div>
+    <?php endif; ?>
+    <?php Utils::deleteSession('register'); ?>
+
+
+    <form action="<?= base_url ?>user/save" class="register__form" method="POST">
         <fieldset class="form__fielset">
             <legend class="form__legend">Registro de Usuario</legend>
             <label for="username" class="form__label">Nombre de Usuario</label>
-            <input type="text" name="usernam" class="form__input">
+            <input type="text" name="username" class="form__input" required>
 
             <label for="email" class="form__label">Correo Electronico</label>
-            <input type="email" name="email" class="form__input">
+            <input type="email" name="email" class="form__input" required>
 
             <label for="password" class="form__label">Contraseña</label>
-            <input type="password" name="password" class="form__input">
+            <input type="password" name="password" class="form__input" required>
 
             <input type="submit" value="Registrarse" class="form__submit">
         </fieldset>
-
     </form>
+
 
 </section>
