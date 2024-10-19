f8d916d3050a89532eeb845c1c10f72f26e8a678
Diego Pasten <diegopasten78@gmail.com>
2024-10-19 18:34:50 -0300

    Se modifica el controller_default

--- proyectos/notas/config/parameters.php
+++ proyectos/notas/config/parameters.php
@@ -2,6 +2,6 @@
 
 //Para que el formateo limpiio de las URLs funcinen será importante que en el servidor de apache esté activado el "rewrite_module"
 define("base_url", "http://localhost/phpMaster/proyectos/notas/"); //Importante que la ruta completa del localhost sea correcta.
-define("controller_default", "notaController");
+define("controller_default", "noteController");
 define("action_default", "index");
 
