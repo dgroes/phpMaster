6e3a8c33edda1a4eb190ce63abd323917dcc0d69
Diego Pasten <diegopasten78@gmail.com>
2024-10-18 21:54:44 -0300

    Se actualiza el constructor y le método save(), añadiendo a este último mejores prácticas

--- proyectos/notas/models/user.php
+++ proyectos/notas/models/user.php
@@ -14,17 +14,21 @@ class User
      * @param string $password Contraseña en texto plano
      */
 
-    public function __construct(int $id, string $username, string $email, string $password)
+    public function __construct(string $username, string $email, string $password)
     {
-        //Conexión a la BD
         $this->db = Database::connect();
 
-        //Asignación de los valores
-        $this->id = $id;
-        $this->username = $username;
-        $this->email = $email;
-        $this->password = $password;
+        // Validar los valores antes de asignarlos
+        if (!empty($username) && !empty($email) && !empty($password)) {
+            $this->username = $username;
+            $this->email = $email;
+            $this->password = $password;
+        } else {
+            throw new Exception("Los valores de username, email o password no son válidos.");
+        }
     }
+
+
 
     //Getter y Setter para id
     public function getId(): int
@@ -74,13 +78,37 @@ class User
     //Insertar los valores recogidos en la BD
     public function save()
     {
-        $sql = "INSERT INTO users VALUES (NULL, '{$this->getUsername()}', '{$this->getEmail()}', '{$this->getPassword()}'";
-        $save = $this->db->query($sql);
+        $username = $this->db->real_escape_string($this->getUsername());
+        $password = $this->db->real_escape_string($this->getPassword());
+        $email = $this->db->real_escape_string($this->getEmail());
 
-        $result = false;
-        if ($save) {
-            $result = true;
+        //Verificación si el username ya existen en la BD
+        $sql_check_username = "SELECT id FROM users WHERE username = '$username'";
+        $result_check_username = $this->db->query($sql_check_username);
+
+        if ($result_check_username && $result_check_username->num_rows > 0) {
+            return 'username'; //Indicar que ya existe un usuario con el mismo username o email
         }
-        return $result;
+
+        //Verificación si el email ya existe en la BD
+        $sql_check_email = "SELECT id FROM users WHERE email = '$email'";
+        $result_check_email = $this->db->query($sql_check_email);
+
+        if ($result_check_email && $result_check_email->num_rows > 0) {
+            return 'email'; //Indicar que ya existe un usuario con el mismo email
+        }
+
+        if (empty($errors)) {
+            $sql = "INSERT INTO users (username, password, email) VALUES ('$username', '$password', '$email');";
+            $save = $this->db->query($sql);
+
+            if ($save) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        return $errors;
     }
 }
