0ef1f23d2ffd7ad6947a1b03e4a7672c92c56b39
Diego Pasten <diegopasten78@gmail.com>
2024-10-23 00:01:25 -0300

    Model de note

---
+++ proyectos/notas/models/note.php
@@ -0,0 +1,90 @@
+<?php
+
+class Note
+{
+    private $id, $user_id, $title, $content, $color, $created_at, $db;
+
+    public function __construct()
+    {
+        $this->db = Database::connect();
+    }
+
+    // Getter y Setter para id
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id)
+    {
+        $this->id = $id;
+    }
+
+    // Getter y Setter para user_id
+    public function getUserId(): int
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(int $user_id)
+    {
+        $this->user_id = $user_id;
+    }
+
+    // Getter y Setter para title
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title)
+    {
+        $this->title = $title;
+    }
+
+    // Getter y Setter para content
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content)
+    {
+        $this->content = $content;
+    }
+
+    // Getter y Setter para color
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+
+    public function setColor(string $color)
+    {
+        $this->color = $color;
+    }
+
+    // Getter y Setter para created_at
+    public function getCreatedAt(): string
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(string $created_at)
+    {
+        $this->created_at = $created_at;
+    }
+
+    public function getAllByUser($user_id)
+    {
+        //Escapado de datos de entrada
+        $user_id = $this->db->real_escape_string($user_id);
+        $sql = "SELECT n.id, n.title, n.content, n.color, n.created_at, u.username
+                FROM notes n
+                INNER JOIN users u ON n.user_id = {$user_id}
+                ORDER BY n.created_at DESC;";
+
+        $notes = $this->db->query($sql);
+        return $notes;
+    }
+}
