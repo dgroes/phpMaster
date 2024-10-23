471e372ec38b8059699ffeb55d1d54e944836642
Diego Pasten <diegopasten78@gmail.com>
2024-10-23 00:00:16 -0300

    Se actualizan los enlaces y condiciones

--- proyectos/notas/views/layout/header.php
+++ proyectos/notas/views/layout/header.php
@@ -13,30 +13,42 @@
         <!-- NAV DE LA PÁGINA -->
         <nav class="nav">
             <ul class="nav__items">
-                <li class="nav__item nav__item--title"><a href="" class="nav__link">
+                <li class="nav__item nav__item--title"><a href="<?= base_url ?>note/index" class="nav__link">
                         <h1 class="nav__title">Notes</h1>
                     </a></li>
             </ul>
             <ul class="nav__items nav__items--right">
-                <li class="nav__item"><a href="#" class="nav__link">Services</a></li>
-                <li class="nav__item">
-                    <details class="dropdown">
-                        <summary class="dropdown__toggle">
-                            Account
-                        </summary>
-                        <ul class="dropdown__menu">
-                            <li class="dropdown__item"><a href="#">Profile</a></li>
-                            <li class="dropdown__item"><a href="#">Settings</a></li>
-                            <li class="dropdown__item"><a href="#">Security</a></li>
-                            <li class="dropdown__item"><a href="<?= base_url ?>user/logout" class="">Logout</a></li>
-                        </ul>
-                    </details>
-                </li>
+
+                <?php if (isset($_SESSION['identity'])) : ?>
+                    <li class="username"><?= $_SESSION['identity']->username ?></p>
+                <?php endif; ?>
+
+                <?php if (isset($_SESSION['identity'])): ?>
+                    <li class="nav__item">
+                        <details class="dropdown">
+                            <summary class="dropdown__toggle">
+                                Account
+                            </summary>
+                            <ul class="dropdown__menu">
+                                <li class="dropdown__item"><a href="#">Profile</a></li>
+                                <li class="dropdown__item"><a href="#">Settings</a></li>
+                                <li class="dropdown__item"><a href="#">Security</a></li>
+                                <li class="dropdown__item"><a href="<?= base_url ?>user/logout" class="">Logout</a></li>
+                            </ul>
+                        </details>
+                    </li>
+                <?php else : ?>
+                    
+                    <?php $currentUrl = $_SERVER['REQUEST_URI']; ?>
+
+                    <?php if (strpos($currentUrl, 'user/loginForm') !== false) : ?>
+                        <a href="<?= base_url ?>user/register" class="">Registrarse</a></li>
+                    <?php elseif (strpos($currentUrl, 'user/register') !== false) : ?>
+                        <a href="<?= base_url ?>user/loginForm" class="">Iniciar Sesión</a></li>
+                    <?php endif; ?>
+                    
+                <?php endif; ?>
+                <!-- <a href="javascript:history.back()">Go Back</a> -->
             </ul>
-            <?php if (isset($_SESSION['identity'])) : ?>
-                <p class="username"><?= $_SESSION['identity']->username ?></p>
-            <?php else : ?>
-                <p class="username">Usuario desconocido</p>
-            <?php endif; ?>
 
         </nav>
--- proyectos/notas/views/login/login.php
+++ proyectos/notas/views/login/login.php
@@ -1,10 +1,10 @@
 <section class="login">
 
-<form action="<?= base_url ?>user/verifyLogin" class="register__form" method="POST">
+<form action="<?= base_url ?>user/log" class="register__form" method="POST">
         <fieldset class="form__fielset">
             <legend class="form__legend">Inicio de sesión</legend>
-            <label for="username" class="form__label">Nombre de Usuario</label>
-            <input type="text" name="username" class="form__input" required>
+            <label for="email" class="form__label">Correo Electrónico</label>
+            <input type="text" name="email" class="form__input" required>
 
             <label for="password" class="form__label">Contraseña</label>
             <input type="password" name="password" class="form__input" required>
