c22043b66b9d0765c0a5c8dac0b05aba9248976d
Diego Pasten <diegopasten78@gmail.com>
2024-10-24 23:46:01 -0300

    se agregan mensajes de alerta

--- proyectos/notas/views/login/login.php
+++ proyectos/notas/views/login/login.php
@@ -1,8 +1,18 @@
 <section class="login">
 
-<form action="<?= base_url ?>user/log" class="register__form" method="POST">
+
+    <form action="<?= base_url ?>user/log" class="register__form" method="POST">
         <fieldset class="form__fielset">
             <legend class="form__legend">Inicio de sesión</legend>
+
+            <?php if (isset($_SESSION['error_login'])) : ?>
+                <div class="alert alert-error">
+                    <i class="fa-solid fa-ban"></i><?= $_SESSION['error_login'] ?>
+                </div>
+                <?php unset($_SESSION['error_login']); ?> <!-- //Eliminar mensaje de sesión_error -->
+            <?php endif; ?>
+
+
             <label for="email" class="form__label">Correo Electrónico</label>
             <input type="text" name="email" class="form__input" required>
 
