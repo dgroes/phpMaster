fcd1431c14885cfa584a66e46529268b646e6392
Diego Pasten <diegopasten78@gmail.com>
2024-10-24 23:45:12 -0300

    View de bienvenido cuando el usuario no está logeado

---
+++ proyectos/notas/views/note/bienvenido.php
@@ -0,0 +1,11 @@
+<section class="welcome">
+    <article class="welcome__star">
+        <h2>¡Crea Notas y Organizate!</h2>
+        <p>
+            Registra tus ideas, recibe sugerencias de tu equipo y píele a la IA que se encarge de los detalles finales
+        </p>
+        <img src="<?= base_url ?>uploads/one.jpeg" class="welcome__image">
+    </article>
+
+
+</section>
