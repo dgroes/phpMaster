c9d11833aa75de3de2e72d26e933bf0646dd658e
Diego Pasten <diegopasten78@gmail.com>
2024-10-28 23:16:09 -0300

    View del formulario de cración de una nota

---
+++ proyectos/notas/views/note/create.php
@@ -0,0 +1,14 @@
+<section class="create">
+    <article>
+        <form action="">
+            <fieldset>
+                <legend class="form__legend">Registro de Usuario</legend>
+                <label for="">Titulo</label>
+                <input type="text">
+
+                <label for="">Contendio</label>
+                <input type="text">
+            </fieldset>
+        </form>
+    </article>
+</section>
