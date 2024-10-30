c5624a6398c2b8b84c138e659b3f50c75ebfb7a9
Diego Pasten <diegopasten78@gmail.com>
2024-10-29 23:59:29 -0300

    Se modifca y se agregan los metodos

--- proyectos/notas/controllers/NoteController.php
+++ proyectos/notas/controllers/NoteController.php
@@ -5,6 +5,22 @@ class NoteController
 {
     public function index()
     {
+        if (isset($_SESSION['identity'])) {
+            $user_id = $_SESSION['identity']->id;
+            $note = new Note();
+            $myNotes = $note->getAllByUser($user_id);
+        } else {
+            require_once 'views/note/bienvenido.php';
+        }
         require_once 'views/note/notes.php';
+    }
+
+    public function seeNotes() {}
+
+    public function create(){
+        Utils::isIdentity();
+        $user_id = $_SESSION['identity']->id;
+        $note = new Note();
+        require_once 'views/note/create.php';
     }
 }
