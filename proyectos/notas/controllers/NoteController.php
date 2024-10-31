892c60c9463dc8e7ed1105d008d3745ed01013e8
Diego Pasten <diegopasten78@gmail.com>
2024-10-30 23:53:36 -0300

    se arrega un pequeño error en el método index, además se crea el método save para almacenar las notas en la BD

--- proyectos/notas/controllers/NoteController.php
+++ proyectos/notas/controllers/NoteController.php
@@ -9,18 +9,41 @@ class NoteController
             $user_id = $_SESSION['identity']->id;
             $note = new Note();
             $myNotes = $note->getAllByUser($user_id);
+
+            require_once 'views/note/notes.php';
         } else {
             require_once 'views/note/bienvenido.php';
         }
-        require_once 'views/note/notes.php';
     }
 
-    public function seeNotes() {}
-
-    public function create(){
+    public function create()
+    {
         Utils::isIdentity();
         $user_id = $_SESSION['identity']->id;
         $note = new Note();
         require_once 'views/note/create.php';
+    }
+
+    public function save()
+    {
+        Utils::isIdentity();
+        if (isset($_POST['title']) && isset($_POST['content']) && isset($_POST['user_id'])) {
+            $note = new Note();
+            $note->setUserId($_SESSION['identity']->id);
+            $note->setTitle($_SESSION['title']);
+            $note->setContent($_SESSION['content']);
+            $note->setColorId($_SESSION['color_id']);
+
+            $save = $note->save();
+            if ($save) {
+                $_SESSION['register-note'] = "complete";
+            } else {
+                $_SESSION['register-note'] = "falied";
+            }
+        } else {
+            $_SESSION['register-note'] = "falied";
+        }
+        header("Location:" . base_url . "note/index");
+        exit();
     }
 }
