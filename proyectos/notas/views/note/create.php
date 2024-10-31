9d949bc9549514df62c565ce21915d36d3f9a997
Diego Pasten <diegopasten78@gmail.com>
2024-10-30 23:49:32 -0300

    Se modifica y se mejora el formulario de crear

--- proyectos/notas/views/note/create.php
+++ proyectos/notas/views/note/create.php
@@ -2,7 +2,7 @@
 
     <article>
         <!-- FORMULARIO DE CRACION DE UNA NUEVA NOTA -->
-        <form action="" class="create__form">
+        <form action="<?= base_url ?>note/save" class="create__form" method="POST">
             <fieldset class="create__fielset">
 
                 <legend class="form__legend">Nueva nota</legend>
@@ -13,15 +13,22 @@
                 <input type="text" name="title " class="create__title">
 
                 <label for="content">Contendio</label>
-                <textarea name="content" id="" class="create__content"></textarea>
+                <textarea name="content" class="create__content"></textarea>
 
                 <label for="color">Color</label>
-                <select name="category_id" aria-label="Select your favorite cuisine..." required>
+
+                <!-- Llamado del utils de colors -->
+                <?php $colors = Utils::showColors(); ?>
+
+                <select name="color_id" aria-label="Select your favorite cuisine..." required>
                     <option selected disabled value="">
                         Seleccionar color...
                     </option>
-                    <option value="">pene</option>
-                    <option value="">penesito</option>
+                    <?php while ($col = $colors->fetch_object()): ?>
+                        <option value="<?= $col->id ?>">
+                            <?= ucfirst($col->detail) ?>
+                        </option>
+                    <?php endwhile; ?>
                 </select>
 
                 <input type="submit" value="Crear Nota" class="create__submit">
