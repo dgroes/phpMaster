b09d7970ad5ebf3037a59d257f8155184df7cf0b
Diego Pasten <diegopasten78@gmail.com>
2024-10-31 22:40:32 -0300

    Se agrean los mensajes de alerta, además se agrega el enlace correo junto con el id de la nota par visuzalizarla en la view independiente de la nota

--- proyectos/notas/views/note/notes.php
+++ proyectos/notas/views/note/notes.php
@@ -15,6 +15,21 @@
 
  <!-- SECCIÓN DE NOTAS -->
  <a class="notes_create" href="<?= base_url ?>/note/create">Nueva Nota +</a>
+
+ <!-- __MENSAJES DE ALERTA__ -->
+ <?php if (isset($_SESSION['register-note']) && $_SESSION['register-note'] == 'complete') : ?>
+     <div class="alert alert-success">
+         <i class="fa-regular fa-square-check"></i> Nota creada correctamente
+     </div>
+ <?php elseif (isset($_SESSION['register-note']) && $_SESSION['register-note'] == 'failed') : ?>
+     <div class="alert alert-error">
+         <i class="fa-solid fa-ban"></i> La nota no se pudo crear, intentalo nuevamente :(
+     </div>
+ <?php endif; ?>
+
+ <?php Utils::deleteSession('register-note'); ?>
+
+
  <section class="notes">
 
      <?php if ($myNotes && $myNotes->num_rows > 0) : ?>
@@ -22,9 +37,9 @@
              <?php $noteColor = strtolower(str_replace(' ', '', $note->color)); ?>
              <section class="notes__note note__<?= $noteColor; ?>">
                  <div class="note__titled"><?= $note->title ?></div>
-                 <div class="note__contentd"><?= $note->content ?></div>
+                 <div class="note__contentd"><?= nl2br(htmlspecialchars(substr($note->content, 0, 300))) ?>...</div>
                  <div class="note__footerd">
-                     <a href="">ver</a>
+                     <a href="<?= base_url ?>note/see&id=<?= $note->id ?>">ver</a>
                      <a href="">eliminar</a>
                      <a href="">editar</a>
                  </div>
