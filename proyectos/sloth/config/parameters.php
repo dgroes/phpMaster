f8ed92c8e06ce8f6506c81059114ebb1645aaa90
Diego Pasten <diegopasten78@gmail.com>
2024-11-01 23:33:25 -0300

    Defir parametros generales del manejo de las URLs

---
+++ proyectos/sloth/config/parameters.php
@@ -0,0 +1,6 @@
+<?php
+
+//Para el formateo limpi de la URLs, será importante que en el Server de Apache esté activo "rewrite_module"
+define("base_url", "http://localhost/phpMaster/proyectos/sloth/"); //Denifir la ruta base
+define("controller_defaul", "habitController"); //Definir el controlador principal por defecto
+define("action_defaul", "index"); //Definir le método por defecto del controller principal
