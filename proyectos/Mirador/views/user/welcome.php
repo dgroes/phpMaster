76165f45a3b062726fe76db69e95f4049858a125
Diego Pasten <diegopasten78@gmail.com>
2024-11-06 23:24:11 -0300

    Se cambia el nombre del fichero de inicio a welcome

---
+++ proyectos/Mirador/views/user/welcome.php
@@ -0,0 +1,18 @@
+<article class="management">
+    <header class="management__subtitle">Gestión de edificio El Mirador de los Andes</header>
+    <p class="management_paragraph">
+        Sistema de gestión de edificios, el cual posee una interfaz intuitiva al usuario, además ofrece perfiles personalizados acorde al tipo de trabajador, con este sistema se podrá realizar a grandes rasgos lo siguiente: <br> <br>
+        - Gestión de residentes <br>
+        - Gestión de Visitas <br>
+        - Gestión de bitacoras <br>
+        - Gestión de turnos <br>
+        - Gestión de Trabajadores <br> <br>
+        Gracias al uso de una Base de Datos se puede mantener un registro y gestión a lo largo de los años, almacenando los datos y poder tener un orden y seguridad de almacenado de datos relevantes para el trabajo.
+    </p>
+    <footer>
+        <p class="management_paragraph">
+            Para acceder al sistema por favor inicia tu sesión de usuario, si aun no posees un perfil de usuario, por favor, hablar con la administración.
+        </p>
+    </footer>
+</article>
+
