63098246424e16582a29d1cfc817d8216583fb7a
Diego Pasten <diegopasten78@gmail.com>
2024-11-06 23:26:39 -0300

    Se actualiza el fichero, añadiendo la clase al tag de parrafo

--- proyectos/Mirador/views/layout/footer.php
+++ proyectos/Mirador/views/layout/footer.php
@@ -1,7 +1,7 @@
 </section>
         <footer class="footer">
 
-            <p> <a href="https://github.com/dgroes" data-tooltip="Visitar perfil" target="_blank"><i class="fa-brands fa-github"> </i> Dgroes</a> © 2024 </p>
+            <p class="footer__git"> <a href="https://github.com/dgroes" data-tooltip="Visitar perfil" target="_blank"><i class="fa-brands fa-github"> </i> Dgroes</a> © 2024 </p>
         </footer>
 </main>
 
