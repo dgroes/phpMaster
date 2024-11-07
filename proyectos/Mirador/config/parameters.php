32454ba6850bab78b0e58faef089db560fca14ea
Diego Pasten <diegopasten78@gmail.com>
2024-11-06 23:33:42 -0300

    Se cambia de 'Usuario' a 'User'

--- proyectos/Mirador/config/parameters.php
+++ proyectos/Mirador/config/parameters.php
@@ -2,6 +2,6 @@
 
 //Para que el formateo limpiio de las URLs funcinen será importante que en el servidor de apache esté activado el "rewrite_module"
 define("base_url", "http://localhost/phpMaster/proyectos/mirador/"); //Importante que la ruta completa del localhost sea correcta.
-define("controller_default", "UsuarioController");
+define("controller_default", "UserController");
 define("action_default", "index");
 date_default_timezone_set('America/Santiago');
