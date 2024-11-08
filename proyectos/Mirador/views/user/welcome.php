1f089c361c6eba8d1f0af7334457160a6cdc94e5
Diego Pasten <diegopasten78@gmail.com>
2024-11-07 23:33:19 -0300

    Se cambian las clases de algunos elemntos dentro del welcome

--- proyectos/Mirador/views/user/welcome.php
+++ proyectos/Mirador/views/user/welcome.php
@@ -1,6 +1,6 @@
-<article class="management">
-    <header class="management__subtitle">Gestión de edificio El Mirador de los Andes</header>
-    <p class="management_paragraph">
+<article class="welcome">
+    <header class="welcome__subtitle">Gestión de edificio El Mirador de los Andes</header>
+    <p class="welcome__paragraph">
         Sistema de gestión de edificios, el cual posee una interfaz intuitiva al usuario, además ofrece perfiles personalizados acorde al tipo de trabajador, con este sistema se podrá realizar a grandes rasgos lo siguiente: <br> <br>
         - Gestión de residentes <br>
         - Gestión de Visitas <br>
@@ -10,7 +10,7 @@
         Gracias al uso de una Base de Datos se puede mantener un registro y gestión a lo largo de los años, almacenando los datos y poder tener un orden y seguridad de almacenado de datos relevantes para el trabajo.
     </p>
     <footer>
-        <p class="management_paragraph">
+        <p class="welcome__paragraph">
             Para acceder al sistema por favor inicia tu sesión de usuario, si aun no posees un perfil de usuario, por favor, hablar con la administración.
         </p>
     </footer>
