6e31307cf177b50b8ce5c9ef412a770416166516
Diego Pasten <diegopasten78@gmail.com>
2024-11-07 23:37:24 -0300

    Se crea el controllador orientado a las opciones de gestión del Mayordomo

---
+++ proyectos/Mirador/controllers/ManagementController.php
@@ -0,0 +1,14 @@
+<?php
+
+class ManagementController {
+
+    //Redirección a la view de turnos
+    public function showShift(){
+        require_once 'views/management/shift.php';
+    }
+
+    //Creación de turno de trabajo
+    public function createShift(){
+        if(isset($_POST['']));
+    }
+}
