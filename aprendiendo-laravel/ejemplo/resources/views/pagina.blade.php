684831886c5ed0525514a3796d09a8c67b58fe83
Diego Pasten <diegopasten78@gmail.com>
2024-11-16 21:37:53 -0300

    Se crea un footer

---
+++ aprendiendo-laravel/ejemplo/resources/views/pagina.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+
+
+@section('titulo', 'Master en PHP')
+
+@section('header')
+    @parent()
+    <h2>Hola</h2>
+@stop
+
+@section('content')
+    <h1>Contenido de la página generica 😈</h1>
+@stop
