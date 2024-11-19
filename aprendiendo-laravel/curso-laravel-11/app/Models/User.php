dcb32616e353709414edd380f2ad6004d158955a
Diego Pasten <diegopasten78@gmail.com>
2024-11-18 21:50:29 -0300

    Modelo de usuario

--- aprendiendo-laravel/curso-laravel-11/app/Models/User.php
+++ aprendiendo-laravel/curso-laravel-11/app/Models/User.php
@@ -11,7 +11,7 @@ class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
-
+    // protected $table = 'usuarios';
     /**
      * The attributes that are mass assignable.
      *
