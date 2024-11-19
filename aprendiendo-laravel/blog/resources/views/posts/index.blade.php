1343240fe4bdb0076f0156b5dc3d2648aa1cbcd8
Diego Pasten <diegopasten78@gmail.com>
2024-11-18 21:48:53 -0300

    View del index

---
+++ aprendiendo-laravel/blog/resources/views/posts/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel 11 | Posts</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.red.min.css">
+</head>
+
+<body>
+    <main class="container-fluid">
+        <h1>Aaquí se mostrarán todos los posts 🍅</h1>
+    </main>
+</body>
+
+</html>
