00a4f234da2894a0204a690813b1fd02a6039313
Diego Pasten <diegopasten78@gmail.com>
2024-11-18 21:52:24 -0300

    Ruta principal del proyecto

--- aprendiendo-laravel/curso-laravel-11/routes/web.php
+++ aprendiendo-laravel/curso-laravel-11/routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+/* Route::get('/task', function () {
+    return 'taks desde web';
+});
+ */
