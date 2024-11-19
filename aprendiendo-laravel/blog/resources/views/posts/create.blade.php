18f1376523e7843589564b6130b00d72e2a3146c
Diego Pasten <diegopasten78@gmail.com>
2024-11-18 21:48:21 -0300

    View de create

---
+++ aprendiendo-laravel/blog/resources/views/posts/create.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel 11 | Posts</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.red.min.css">
+</head>
+
+<body>
+    <main class="container-fluid">
+        <h1>Aquí se mostrá un formulario para crear un post 🐕</h1>
+    </main>
+
+
+    <div class="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
+        <span class="font-medium">Info alert!</span> Change a few things up and try submitting again.
+    </div>
+</body>
+
+</html>
