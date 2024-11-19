20b04fe6c23347acf01ba20570658554b366b66e
Diego Pasten <diegopasten78@gmail.com>
2024-11-18 21:49:41 -0300

    Rutas del proyecto de practica

--- aprendiendo-laravel/blog/routes/web.php
+++ aprendiendo-laravel/blog/routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
 });
+ */
+
+ //Cuando se utilizan controladores de acción unica, esta irá sin corchetes
+Route::get('/', HomeController::class);
+
+
+Route::get('/posts', [PostController::class, 'index']);
+
+Route::get('/posts/create',[PostController::class, 'create']);
+
+Route::get('/posts/{post}', [PostController::class, 'show']);
+
+
+/* Route::get('/posts/{post}/{category}', function ($post, $category) {
+    return "aquí se mostrará el post {$post} dela categoría {$category}";
+}); */
+
+/* Route::get('/posts/post-2', function () {
+    return "aquí se mostrará el post 2";
+});
+ */
