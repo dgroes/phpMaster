02e0a87f14136cef1d57de89fbe610c97ec2ecde
Diego Pasten <diegopasten78@gmail.com>
2024-11-23 22:50:16 -0300

    Se modifica la view individual del post

--- aprendiendo-laravel/blog/resources/views/posts/show.blade.php
+++ aprendiendo-laravel/blog/resources/views/posts/show.blade.php
@@ -1,21 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
+<x-app-layout>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel 11 | Posts</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.red.min.css">
-</head>
+    <article>
+        <a href="/posts">Volver a posts</a>
+    </article>
 
-<body>
-    <main class="container-fluid">
-        <h1>Aquí se mostrá un formulario para crear un post 🐕. {{ $post }}</h1>
-        @if ($post == 'pene')
-            <h3>Ordinado</h3>
-        @endif
-    </main>
-</body>
+    <article>
+        <header>
+            <h1>{{ $post->title }}</h1>
+        </header>
+        <p>
+            {{ $post->content }}
+        </p>
+        <footer>
+            <strong>{{ $post->category }}</strong>
+        </footer>
+    </article>
 
-</html>
+</x-app-layout>
