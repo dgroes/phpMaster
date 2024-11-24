6503892c9c3c6c22b237f9f2bddec430622a1525
Diego Pasten <diegopasten78@gmail.com>
2024-11-23 22:54:04 -0300

    Actualización de fichero

--- aprendiendo-laravel/blog/app/Models/Post.php
+++ aprendiendo-laravel/blog/app/Models/Post.php
@@ -13,13 +13,24 @@ class Post extends Model
 
     protected $table = 'post';
 
+    //CASTEO DE VALORES
+    protected function casts(): array
+    {
+        return [
+            'published_at' => 'datetime',
+            'is_active' => 'boolean'
+        ];
+    }
+
     protected function title(): Attribute
     {
+
+
         return Attribute::make(
             set: function ($value) {
                 return strtolower($value);
-            }, 
-            get: function($value){
+            },
+            get: function ($value) {
                 return ucfirst($value);
             }
         );
