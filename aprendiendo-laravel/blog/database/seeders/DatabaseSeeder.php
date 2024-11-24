fda72fbb3064daba56bebc98342394303fdb3601
Diego Pasten <diegopasten78@gmail.com>
2024-11-23 22:52:50 -0300

    Se modifica el fichero, agregando una factory para Post

--- aprendiendo-laravel/blog/database/seeders/DatabaseSeeder.php
+++ aprendiendo-laravel/blog/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +19,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        Post::factory(100)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        /* DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => Str::random(10).'@example.com',
+            'password' => Hash::make('password'),
+        ]); */
+
+        $this->call([
+            UserSeeder::class
         ]);
+
+        // User::factory(50)->create();
     }
 }
