b88f447e862f17ad595f9ebfff5c6459282077c6
Diego Pasten <diegopasten78@gmail.com>
2024-11-26 21:06:51 -0300

    Se modifica el fichero

--- aprendiendo-laravel/blog/app/Http/Controllers/PostController.php
+++ aprendiendo-laravel/blog/app/Http/Controllers/PostController.php
@@ -10,7 +10,7 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::orderBy('id', 'desc')
-            ->get();
+            ->paginate(10);
 
         // return $posts;
         return view('posts.index', compact('posts'));
@@ -25,16 +25,17 @@ class PostController extends Controller
     {
         $post = new Post();
         $post->title = $request->title;
+        $post->slug = $request->slug;
         $post->category = $request->category;
         $post->content = $request->content;
         $post->save();
 
-        return redirect('/posts');
+        return redirect()->route('posts.index');
     }
 
-    public function show($post)
+    public function show(Post $post)
     {
-        $post = Post::find($post);
+        // $post = Post::find($post);
         // return $post;
 
         return view('posts.show', compact('post'));
@@ -43,5 +44,31 @@ class PostController extends Controller
         /* return view('posts.show', [
             'post' => $post
         ]); */
+    }
+
+    public function edit(Post $post)
+    {
+        // $post = Post::find($post);
+        return view('posts.edit', compact('post'));
+    }
+
+    public function update(Request $request, Post $post)
+    {
+        // $post = Post::find($post);
+
+        $post->title = $request->title;
+        $post->slug = $request->slug;
+        $post->category = $request->category;
+        $post->content = $request->content;
+        $post->save();
+
+        return redirect()->route('posts.show', $post);
+    }
+
+    public function destroy(Post $post)
+    {
+        // $post = Post::find($post);
+        $post->delete();
+        return redirect()->route('posts.index');
     }
 }
