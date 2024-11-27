42b53ea10a997f3f16854e66ec0bd72887f17323
Diego Pasten <diegopasten78@gmail.com>
2024-11-26 21:06:00 -0300

    Se agrega el campo Slug a la tabla

--- aprendiendo-laravel/blog/database/migrations/2024_11_22_024616_create_post_table.php
+++ aprendiendo-laravel/blog/database/migrations/2024_11_22_024616_create_post_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('post', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->string('slug')->unique();
             $table->string('category');
             $table->longText('content');
             $table->timestamp('published_at');
