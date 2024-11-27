1ebf33f6a8085b10fef5d36aaeaa132705fa04bf
Diego Pasten <diegopasten78@gmail.com>
2024-11-26 21:06:36 -0300

    Se agrega el campo de slug

--- aprendiendo-laravel/blog/database/factories/PostFactory.php
+++ aprendiendo-laravel/blog/database/factories/PostFactory.php
@@ -18,6 +18,7 @@ class PostFactory extends Factory
     {
         return [
             'title' => $this->faker->sentence(),
+            'slug' => $this->faker->slug(),
             'content' => $this->faker->text(1000),
             'category' => $this->faker->word(),
             'published_at' => $this->faker->dateTime(),
