2fa77a4d73910871682018dea3b60c53a2fe33b4
Diego Pasten <diegopasten78@gmail.com>
2024-11-26 21:03:09 -0300

    Se modifican las rutas, además de algunos cambios menores

--- aprendiendo-laravel/blog/resources/views/posts/show.blade.php
+++ aprendiendo-laravel/blog/resources/views/posts/show.blade.php
@@ -1,18 +1,33 @@
 <x-app-layout>
 
     <article>
-        <a href="/posts">Volver a posts</a>
+        <a href="{{ route('posts.index') }}">Volver a posts</a>
     </article>
 
     <article>
         <header>
             <h1>{{ $post->title }}</h1>
         </header>
+        <header>
+            Slug
+            <h1>{{ $post->slug }}</h1>
+        </header>
         <p>
             {{ $post->content }}
         </p>
         <footer>
             <strong>{{ $post->category }}</strong>
+        </footer>
+
+        <footer>
+
+            <a href="{{ route('posts.edit', $post) }}">Editar</a>
+
+            <form action="{{ route('posts.destroy', $post) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <input type="submit" value="Elimina Post">
+            </form>
         </footer>
     </article>
 
