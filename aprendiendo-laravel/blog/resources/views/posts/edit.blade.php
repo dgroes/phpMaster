140b472d4ca1ce7dd11ed8996ba83183db4b719e
Diego Pasten <diegopasten78@gmail.com>
2024-11-27 22:45:56 -0300

    Se agregan los alert par ala view

--- aprendiendo-laravel/blog/resources/views/posts/edit.blade.php
+++ aprendiendo-laravel/blog/resources/views/posts/edit.blade.php
@@ -1,9 +1,32 @@
 <x-app-layout>
+
+    {{-- <article>
+        <a href="{{ route('posts.index') }}">Volver al post</a>
+    </article>
+ --}}
     <article>
 
         <head>
             <h1>Edición de post: <i>"{{ $post->title }}"</i></h1>
         </head>
+
+        @if ($errors->any())
+
+            <br>
+            <article>
+
+                <h4>Errores:</h4>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>
+                            {{ $error }}
+                        </li>
+                    @endforeach
+                </ul>
+
+            </article>
+            <br>
+        @endif
 
         <form action="{{ route('posts.update', $post) }}" method="POST">
 
@@ -12,16 +35,16 @@
             @method('PUT')
 
             <label for="title">Titulo:</label>
-            <input type="text" name="title" value="{{ $post->title }}">
+            <input type="text" name="title" value="{{ old('title', $post->title) }}">
 
             <label for="slug">Slug:</label>
-            <input type="text" name="slug" value="{{ $post->slug }}">
+            <input type="text" name="slug" value="{{ old('slug', $post->slug) }}">
 
             <label for="category">Categoría:</label>
-            <input type="text" name="category" value="{{ $post->category }}">
+            <input type="text" name="category" value="{{ old('category', $post->category) }}">
 
             <label for="content">Contentido:</label>
-            <textarea name="content" id="" cols="30" rows="20">{{ $post->content }}</textarea>
+            <textarea name="content" id="" cols="30" rows="20">{{ old('content', $post->content) }}</textarea>
 
             <input type="submit" value="Editar Registro">
         </form>
