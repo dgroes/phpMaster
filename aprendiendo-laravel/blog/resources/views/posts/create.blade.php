e4451e77d37aee60054e4b39073bcfb97514d8bc
Diego Pasten <diegopasten78@gmail.com>
2024-11-27 22:45:10 -0300

    Se agregan los mensajes de alerta

--- aprendiendo-laravel/blog/resources/views/posts/create.blade.php
+++ aprendiendo-laravel/blog/resources/views/posts/create.blade.php
@@ -1,9 +1,32 @@
 <x-app-layout>
     <article>
+        <a href="{{ route('posts.index') }}">Volver a posts</a>
+    </article>
+    <article>
 
         <head>
             <h1>Creación de Nuevo Post</h1>
         </head>
+
+        {{-- {{__('Client Closed Request')}} --}}
+
+        {{-- Primera forma de mostrar los errores de formulario --}}
+        @if ($errors->any())
+
+            <div>
+
+                <h4>Errores:</h4>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>
+                            {{ $error }}
+                        </li>
+                    @endforeach
+                </ul>
+
+            </div>
+        @endif
+
 
         <form action="{{ route('posts.store') }}" method="POST">
 
@@ -11,16 +34,25 @@
             @csrf
 
             <label for="title">Titulo:</label>
-            <input type="text" name="title">
+            <input type="text" name="title" value="{{ old('title') }}">
+            {{--  @error('title')
+                <p>{{ $message }}</p>
+            @enderror --}}
 
             <label for="slug">Slug:</label>
-            <input type="text" name="slug">
+            <input type="text" name="slug" value="{{ old('slug') }}">
+            {{-- @error('slug')
+                <p>{{ $message }}</p>
+            @enderror --}}
 
             <label for="category">Categoría:</label>
-            <input type="text" name="category">
+            <input type="text" name="category" value="{{ old('category') }}">
+            {{-- @error('category')
+                <p>{{ $message }}</p>
+            @enderror --}}
 
             <label for="content">Contentido:</label>
-            <textarea name="content" id="" cols="30" rows="20"></textarea>
+            <textarea name="content" id="" cols="30" rows="20">{{ old('content') }}</textarea>
 
             <input type="submit" value="Guadar Registro">
         </form>
