9fa436de78080a31a673651b060ca02204f41252
Diego Pasten <diegopasten78@gmail.com>
2024-11-27 22:43:18 -0300

    Se cambio is_active a false

--- aprendiendo-laravel/blog/database/migrations/2024_11_22_024616_create_post_table.php
+++ aprendiendo-laravel/blog/database/migrations/2024_11_22_024616_create_post_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('category');
             $table->longText('content');
             $table->timestamp('published_at');
-            $table->boolean('is_active')->default(true);
+            $table->boolean('is_active')->default(false);
             $table->timestamps();
         });
     }
