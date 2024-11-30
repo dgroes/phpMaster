ab1817dcda0df74ee39b8a731e0ef761bae67594
Diego Pasten <diegopasten78@gmail.com>
2024-11-29 23:17:25 -0300

    build: :building_construction:

--- aprendiendo-laravel/laravel11-tasks/routes/web.php
+++ aprendiendo-laravel/laravel11-tasks/routes/web.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+// Route::view('/', 'welcome');
+
+// Redireccionamos de la / al dashboard, evitando el welcome al acceder la localhost
+Route::redirect('/', 'dashboard');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
