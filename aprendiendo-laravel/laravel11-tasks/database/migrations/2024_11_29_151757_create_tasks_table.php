e07a8bd69779d1394e01f54514c33193547fdb87
Diego Pasten <diegopasten78@gmail.com>
2024-12-02 23:18:18 -0300

    Se agrega el softdeletes a la seed

--- aprendiendo-laravel/laravel11-tasks/database/migrations/2024_11_29_151757_create_tasks_table.php
+++ aprendiendo-laravel/laravel11-tasks/database/migrations/2024_11_29_151757_create_tasks_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
