c3c282c862ec10d40160a43212b60a5bd8a2d3be
Diego Pasten <diegopasten78@gmail.com>
2024-12-07 12:41:48 -0300

    build: :rocket: Se añade Breeze y Buggregator

--- aprendiendo-laravel/vinoteca/config/logging.php
+++ aprendiendo-laravel/vinoteca/config/logging.php
@@ -89,7 +89,7 @@ return [
             'handler_with' => [
                 'host' => env('PAPERTRAIL_URL'),
                 'port' => env('PAPERTRAIL_PORT'),
-                'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
+                'connectionString' => 'tls://' . env('PAPERTRAIL_URL') . ':' . env('PAPERTRAIL_PORT'),
             ],
             'processors' => [PsrLogMessageProcessor::class],
         ],
@@ -125,6 +125,10 @@ return [
 
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
+        ],
+        'bugger' => [
+            'driver' => 'buggregator',
+            'level' => 'debug', // Puedes ajustar el nivel según tus necesidades
         ],
 
     ],
