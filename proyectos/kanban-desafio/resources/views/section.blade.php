79386f5d863baa20a4e2c1a9b6365cc2b7bbe0d9
Diego Pasten <diegopasten78@gmail.com>
2024-12-19 12:27:09 -0300

    feat: :zap: Posible futuro componente en donde se visualizará el Kanban general

---
+++ proyectos/kanban-desafio/resources/views/section.blade.php
@@ -0,0 +1,7 @@
+// ...existing code...
+<section class="section-cards">
+    <div class="card">Card 1</div>
+    <div class="card">Card 2</div>
+    <div class="card">Card 3</div>
+</section>
+// ...existing code...
