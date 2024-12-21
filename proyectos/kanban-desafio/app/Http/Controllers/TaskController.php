3a973395c26f93d359dcfac9033e165529af5437
Diego Pasten <diegopasten78@gmail.com>
2024-12-20 23:50:03 -0300

    build: :zap: Se modifica el controller

--- proyectos/kanban-desafio/app/Http/Controllers/TaskController.php
+++ proyectos/kanban-desafio/app/Http/Controllers/TaskController.php
@@ -2,9 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TaskController extends Controller
 {
-    //
+    //Index del sistema
+    public function index()
+    {
+        // $tasks = Task::where('user_id', auth()->id());
+        $tasks = Task::all();
+        return view('dashboard', compact('tasks'));
+    }
+
+    //View of Create task
+    public function create()
+    {
+        return view('tasks.create');
+    }
+
+    //Store Task
+    public function store(Request $request)
+    {
+         $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'priority' => 'required|string',
+            'tags' => 'nullable|array',
+        ]);
+
+        Task::create([
+            'user_id' => Auth::id(), // Asegúrate de obtener el ID del usuario autenticado
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            'status' => false,
+            'priority' => $request->input('priority'),
+            'tags' => $request->input('tags'),
+        ]);
+
+    //    Task::create($request->all());
+        return redirect()->route('tasks.index');
+    }
 }
