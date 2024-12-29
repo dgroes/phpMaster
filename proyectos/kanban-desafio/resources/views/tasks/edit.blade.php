04ff9d0e610ee2c363e1cc102b0ec524accf0442
Diego Pasten <diegopasten78@gmail.com>
2024-12-28 21:46:00 -0300

    build: :building_construction: View de edición de Task

---
+++ proyectos/kanban-desafio/resources/views/tasks/edit.blade.php
@@ -0,0 +1,68 @@
+<x-app-layout class="flex justify-center items-center h-screen bg-gray-100">
+    <article class="group bg-neutral text-primary-content p-8 rounded-lg shadow-md w-full max-w-2xl">
+        <form action="{{ route('tasks.update', $task) }}" method="POST">
+
+            <div class="hero-content text-center">
+                <div class="max-w-md">
+                    <h1 class="text-5xl font-bold text-white ">{{ $task->title }}</h1>
+                </div>
+            </div>
+            @csrf
+            @method('PUT')
+            {{-- Titulo --}}
+            <label class="form-control w-full mb-4">
+                <div class="label">
+                    <span class="label-text text-lg">Titulo de tarea</span>
+                    <span class="badge badge-primary">Obligatorio</span>
+                </div>
+                <input type="text" name="title" placeholder="Type here" value="{{ old('title', $task->title) }}"
+                    class="input input-bordered w-full text-white p-4" />
+            </label>
+
+            {{-- Descripción --}}
+            <label class="form-control w-full mb-4">
+                <div class="label">
+                    <span class="label-text text-lg">Descripción</span>
+                    <span class="badge badge-primary">Obligatorio</span>
+                </div>
+                <textarea name="description" class="textarea textarea-bordered h-32 text-white p-4" placeholder="Descripción">{{ old('description', $task->description) }}</textarea>
+            </label>
+
+            {{-- Prioridad --}}
+            <label class="form-control w-full mb-4">
+                <div class="label">
+                    <span class="label-text text-lg">Nivel de prioridad</span>
+                    <span class="badge badge-primary">Obligatorio</span>
+                </div>
+                <select name="priority" class="select select-bordered text-white p-4 w-full">
+                    <option disabled selected>Selecciona un nivel</option>
+                    <option value="high" {{ old('priority', $task->priority) == 'high' ? 'selected' : '' }}>Alta
+                    </option>
+                    <option value="medium" {{ old('priority', $task->priority) == 'medium' ? 'selected' : '' }}>Media
+                    </option>
+                    <option value="low" {{ old('priority', $task->priority) == 'low' ? 'selected' : '' }}>Baja
+                    </option>
+
+                </select>
+            </label>
+
+            {{-- Tags --}}
+            <label class="form-control w-full mb-4">
+                <div class="label">
+                    <span class="label-text text-lg">Tags
+                        <div class="tooltip" data-tip="No es necesario añadir un '#' al principio de cada tag.">
+                            <div class="badge badge-secondary">Info</div>
+                        </div>
+                    </span>
+                    <span class="badge badge-accent">Opcional</span>
+                </div>
+                <input type="text" name="tags[]" placeholder="Escribe tags separados por espacio..."
+                    value="{{ old('tags', isset($task->tags) ? implode(' ', $task->tags) : '') }}"
+                    class="input input-bordered w-full text-white p-4" />
+
+            </label>
+
+            <button type="submit" class="btn btn-primary w-full text-lg p-4">Editar tarea</button>
+        </form>
+    </article>
+</x-app-layout>
