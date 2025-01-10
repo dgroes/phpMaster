6d5c10efc7e46dd2f3ac324b0a3a4c400cc25ae3
Diego Pasten <diegopasten78@gmail.com>
2025-01-10 12:18:59 -0300

    feat: :building_construction: Se agregan nuevos script: Toastigy y sweealert

--- proyectos/curso-reserva/resources/views/layouts/app.blade.php
+++ proyectos/curso-reserva/resources/views/layouts/app.blade.php
@@ -80,7 +80,7 @@
     <script src="{{ asset('assets/libs/node-waves/waves.min.js') }}"></script>
     <script src="{{ asset('assets/libs/feather-icons/feather.min.js') }}"></script>
     <script src="{{ asset('assets/js/pages/plugins/lord-icon-2.1.0.js') }}"></script>
-    <script src="{{ asset('assets/js/plugins.js') }}"></script>
+    {{-- <script src="{{ asset('assets/js/plugins.js') }}"></script> --}}
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
@@ -95,6 +95,11 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
 
+    {{-- Toastify JS: Mensajes de alertas dinamicos --}}
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
+
+    {{-- SweetAlert2 JS: Mensajes de alertas dinamicos --}}
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 
     <!-- App js -->
