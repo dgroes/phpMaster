aba9171e2fad866fdce69592dbcc4517f09b74af
Diego Pasten <diegopasten78@gmail.com>
2025-01-10 12:16:01 -0300

    feat: :building_construction: controller User completado

--- proyectos/curso-reserva/app/Http/Controllers/UserController.php
+++ proyectos/curso-reserva/app/Http/Controllers/UserController.php
@@ -17,7 +17,81 @@ class UserController extends Controller
 
     public function create()
     {
+        // Obtener todos los roles para el campo seleccionable
         $roles = Role::all();
+
+        //Devolver la vista de creación de usuarios con los roles disponibles
         return view('usuarios.create', compact('roles'));
+    }
+    public function store(Request $request){
+        $request->validate([
+            'nombres' => 'required|string|max:255',
+            'apellidos' => 'required|string|max:255',
+            'teléfono' => 'required|string|max:15',
+            'rol_id' => 'required|integer',
+            'email' => 'required|email|unique:users',
+            // 'password' => 'required|string|max:8|in:password',
+            'foto' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
+        ]);
+
+        $fotoPath = null;
+        if ($request->hasFile('foto')){
+            $fotoPath = $request->file('foto')->store('fotos', 'public');
+        }
+
+        user::create([
+            'nombres' => $request->nombres,
+            'apellidos' => $request->apellidos,
+            'teléfono' => $request->teléfono,
+            'email' => $request->email,
+            'rol_id' => $request->rol_id,
+            'password' => Hash::make('password'),
+            'foto' => $fotoPath,
+        ]);
+
+        return redirect()->route('usuarios.index')->with('success', 'Registro creado correctamente');
+    }
+
+    public function edit($id){
+        $usuario = User::findOrFail($id); // Buscar el usuario por su ID
+        $roles = Role::all(); // Obtener todos los roles para el campo seleccionable
+
+        return view('usuarios.edit', compact('usuario', 'roles'));
+    }
+
+    public function update(Request $request, $id){
+        $request->validate([
+            'nombres' => 'required|string|max:255',
+            'apellidos' => 'required|string|max:255',
+            'teléfono' => 'required|string|max:15',
+            'email' => 'required|email|unique:users,email,'.$id, // Validar que el email sea único, excepto para el usuario actual
+            'rol_id' => 'required|integer',
+            'foto' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
+        ]);
+
+        $usuario = User::findOrFail($id); // Buscar el usuario por su ID
+
+        $fotoPath = $usuario->foto;
+        if ($request->hasFile('foto')){
+            $fotoPath = $request->file('foto')->store('fotos', 'public');
+            $usuario->foto = $fotoPath;
+        }
+
+        $usuario->update([
+            'nombres' => $request->nombres,
+            'apellidos' => $request->apellidos,
+            'teléfono' => $request->teléfono,
+            'email' => $request->email,
+            'rol_id' => $request->rol_id,
+        ]);
+
+        return redirect()->route('usuarios.index')->with('success', 'Registro actualizado correctamente');
+    }
+
+    public function destroy($id){
+        $usuario = User::findOrFail($id);
+        $usuario->delete();
+
+        return redirect()->route('usuarios.index')->with('success', 'Registro eliminado correctamente');
     }
 }
