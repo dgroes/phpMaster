3cf8be87fc4559e0b78d65f06f3aebcb433d0e7a
Diego Pasten <diegopasten78@gmail.com>
2025-01-11 17:59:08 -0300

    feat: :rocket: Se agregan las rutas "generales" para Reservationks

--- proyectos/curso-reserva/routes/web.php
+++ proyectos/curso-reserva/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ReservationController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
@@ -31,5 +32,8 @@ Route::middleware('auth')->group(function () {
 
 //Ruta de usuarios
 Route::resource('usuarios', UserController::class);
+
+//Ruta de reservaciones
+Route::resource('reservations', ReservationController::class);
 
 require __DIR__.'/auth.php';
