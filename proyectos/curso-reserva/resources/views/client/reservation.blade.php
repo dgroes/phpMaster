0c62add9b5edff816fb343e1598bcb8d7e92c7c8
Diego Pasten <diegopasten78@gmail.com>
2025-01-16 19:55:24 -0300

    feat: :art: Se formatea el código

--- proyectos/curso-reserva/resources/views/client/reservation.blade.php
+++ proyectos/curso-reserva/resources/views/client/reservation.blade.php
@@ -20,70 +20,55 @@
     <div class="row">
         <div class="col-lg-12">
             <div class="card">
-
                 <div class="card-header align-items-center d-flex">
-                    <h4 class="card-title mb-0 flex-grow-1">Crear Nueva Reservasss</h4>
+                    <h4 class="card-title mb-0 flex-grow-1">Crear Nueva Reserva</h4>
                 </div>
-
                 <div class="card-body">
-                    <form class="row gy-4" id="reservationForm">
+                    <form class="row gy-1" id="reservationForm">
                         @csrf
 
-                        {{-- Nombre del Usuario --}}
                         <div class="col-xxl-3 col-md-6">
                             <div>
                                 <label for="user" class="form-label">{{ __('Usuario') }}</label>
-                                <input id="user" type="text"
-                                    class="form-control @error('reservation_date') is-invalid @enderror"
-                                    value="{{ Auth::user()->nombres }} {{ Auth::user()->apellidos }}" readonly>
-                                <input type="hidden" name="user_id" id="user_id" value="{{ Auth::user()->id }}">
+                                <input id="user" type="text" class="form-control @error('reservation_date') is-invalid @enderror" value="{{ Auth::user()->nombres }} {{ Auth::user()->apellidos }}" readonly>
+                                <input type="hidden" id="user_id" name="user_id" value="{{ Auth::user()->id }}">
                             </div>
                         </div>
 
-                        {{-- Nombre del Consultor --}}
                         <div class="col-xxl-3 col-md-6">
                             <div>
                                 <label for="consultant_id" class="form-label">{{ __('Consultor') }}</label>
-                                <select class="form-select @error('consultant_id') is-invalid @enderror" id="consultant_id"
-                                    name="consultant_id" required
-                                    value="{{ old('consultant_id') }}>
-                                    <option value=""
-                                    readonly>{{ __('Seleccionar un consultor') }}-</option>
-                                    @foreach ($consultants as $consultant)
-                                        <option value="{{ $consultant->id }}">{{ $consultant->nombres }}
-                                            {{ $consultant->apellidos }}</option>
+                                <select class="form-select @error('consultant_id') is-invalid @enderror" id="consultant_id" name="consultant_id" required>
+                                    <option value="">Seleccionar Consultor</option>
+                                    @foreach ($consultants as $consultant )
+                                        <option value="{{ $consultant->id }}">{{ $consultant->nombres }} {{ $consultant->apellidos }}</option>
                                     @endforeach
                                 </select>
                                 @error('consultant_id')
                                     <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
+                                        <strong>{{ $message}}</strong>
                                     </span>
                                 @enderror
                             </div>
                         </div>
 
-                        {{-- Fecha de la Reserva --}}
                         <div class="col-xxl-3 col-md-6">
                             <div>
-                                <label for="reservation_date" class="form-label">{{ __('Fecha de Reservación') }}</label>
-                                <input type="date" class="form-control @error('reservation_date') is-invalid @enderror"
-                                    id="reservation_date" name="reservation_date" value="{{ old('reservation_date') }}"
-                                    required>
+                                <label for="reservation_date" class="form-label">{{ __('Fecha de Reserva') }}</label>
+                                <input type="date" class="form-control @error('reservation_date') is-invalid @enderror" id="reservation_date" name="reservation_date" value="{{ old('reservation_date') }}" required>
                                 @error('reservation_date')
                                     <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
+                                        <strong>{{ $message}}</strong>
                                     </span>
                                 @enderror
                             </div>
                         </div>
 
-                        {{-- Hora de inicio la Reserva --}}
                         <div class="col-xxl-3 col-md-6">
                             <div>
-                                <label for="start_time" class="form-label">{{ __('Hora de inicio') }}</label>
-                                <select class="form-select @error('start_time') is-invalid @enderror" id="start_time"
-                                    name="start_time" required>
-                                    <option value="" readonly>{{ __('Seleccionar una Hora') }}</option>
+                                <label for="start_time" class="form-label">{{ __('Hora de Inicio') }}</label>
+                                <select class="form-select @error('start_time') is-invalid @enderror" id="start_time" name="start_time" required>
+                                    <option value="">Seleccionar una hora</option>
                                     <option value="09:00">09:00</option>
                                     <option value="10:00">10:00</option>
                                     <option value="11:00">11:00</option>
@@ -95,60 +80,52 @@
                                 </select>
                                 @error('start_time')
                                     <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
+                                        <strong>{{ $message}}</strong>
                                     </span>
                                 @enderror
                             </div>
                         </div>
 
-                        {{-- Hora de fin de la Reserva --}}
                         <div class="col-xxl-3 col-md-6">
                             <div>
-                                <label for="end_time" class="form-label">{{ __('Hora de Fin') }}</label>
-                                <input type="text" class="form-control @error('end_time') is-invalid @enderror"
-                                    id="end_time" name="end_time" value="{{ old('end_time') }}" required readonly>
+                                <label for="end_time" class="form-label">{{ __('Hora Fin') }}</label>
+                                <input type="text" class="form-control @error('end_time') is-invalid @enderror" id="end_time" name="end_time" readonly>
                                 @error('end_time')
                                     <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
+                                        <strong>{{ $message}}</strong>
                                     </span>
                                 @enderror
                             </div>
                         </div>
 
-                        {{-- Estado de la Reserva --}}
                         <div class="col-xxl-3 col-md-6">
                             <div>
                                 <label for="reservation_status" class="form-label">{{ __('Estado de la Reserva') }}</label>
-                                <select class="form-select @error('reservation_status') is-invalid @enderror"
-                                    id="reservation_status" name="reservation_status" required>
-                                    <option value="" readonly>{{ __('Seleccionar un estado') }}</option>
-                                    <option value="confirmada">Confirmada</option>
+                                <select class="form-select @error('reservation_status') is-invalid @enderror" id="reservation_status" name="reservation_status" required>
+                                    <option value="">Seleccionar un estado</option>
                                     <option value="pendiente">Pendiente</option>
+                                    <option value="confirmada">Confirmada</option>
                                 </select>
                                 @error('reservation_status')
                                     <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
+                                        <strong>{{ $message}}</strong>
                                     </span>
                                 @enderror
                             </div>
                         </div>
 
-                        {{-- Total a pagar --}}
                         <div class="col-xxl-3 col-md-6">
                             <div>
                                 <label for="total_amount" class="form-label">{{ __('Total a pagar (USD)') }}</label>
-                                <input type="text" class="form-control @error('total_amount') is-invalid @enderror"
-                                    id="total_amount" name="total_amount" value="50.000" required readonly>
+                                <input type="text" class="form-control @error('total_amount') is-invalid @enderror" id="total_amount" name="total_amount" readonly>
                                 @error('total_amount')
                                     <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
+                                        <strong>{{ $message}}</strong>
                                     </span>
                                 @enderror
                             </div>
                         </div>
 
-
-                        {{-- Metodo de pago --}}
                         <div class="col-xxl-3 col-md-6">
                             <div>
                                 <label for="payment_status" class="form-label">{{ __('Metodo de Pago') }}</label>
@@ -159,7 +136,6 @@
                     </form>
                 </div>
             </div>
-
         </div>
     </div>
 @endsection
