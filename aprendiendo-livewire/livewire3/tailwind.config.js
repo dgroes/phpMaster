ac5c8f320174f0b6934c163160351d26bfeb4106
Diego Pasten <diegopasten78@gmail.com>
2025-01-20 10:23:12 -0300

    feat: :heavy_plus_sign: Se instala Jetstream con Livewire

--- aprendiendo-livewire/livewire3/tailwind.config.js
+++ aprendiendo-livewire/livewire3/tailwind.config.js
@@ -1,14 +1,16 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
+        './resources/views/**/*.blade.php',
     ],
+
     theme: {
         extend: {
             fontFamily: {
@@ -16,5 +18,6 @@ export default {
             },
         },
     },
-    plugins: [],
+
+    plugins: [forms, typography],
 };
