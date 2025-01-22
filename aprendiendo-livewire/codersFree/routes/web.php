e155ae897bd179947e0e82a8f44ae07575875c14
Diego Pasten <diegopasten78@gmail.com>
2025-01-22 19:58:35 -0300

    feat: :arrow_down: Se ajustan las rutas

--- aprendiendo-livewire/codersFree/routes/web.php
+++ aprendiendo-livewire/codersFree/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\Post\CreatePost;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,4 +15,6 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+    // Route::get('/dashboard', CreatePost::class)
+    // ->name('dashboard');
 });
