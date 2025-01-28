2f77dfa794ee78f6d05979d37748f1b71b7a2f09
Diego Pasten <diegopasten78@gmail.com>
2025-01-28 19:03:08 -0300

    feat: :truck: Se modifican las rutas

--- aprendiendo-livewire/codersFree/routes/web.php
+++ aprendiendo-livewire/codersFree/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\Formulario;
 use App\Livewire\Post\CreatePost;
 use Illuminate\Support\Facades\Route;
 
@@ -12,11 +13,16 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
+
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
     // Route::get('/dashboard', CreatePost::class)
     // ->name('dashboard');
+
+   /*  Route::get('/dashboard', Formulario::class)
+    ->name('dashboard'); */
 
     Route::view('/prueba', 'prueba')->name('prueba');
 });
