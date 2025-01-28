6a72f2a2c0fc37d0ab4ab5dae246715bb2051a46
Diego Pasten <diegopasten78@gmail.com>
2025-01-28 19:04:59 -0300

    feat: :lipstick: Se añaden y modifican algunas cosas

--- aprendiendo-livewire/codersFree/resources/views/livewire/formulario.blade.php
+++ aprendiendo-livewire/codersFree/resources/views/livewire/formulario.blade.php
@@ -2,6 +2,11 @@
 
     {{-- Formulario de creación de un Post --}}
     <div class="bg-white shadow rounded-lg p-6">
+
+        @if ($postCreate->image)
+            <img src="{{ $postCreate->image->temporaryUrl() }}" alt="">
+        @endif
+
         <h2 class="text-lg font-semibold mb-4">Crear un nuevo Post</h2>
         <form wire:submit="save">
             {{-- Nombre --}}
@@ -37,6 +42,23 @@
                 <x-input-error for='postCreate.category_id' />
             </div>
 
+            {{-- Imagen --}}
+            <div class="mb-4">
+                <x-label>Imagen</x-label>
+
+                <div x-data="{ uploading: false, progress: 0 }" x-on:livewire-upload-start="uploading = true"
+                    x-on:livewire-upload-finish="uploading = false" x-on:livewire-upload-cancel="uploading = false"
+                    x-on:livewire-upload-error="uploading = false"
+                    x-on:livewire-upload-progress="progress = $event.detail.progress">
+                    <input type="file" wire:model="postCreate.image" />
+
+                    <!-- Progress Bar -->
+                    <div x-show="uploading">
+                        <progress max="100" x-bind:value="progress"></progress>
+                    </div>
+                </div>
+            </div>
+
             {{-- Etiquetas --}}
             <div class="mb-4">
                 <x-label>Etiquetas</x-label>
@@ -56,16 +78,34 @@
 
             {{-- Enviar --}}
             <div class="flex justify-end">
-                <x-button>Crear</x-button>
+                {{--   <x-button wire:loading.class="opacity-50" wire:target="save">
+                    Crear
+                </x-button> --}}
+                <x-button wire:loading.attr="disabled">
+                    Crear
+                </x-button>
             </div>
 
+            {{--  <div wire:loading.delay>
+                <div>Hola mundo</div>
+            </div>
+            --}}
 
         </form>
+
+        <div wire:loading wire:target="save">
+            Procesando...
+        </div>
     </div>
 
     {{-- Listado de los Posts --}}
     <div class="bg-white shadow rounded-lg p-6 mt-6">
         <h2 class="text-lg font-semibold mb-4">Listado de Posts</h2>
+
+        <div class="mb-4">
+            <x-input class="w-full" placeholder="Buscar" wire:model.live="search"/>
+        </div>
+
         <ul class="list-disc list-inside space-y-6">
             @foreach ($posts as $post)
                 <li class="flex justify-between" wire:key="post-{{ $post->id }}">
@@ -81,7 +121,15 @@
                 </li>
             @endforeach
         </ul>
+
+        {{-- Paginación --}}
+        <div class="mt-6" >
+            {{ $posts->links('vendor.livewire.simple-tailwind') }}
+            {{-- {{ $posts->links() }} --}}
+        </div>
     </div>
+
+
 
     {{-- Modal de formulario de edición --}}
     <form wire:submit="update">
