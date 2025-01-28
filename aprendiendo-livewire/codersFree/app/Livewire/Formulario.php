a8aa2fd7da56a523ed511682ce8eb33dcc9ce970
Diego Pasten <diegopasten78@gmail.com>
2025-01-28 19:02:03 -0300

    feat: Se actualia el componente

--- aprendiendo-livewire/codersFree/app/Livewire/Formulario.php
+++ aprendiendo-livewire/codersFree/app/Livewire/Formulario.php
@@ -7,18 +7,29 @@ use App\Livewire\Forms\PostEditForm;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\Tag;
+use Livewire\Attributes\Lazy;
 use Livewire\Attributes\Rule;
+use Livewire\Attributes\Url;
 use Livewire\Component;
+use Livewire\WithFileUploads;
+use Livewire\WithPagination;
 
+// #[Lazy]
 class Formulario extends Component
 {
+    use WithFileUploads;
+    use WithPagination;
+
     public $categories;
     public $tags;
 
     public PostCreateForm $postCreate;
     public PostEditForm $postEdit;
 
-    public $posts;
+    #[Url(as: 's')]
+    public $search = '';
+
+    // public $posts;
 
 
     // Mëtodo para inicializar las propiedades Mount
@@ -27,32 +38,26 @@ class Formulario extends Component
     {
         $this->categories = Category::all();
         $this->tags = Tag::all();
-        $this->posts = Post::all();
+        /*  $this->posts = Post::all(); */
     }
 
-    public function updating($property, $value){
-        if($property == 'posCreate.category_id'){
-            if($value > 3){
+    public function updating($property, $value)
+    {
+        if ($property == 'posCreate.category_id') {
+            if ($value > 3) {
                 throw new \Exception('No puedes seleccionar una categoria mayor a 3');
             }
         }
     }
 
-    /* public function updated($property, $value){}
-    } */
-
-    public function hydrate(){}
-
-    public function dehydrate(){}
-
-    // Método para guarda el post
     public function save()
     {
         //Validaciones con PostCreateForm
         $this->postCreate->save();
+        $this->resetPage(pageName: 'pagePost'); //Resetea la paginación
 
         // Refresca la lista de posts
-        $this->posts = Post::all();
+        /* $this->posts = Post::all(); */
 
         // Disparar evento
         $this->dispatch('post-created', 'Nuevo articulo creado', 'success');
@@ -69,7 +74,7 @@ class Formulario extends Component
     public function update()
     {
         $this->postEdit->update();
-        $this->posts = Post::all(); //Refrescar listado dinamico
+        /*  $this->posts = Post::all(); */ //Refrescar listado dinamico
 
         // Disparar evento
         $this->dispatch('post-created', 'Articulo actualizado');
@@ -81,14 +86,23 @@ class Formulario extends Component
         $post = Post::find($postId);
         $post->delete();
 
-        $this->posts = Post::all(); //Refrescar listado dinamico
+        /* $this->posts = Post::all(); */ //Refrescar listado dinamico
         // Disparar evento
         $this->dispatch('post-created', 'Articulo eliminado');
     }
 
+    /*  public function placeholder(){
+        return view('livewire.placeholder.skeleton');
+    } */
     // Método para renderizar la vista
     public function render()
     {
-        return view('livewire.formulario');
+        $posts = Post::orderBy('id', 'desc')
+            ->when($this->search, function ($query) {
+                return $query->where('title', 'like', '%' . $this->search . '%');
+            })
+            ->paginate(5, pageName: 'pagePost');
+
+        return view('livewire.formulario', compact('posts'));
     }
 }
