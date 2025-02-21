d03da730cd89e5e050eda9fbacb944f6c8d312ea
Diego Pasten <diegopasten78@gmail.com>
2025-02-21 13:01:40 -0300

    feat: :building_construction: Se le agregan nuevos campos a la tabla User, acorde a las nuevas tablas de laravel-world y sus relaciones

---
+++ aprendiendo-filament/curso-filamentphp/database/migrations/2025_02_21_131237_add_address_fields_to_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        /* C02: Agregar nuevos campos a la tabla Users */
+        Schema::table('users', function (Blueprint $table) {
+            // Añadiendo nuevos campos a la tabla Users
+            //Laravel ya detecta a que tablas pertenecen los campos, sino será necesario agregar las relaciones
+            $table->foreignId('country_id')->nullable()->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('state_id')->nullable()->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('city_id')->nullable()->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->string('address')->nullable();
+            $table->string('postal_code')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
