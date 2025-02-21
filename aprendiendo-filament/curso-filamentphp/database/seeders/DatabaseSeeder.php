451ba9c31a9194c09ffde5afde4c271157fcdfa9
Diego Pasten <diegopasten78@gmail.com>
2025-02-21 00:41:52 -0300

    feat: :wrench: Creación de la semilla de Adminstrador

--- aprendiendo-filament/curso-filamentphp/database/seeders/DatabaseSeeder.php
+++ aprendiendo-filament/curso-filamentphp/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +17,16 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+       /*  User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]); */
+
+        /* C00: Creación de un perfil Adminstrador con Seed */
+        DB::table('users')->insert([
+            'name' => "Administrador",
+            'email' => "admin@admin.com",
+            'password' => Hash::make('password')
         ]);
     }
 }
