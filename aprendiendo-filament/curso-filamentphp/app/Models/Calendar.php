45ec097ace26bf483d2ce4d140d4fafee4e52aac
Diego Pasten <diegopasten78@gmail.com>
2025-02-23 13:52:53 -0300

    feat: :bulb: Se añade los campos rellenables, se utiliza $guarded

--- aprendiendo-filament/curso-filamentphp/app/Models/Calendar.php
+++ aprendiendo-filament/curso-filamentphp/app/Models/Calendar.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Calendar extends Model
 {
-    //
+    //Rellenado de campos
+
+    //Opción en entornos de desarrollo
+    protected $guarded = [];
+
+    //Opción en entornos de producción
+    /* protected $fillable = [
+        'name',
+        'year',
+        'active',
+    ]; */
 }
