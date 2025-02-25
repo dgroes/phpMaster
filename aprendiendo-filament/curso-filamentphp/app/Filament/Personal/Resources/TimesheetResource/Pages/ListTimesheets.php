7a35172d765b7a09807242ed86f5fe56d1e7b66c
Diego Pasten <diegopasten78@gmail.com>
2025-02-25 14:17:24 -0300

    feat: :fire: Creación y configuración de List Timesheet de Personal

---
+++ aprendiendo-filament/curso-filamentphp/app/Filament/Personal/Resources/TimesheetResource/Pages/ListTimesheets.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Filament\Personal\Resources\TimesheetResource\Pages;
+
+use App\Filament\Personal\Resources\TimesheetResource;
+use App\Models\Timesheet;
+use Carbon\Carbon;
+use Filament\Actions;
+use Filament\Actions\Action;
+use Filament\Resources\Pages\ListRecords;
+use Illuminate\Support\Facades\Auth;
+
+class ListTimesheets extends ListRecords
+{
+    protected static string $resource = TimesheetResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            /* C13: Header Actions */
+            Action::make("inWork")
+                ->label("Entrar a Trabajar") //Texto del action
+                ->color('success') //Color del botón
+                ->requiresConfirmation() //Modal de confirmación
+                ->action(function () {
+                    $user = Auth::user();
+                    $timesheet = new Timesheet();
+                    $timesheet->calendar_id = 1;
+                    $timesheet->user_id = $user->id;
+                    $timesheet->day_in = Carbon::now();
+                    $timesheet->day_out = Carbon::now();
+                    $timesheet->type = 'work';
+                    $timesheet->save();
+                }),
+            Action::make("inPause")
+                ->label("Comenzar Pausa")
+                ->color('info')
+                ->requiresConfirmation(),
+            Actions\CreateAction::make(),
+
+        ];
+    }
+}
