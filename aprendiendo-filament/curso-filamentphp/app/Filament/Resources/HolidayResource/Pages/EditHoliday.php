219b0f5c3837595a2025c21d42b70fc436306162
Diego Pasten <diegopasten78@gmail.com>
2025-02-27 14:32:49 -0300

    feat: Edit de vaciones para aprovar o rechazar

--- aprendiendo-filament/curso-filamentphp/app/Filament/Resources/HolidayResource/Pages/EditHoliday.php
+++ aprendiendo-filament/curso-filamentphp/app/Filament/Resources/HolidayResource/Pages/EditHoliday.php
@@ -3,8 +3,16 @@
 namespace App\Filament\Resources\HolidayResource\Pages;
 
 use App\Filament\Resources\HolidayResource;
+use App\Mail\HolidayApproved;
+use App\Mail\HolidayDecline;
+use App\Models\User;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Log;
+
 
 class EditHoliday extends EditRecord
 {
@@ -15,5 +23,46 @@ class EditHoliday extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    /* C19: Envio de correo */
+    protected function handleRecordUpdate(Model $record, array $data): Model
+    {
+        $record->update($data);
+
+        //SEND EMAIL ONLY IF APPROVED
+        if($record->type == 'approved'){
+            $user = User::find($record->user_id);
+            $data = array(
+                'name' => $user->name,
+                'email' => $user->email,
+                'day' => $record->day
+            );
+            Mail::to($user)->send(new HolidayApproved($data));
+            $recipient =$user;
+
+            Notification::make()
+                ->title('Solicitud de vacaciones')
+                ->body("El día ".$data['day'].' esta aprovado')
+                ->sendToDatabase($recipient);
+        }
+        else if($record->type == 'decline'){
+            $user = User::find($record->user_id);
+            $data = array(
+                'name' => $user->name,
+                'email' => $user->email,
+                'day' => $record->day
+            );
+            Mail::to($user)->send(new HolidayDecline($data));
+            $recipient =$user;
+
+            Notification::make()
+                ->title('Solicitud de vacaciones')
+                ->body("El día ".$data['day'].' esta rechazado')
+                ->sendToDatabase($recipient);
+        }
+
+
+        return $record;
     }
 }
